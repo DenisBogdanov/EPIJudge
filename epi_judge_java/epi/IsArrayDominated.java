d4f316fea6de4a7f85ff27f5eb4afedf288ebc2d
Denis Bogdanov <bogdanov.math@gmail.com>
2022-04-18 20:39:34 +0300

    Java.

--- epi_judge_java/epi/IsArrayDominated.java
+++ epi_judge_java/epi/IsArrayDominated.java
@@ -1,36 +1,48 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 import epi.test_framework.TestFailure;
 import epi.test_framework.TimedExecutor;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
 class Team {
+  private final List<Player> players;
+
+  public Team(List<Integer> height) {
+    players = height.stream().map(Player::new).collect(Collectors.toList());
+  }
+
+  public static boolean validPlacementExists(Team team0, Team team1) {
+    Collections.sort(team0.players);
+    Collections.sort(team1.players);
+
+    for (int i = 0; i < team0.players.size(); i++) {
+      if (team0.players.get(i).height >= team1.players.get(i).height) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+
   private static class Player implements Comparable<Player> {
     public Integer height;
 
-    public Player(Integer h) { height = h; }
+    public Player(Integer h) {
+      height = h;
+    }
 
     @Override
     public int compareTo(Player that) {
       return Integer.compare(height, that.height);
     }
   }
-
-  public Team(List<Integer> height) {
-    players =
-        height.stream().map(h -> new Player(h)).collect(Collectors.toList());
-  }
-
-  // Checks if team0 can be placed in front of team1.
-  public static boolean validPlacementExists(Team team0, Team team1) {
-    // TODO - you fill in here.
-    return true;
-  }
-  private List<Player> players;
 }
+
 public class IsArrayDominated {
   @EpiTest(testDataFile = "is_array_dominated.tsv")
   public static void
@@ -50,7 +62,8 @@ public class IsArrayDominated {
     System.exit(
         GenericTest
             .runFromAnnotations(args, "IsArrayDominated.java",
-                                new Object() {}.getClass().getEnclosingClass())
+                new Object() {
+                }.getClass().getEnclosingClass())
             .ordinal());
   }
 }
