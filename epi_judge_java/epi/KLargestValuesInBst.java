f962ded4e25046d870d30562099ca3d6201f1ac8
Denis Bogdanov <bogdanov.math@gmail.com>
2022-04-19 21:30:46 +0300

    Java.

--- epi_judge_java/epi/KLargestValuesInBst.java
+++ epi_judge_java/epi/KLargestValuesInBst.java
@@ -1,18 +1,49 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.EpiTestComparator;
 import epi.test_framework.GenericTest;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.function.BiPredicate;
-public class KLargestValuesInBst {
-  @EpiTest(testDataFile = "k_largest_values_in_bst.tsv")
 
+public class KLargestValuesInBst {
+
+  @EpiTest(testDataFile = "k_largest_values_in_bst.tsv")
   public static List<Integer> findKLargestInBst(BstNode<Integer> tree, int k) {
-    // TODO - you fill in here.
-    return Collections.emptyList();
+//    return withInOrderTraversal(tree, k);
+
+
+    List<Integer> result = new ArrayList<>(k);
+    reverseOrder(tree, k, result);
+    return result;
   }
+
+  private static void reverseOrder(BstNode<Integer> tree, int k, List<Integer> list) {
+    if (tree != null && list.size() < k) {
+      reverseOrder(tree.right, k, list);
+      if (list.size() < k) {
+        list.add(tree.data);
+        reverseOrder(tree.left, k, list);
+      }
+    }
+  }
+
+  private static List<Integer> withInOrderTraversal(BstNode<Integer> tree, int k) {
+    List<Integer> result = new ArrayList<>();
+    inOrder(tree, result);
+    return result.subList(result.size() - k, result.size());
+  }
+
+  private static void inOrder(BstNode<Integer> tree, List<Integer> list) {
+    if (tree != null) {
+      inOrder(tree.left, list);
+      list.add(tree.data);
+      inOrder(tree.right, list);
+    }
+  }
+
   @EpiTestComparator
   public static boolean comp(List<Integer> expected, List<Integer> result) {
     if (result == null) {
@@ -27,7 +58,8 @@ public class KLargestValuesInBst {
     System.exit(
         GenericTest
             .runFromAnnotations(args, "KLargestValuesInBst.java",
-                                new Object() {}.getClass().getEnclosingClass())
+                new Object() {
+                }.getClass().getEnclosingClass())
             .ordinal());
   }
 }
