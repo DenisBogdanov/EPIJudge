62c0e159e13944f8282542b5b2192da355dc2fed
Denis Bogdanov <bogdanov.math@gmail.com>
2022-04-21 21:06:10 +0300

    Java.

--- epi_judge_java/epi/MinimumDistance3SortedArrays.java
+++ epi_judge_java/epi/MinimumDistance3SortedArrays.java
@@ -1,42 +1,63 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 
 import java.util.List;
+import java.util.NavigableSet;
+import java.util.TreeSet;
+
 public class MinimumDistance3SortedArrays {
 
-  public static class ArrayData implements Comparable<ArrayData> {
-    public int val;
-    public int idx;
+  @EpiTest(testDataFile = "minimum_distance_3_sorted_arrays.tsv")
+  public static int findMinDistanceSortedArrays(List<List<Integer>> sortedArrays) {
+    int[] heads = new int[sortedArrays.size()];
 
-    public ArrayData(int idx, int val) {
+    NavigableSet<ArrayData> currentHeads = new TreeSet<>();
+    for (int i = 0; i < sortedArrays.size(); i++) {
+      currentHeads.add(new ArrayData(i, sortedArrays.get(i).get(0)));
+    }
+
+    int result = Integer.MAX_VALUE;
+
+    while (true) {
+      result = Math.min(result, currentHeads.last().val - currentHeads.first().val);
+      int indexNextMin = currentHeads.first().index;
+      heads[indexNextMin] = heads[indexNextMin] + 1;
+      if (heads[indexNextMin] >= sortedArrays.get(indexNextMin).size()) {
+        return result;
+      }
+
+      currentHeads.pollFirst();
+      currentHeads.add(new ArrayData(indexNextMin, sortedArrays.get(indexNextMin).get(heads[indexNextMin])));
+    }
+  }
+
+  public static class ArrayData implements Comparable<ArrayData> {
+    public int index;
+    public int val;
+
+    public ArrayData(int index, int val) {
       this.val = val;
-      this.idx = idx;
+      this.index = index;
     }
 
     @Override
     public int compareTo(ArrayData o) {
       int result = Integer.compare(val, o.val);
       if (result == 0) {
-        result = Integer.compare(idx, o.idx);
+        result = Integer.compare(index, o.index);
       }
       return result;
     }
-  }
-
-  @EpiTest(testDataFile = "minimum_distance_3_sorted_arrays.tsv")
-
-  public static int
-  findMinDistanceSortedArrays(List<List<Integer>> sortedArrays) {
-    // TODO - you fill in here.
-    return 0;
   }
 
   public static void main(String[] args) {
     System.exit(
         GenericTest
             .runFromAnnotations(args, "MinimumDistance3SortedArrays.java",
-                                new Object() {}.getClass().getEnclosingClass())
+                new Object() {
+                }.getClass().getEnclosingClass())
             .ordinal());
   }
 }
