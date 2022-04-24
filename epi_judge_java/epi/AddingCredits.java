34b169bb9f0096fe9629c7739e899b8c6ef71120
Denis Bogdanov <bogdanov.math@gmail.com>
2022-04-24 16:13:01 +0300

    Java.

--- epi_judge_java/epi/AddingCredits.java
+++ epi_judge_java/epi/AddingCredits.java
@@ -5,40 +5,90 @@ import epi.test_framework.EpiUserType;
 import epi.test_framework.GenericTest;
 import epi.test_framework.TestFailure;
 
-import java.util.List;
+import java.util.*;
 
 public class AddingCredits {
 
   public static class ClientsCreditsInfo {
-    public void insert(String clientID, int c) {
-      // TODO - you fill in here.
-      return;
+    private int addedToAll = 0;
+    private final Map<String, Integer> clientToCreditMap = new HashMap<>();
+    private final NavigableMap<Integer, Set<String>> creditsToClientsMap = new TreeMap<>();
+
+    public void insert(String clientID, int credits) {
+      remove(clientID);
+      clientToCreditMap.put(clientID, credits - addedToAll);
+      creditsToClientsMap.computeIfAbsent(credits - addedToAll, k -> new HashSet<>()).add(clientID);
     }
 
     public boolean remove(String clientID) {
-      // TODO - you fill in here.
-      return true;
+      Integer credit = clientToCreditMap.get(clientID);
+      if (credit != null) {
+        creditsToClientsMap.get(credit).remove(clientID);
+        if (creditsToClientsMap.get(credit).isEmpty()) {
+          creditsToClientsMap.remove(credit);
+        }
+
+        clientToCreditMap.remove(clientID);
+        return true;
+      }
+
+      return false;
     }
 
     public int lookup(String clientID) {
-      // TODO - you fill in here.
-      return 0;
+      Integer credit = clientToCreditMap.get(clientID);
+      return credit == null ? -1 : credit + addedToAll;
     }
 
-    public void addAll(int C) {
-      // TODO - you fill in here.
-      return;
+    public void addAll(int c) {
+      addedToAll += c;
     }
 
     public String max() {
-      // TODO - you fill in here.
-      return "";
+      return creditsToClientsMap.isEmpty()
+          ? ""
+          : creditsToClientsMap.lastEntry().getValue().iterator().next();
     }
 
     @Override
     public String toString() {
       // TODO - you fill in here.
       return super.toString();
+    }
+
+    private static class Client {
+      final String id;
+      int credits;
+
+      public Client(String id) {
+        this.id = id;
+      }
+
+      public Client(String id, int credits) {
+        this.id = id;
+        this.credits = credits;
+      }
+
+      @Override
+      public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Client client = (Client) o;
+        return id.equals(client.id);
+      }
+
+      @Override
+      public int hashCode() {
+        return Objects.hash(id);
+      }
+
+      @Override
+      public String toString() {
+        return "Client{" +
+            "id='" + id + '\'' +
+            ", credits=" + credits +
+            '}';
+      }
     }
   }
 
