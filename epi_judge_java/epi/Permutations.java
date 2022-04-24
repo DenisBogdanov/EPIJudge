0665b26c6201e3fdffad03e99572f0d6b25adb1d
Denis Bogdanov <bogdanov.math@gmail.com>
2022-04-24 20:19:52 +0300

    Java.

--- epi_judge_java/epi/Permutations.java
+++ epi_judge_java/epi/Permutations.java
@@ -1,18 +1,34 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.EpiTestComparator;
-import epi.test_framework.GenericTest;
 import epi.test_framework.LexicographicalListComparator;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-public class Permutations {
-  @EpiTest(testDataFile = "permutations.tsv")
 
-  public static List<List<Integer>> permutations(List<Integer> A) {
-    // TODO - you fill in here.
-    return null;
+public class Permutations {
+
+  @EpiTest(testDataFile = "permutations.tsv")
+  public static List<List<Integer>> permutations(List<Integer> list) {
+    List<List<Integer>> result = new ArrayList<>();
+    solve(list, 0, result);
+    return result;
   }
+
+  private static void solve(List<Integer> list, int index, List<List<Integer>> result) {
+    if (index == list.size() - 1) {
+      result.add(new ArrayList<>(list));
+    } else {
+      for (int j = index; j < list.size(); j++) {
+        Collections.swap(list, index, j);
+        solve(list, index + 1, result);
+        Collections.swap(list, index, j);
+      }
+    }
+  }
+
   @EpiTestComparator
   public static boolean comp(List<List<Integer>> expected,
                              List<List<Integer>> result) {
@@ -31,10 +47,12 @@ public class Permutations {
   }
 
   public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "Permutations.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
+    System.out.println(permutations(new ArrayList<>(List.of(1, 2, 3))));
+//    System.exit(
+//        GenericTest
+//            .runFromAnnotations(args, "Permutations.java",
+//                new Object() {
+//                }.getClass().getEnclosingClass())
+//            .ordinal());
   }
 }
