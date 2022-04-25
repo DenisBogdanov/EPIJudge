2c05508b6d3cf089e00e5ec9020f9aaa1cc0d0fd
Denis Bogdanov <bogdanov.math@gmail.com>
2022-04-25 20:29:06 +0300

    Java.

--- epi_judge_java/epi/Fibonacci.java
+++ epi_judge_java/epi/Fibonacci.java
@@ -1,20 +1,32 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
+
 public class Fibonacci {
 
   @EpiTest(testDataFile = "fibonacci.tsv")
-
   public static int fibonacci(int n) {
-    // TODO - you fill in here.
-    return -1;
+    if (n < 2) return n;
+    int a = 0;
+    int b = 1;
+
+    while (n >= 2) {
+      n--;
+      int temp = a;
+      a = b;
+      b += temp;
+    }
+
+    return b;
   }
 
   public static void main(String[] args) {
     System.exit(
         GenericTest
             .runFromAnnotations(args, "Fibonacci.java",
-                                new Object() {}.getClass().getEnclosingClass())
+                new Object() {
+                }.getClass().getEnclosingClass())
             .ordinal());
   }
 }
