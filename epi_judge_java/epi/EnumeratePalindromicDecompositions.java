074b7c31ff857ac5cca47b7316d3d02352c584d7
Denis Bogdanov <bogdanov.math@gmail.com>
2022-04-25 19:57:00 +0300

    Java.

--- epi_judge_java/epi/EnumeratePalindromicDecompositions.java
+++ epi_judge_java/epi/EnumeratePalindromicDecompositions.java
@@ -1,18 +1,46 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.EpiTestComparator;
 import epi.test_framework.GenericTest;
 import epi.test_framework.LexicographicalListComparator;
 
+import java.util.ArrayList;
 import java.util.List;
-import java.util.function.BiPredicate;
-public class EnumeratePalindromicDecompositions {
-  @EpiTest(testDataFile = "enumerate_palindromic_decompositions.tsv")
 
+public class EnumeratePalindromicDecompositions {
+
+  @EpiTest(testDataFile = "enumerate_palindromic_decompositions.tsv")
   public static List<List<String>> palindromeDecompositions(String text) {
-    // TODO - you fill in here.
-    return null;
+    List<List<String>> result = new ArrayList<>();
+    solve(text, 0, new ArrayList<>(), result);
+    return result;
   }
+
+  private static void solve(String text, int index, List<String> curr, List<List<String>> result) {
+    if (index == text.length()) {
+      result.add(new ArrayList<>(curr));
+    } else {
+      for (int i = index + 1; i <= text.length(); i++) {
+        if (isPalindrome(text.substring(index, i))) {
+          curr.add(text.substring(index, i));
+          solve(text, i, curr, result);
+          curr.remove(curr.size() - 1);
+        }
+      }
+    }
+  }
+
+  private static boolean isPalindrome(String str) {
+    for (int i = 0; i < str.length() / 2; i++) {
+      if (str.charAt(i) != str.charAt(str.length() - 1 - i)) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+
   @EpiTestComparator
   public static boolean comp(List<List<String>> expected,
                              List<List<String>> result) {
@@ -28,7 +56,8 @@ public class EnumeratePalindromicDecompositions {
     System.exit(
         GenericTest
             .runFromAnnotations(args, "EnumeratePalindromicDecompositions.java",
-                                new Object() {}.getClass().getEnclosingClass())
+                new Object() {
+                }.getClass().getEnclosingClass())
             .ordinal());
   }
 }
