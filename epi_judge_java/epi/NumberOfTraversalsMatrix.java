4502d6c52f9eb9eaecad9dd48629884de7b581ff
Denis Bogdanov <bogdanov.math@gmail.com>
2022-04-26 20:18:53 +0300

    Java.

--- epi_judge_java/epi/NumberOfTraversalsMatrix.java
+++ epi_judge_java/epi/NumberOfTraversalsMatrix.java
@@ -1,19 +1,38 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
-public class NumberOfTraversalsMatrix {
-  @EpiTest(testDataFile = "number_of_traversals_matrix.tsv")
 
+public class NumberOfTraversalsMatrix {
+
+  @EpiTest(testDataFile = "number_of_traversals_matrix.tsv")
   public static int numberOfWays(int n, int m) {
-    // TODO - you fill in here.
-    return 0;
+    int[][] grid = new int[n][m];
+
+    for (int i = 0; i < m; i++) {
+      grid[0][i] = 1;
+    }
+
+    for (int i = 0; i < n; i++) {
+      grid[i][0] = 1;
+    }
+
+    for (int i = 1; i < n; i++) {
+      for (int j = 1; j < m; j++) {
+        grid[i][j] = grid[i - 1][j] + grid[i][j - 1];
+      }
+    }
+
+
+    return grid[n - 1][m - 1];
   }
 
   public static void main(String[] args) {
     System.exit(
         GenericTest
             .runFromAnnotations(args, "NumberOfTraversalsMatrix.java",
-                                new Object() {}.getClass().getEnclosingClass())
+                new Object() {
+                }.getClass().getEnclosingClass())
             .ordinal());
   }
 }
