2609618d8cf523ea3dd4228e8490e05e6fb5e8cd
Denis Bogdanov <bogdanov.math@gmail.com>
2022-04-26 20:41:54 +0300

    Java.

--- epi_judge_java/epi/BinomialCoefficients.java
+++ epi_judge_java/epi/BinomialCoefficients.java
@@ -1,19 +1,31 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
-public class BinomialCoefficients {
-  @EpiTest(testDataFile = "binomial_coefficients.tsv")
 
+import java.util.HashMap;
+import java.util.Map;
+
+public class BinomialCoefficients {
+  private static final Map<String, Integer> cache = new HashMap<>();
+
+  @EpiTest(testDataFile = "binomial_coefficients.tsv")
   public static int computeBinomialCoefficient(int n, int k) {
-    // TODO - you fill in here.
-    return 0;
+    if (k == 0) return 1;
+    if (k == 1) return n;
+    if (n == k) return 1;
+
+    int left = cache.computeIfAbsent((n - 1) + "#" + k, key -> computeBinomialCoefficient(n - 1, k));
+    int right = cache.computeIfAbsent((n - 1) + "#" + (k - 1), key -> computeBinomialCoefficient(n - 1, k - 1));
+    return left + right;
   }
 
   public static void main(String[] args) {
     System.exit(
         GenericTest
             .runFromAnnotations(args, "BinomialCoefficients.java",
-                                new Object() {}.getClass().getEnclosingClass())
+                new Object() {
+                }.getClass().getEnclosingClass())
             .ordinal());
   }
 }
