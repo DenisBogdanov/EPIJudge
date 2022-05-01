a5f0f421d62352becfde6fa5088c99e670705df3
Denis Bogdanov <bogdanov.math@gmail.com>
2022-05-01 15:45:39 +0300

    Java.

--- epi_judge_java/epi/MakingChange.java
+++ epi_judge_java/epi/MakingChange.java
@@ -1,19 +1,29 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
-public class MakingChange {
-  @EpiTest(testDataFile = "making_change.tsv")
 
+public class MakingChange {
+  private static final int[] COINS = {100, 50, 25, 10, 5, 1};
+
+  @EpiTest(testDataFile = "making_change.tsv")
   public static int changeMaking(int cents) {
-    // TODO - you fill in here.
-    return 0;
+    int result = 0;
+
+    for (int coin : COINS) {
+      result += cents / coin;
+      cents %= coin;
+    }
+
+    return result;
   }
 
   public static void main(String[] args) {
     System.exit(
         GenericTest
             .runFromAnnotations(args, "MakingChange.java",
-                                new Object() {}.getClass().getEnclosingClass())
+                new Object() {
+                }.getClass().getEnclosingClass())
             .ordinal());
   }
 }
