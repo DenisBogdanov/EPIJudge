340f4f6374ce65d011ef694f0e666ee758d1356a
Denis Bogdanov <bogdanov.math@gmail.com>
2022-05-09 14:15:39 +0300

    Java.

--- epi_judge_java/epi/MajorityElement.java
+++ epi_judge_java/epi/MajorityElement.java
@@ -1,15 +1,35 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 
 import java.util.Iterator;
 import java.util.List;
+import java.util.Objects;
+
 public class MajorityElement {
 
   public static String majoritySearch(Iterator<String> stream) {
-    // TODO - you fill in here.
-    return "";
+    int count = 1;
+    String current = stream.next();
+
+    while (stream.hasNext()) {
+      String next = stream.next();
+      if (Objects.equals(next, current)) {
+        count++;
+      } else {
+        if (count < 1) {
+          current = next;
+          count = 1;
+        } else {
+          count--;
+        }
+      }
+    }
+
+    return current;
   }
+
   @EpiTest(testDataFile = "majority_element.tsv")
   public static String majoritySearchWrapper(List<String> stream) {
     return majoritySearch(stream.iterator());
@@ -19,7 +39,8 @@ public class MajorityElement {
     System.exit(
         GenericTest
             .runFromAnnotations(args, "MajorityElement.java",
-                                new Object() {}.getClass().getEnclosingClass())
+                new Object() {
+                }.getClass().getEnclosingClass())
             .ordinal());
   }
 }
