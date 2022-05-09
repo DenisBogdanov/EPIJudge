0fb4900765b1d55f7a16d40d4196fdbc62376c77
Denis Bogdanov <bogdanov.math@gmail.com>
2022-05-09 13:57:27 +0300

    Java.

--- epi_judge_java/epi/ThreeSum.java
+++ epi_judge_java/epi/ThreeSum.java
@@ -1,21 +1,43 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 
+import java.util.Collections;
 import java.util.List;
-public class ThreeSum {
-  @EpiTest(testDataFile = "three_sum.tsv")
 
-  public static boolean hasThreeSum(List<Integer> A, int t) {
-    // TODO - you fill in here.
-    return true;
+public class ThreeSum {
+
+  @EpiTest(testDataFile = "three_sum.tsv")
+  public static boolean hasThreeSum(List<Integer> list, int t) {
+    Collections.sort(list);
+
+    for (int i = 0; i < list.size(); i++) {
+      int target = t - list.get(i);
+
+      int left = i;
+      int right = list.size() - 1;
+
+      while (left <= right) {
+        if (list.get(left) + list.get(right) == target) {
+          return true;
+        } else if (list.get(left) + list.get(right) < target) {
+          left++;
+        } else {
+          right--;
+        }
+      }
+    }
+
+    return false;
   }
 
   public static void main(String[] args) {
     System.exit(
         GenericTest
             .runFromAnnotations(args, "ThreeSum.java",
-                                new Object() {}.getClass().getEnclosingClass())
+                new Object() {
+                }.getClass().getEnclosingClass())
             .ordinal());
   }
 }
--- epi_judge_java/epi/TwoSum.java
+++ epi_judge_java/epi/TwoSum.java
@@ -1,21 +1,38 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 
+import java.util.Collections;
 import java.util.List;
-public class TwoSum {
-  @EpiTest(testDataFile = "two_sum.tsv")
 
-  public static boolean hasTwoSum(List<Integer> A, int t) {
-    // TODO - you fill in here.
-    return true;
+public class TwoSum {
+
+  @EpiTest(testDataFile = "two_sum.tsv")
+  public static boolean hasTwoSum(List<Integer> list, int t) {
+    Collections.sort(list);
+    int left = 0;
+    int right = list.size() - 1;
+
+    while (left <= right) {
+      if (list.get(left) + list.get(right) == t) {
+        return true;
+      } else if (list.get(left) + list.get(right) < t) {
+        left++;
+      } else {
+        right--;
+      }
+    }
+
+    return false;
   }
 
   public static void main(String[] args) {
     System.exit(
         GenericTest
             .runFromAnnotations(args, "TwoSum.java",
-                                new Object() {}.getClass().getEnclosingClass())
+                new Object() {
+                }.getClass().getEnclosingClass())
             .ordinal());
   }
 }
