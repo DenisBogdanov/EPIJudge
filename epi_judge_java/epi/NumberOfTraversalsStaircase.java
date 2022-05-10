a6853be1a7dcd12516a1b47876da9815e64f2002
Denis Bogdanov <bogdanov.math@gmail.com>
2022-05-10 18:58:40 +0300

    Java.

--- epi_judge_java/epi/NumberOfTraversalsStaircase.java
+++ epi_judge_java/epi/NumberOfTraversalsStaircase.java
@@ -1,19 +1,35 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
-public class NumberOfTraversalsStaircase {
-  @EpiTest(testDataFile = "number_of_traversals_staircase.tsv")
 
+public class NumberOfTraversalsStaircase {
+
+  @EpiTest(testDataFile = "number_of_traversals_staircase.tsv")
   public static int numberOfWaysToTop(int top, int maximumStep) {
-    // TODO - you fill in here.
-    return 0;
+    return solve(top, maximumStep, new int[top + 1]);
+  }
+
+  private static int solve(int top, int maximumStep, int[] numberOfWaysDp) {
+    if (top <= 1) {
+      return 1;
+    }
+
+    if (numberOfWaysDp[top] == 0) {
+      for (int i = 1; i <= maximumStep && top - i >= 0; i++) {
+        numberOfWaysDp[top] += solve(top - i, maximumStep, numberOfWaysDp);
+      }
+    }
+
+    return numberOfWaysDp[top];
   }
 
   public static void main(String[] args) {
     System.exit(
         GenericTest
             .runFromAnnotations(args, "NumberOfTraversalsStaircase.java",
-                                new Object() {}.getClass().getEnclosingClass())
+                new Object() {
+                }.getClass().getEnclosingClass())
             .ordinal());
   }
 }
