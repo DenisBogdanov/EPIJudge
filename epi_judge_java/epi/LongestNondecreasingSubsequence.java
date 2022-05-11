c6c51dc20dab2dd50529162dff31b9792ef8af26
Denis Bogdanov <bogdanov.math@gmail.com>
2022-05-11 09:24:14 +0300

    Java.

--- epi_judge_java/epi/LongestNondecreasingSubsequence.java
+++ epi_judge_java/epi/LongestNondecreasingSubsequence.java
@@ -1,21 +1,38 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 
+import java.util.Arrays;
 import java.util.List;
-public class LongestNondecreasingSubsequence {
-  @EpiTest(testDataFile = "longest_nondecreasing_subsequence.tsv")
 
-  public static int longestNondecreasingSubsequenceLength(List<Integer> A) {
-    // TODO - you fill in here.
-    return 0;
+public class LongestNondecreasingSubsequence {
+
+  @EpiTest(testDataFile = "longest_nondecreasing_subsequence.tsv")
+  public static int longestNondecreasingSubsequenceLength(List<Integer> list) {
+    int n = list.size();
+    int[] dp = new int[n];
+
+    for (int i = 0; i < n; i++) {
+      int max = 0;
+      for (int j = 0; j < i; j++) {
+        if (list.get(j) <= list.get(i) && dp[j] > max) {
+          max = dp[j];
+        }
+      }
+
+      dp[i] = 1 + max;
+    }
+
+    return Arrays.stream(dp).max().orElseThrow();
   }
 
   public static void main(String[] args) {
     System.exit(
         GenericTest
             .runFromAnnotations(args, "LongestNondecreasingSubsequence.java",
-                                new Object() {}.getClass().getEnclosingClass())
+                new Object() {
+                }.getClass().getEnclosingClass())
             .ordinal());
   }
 }
