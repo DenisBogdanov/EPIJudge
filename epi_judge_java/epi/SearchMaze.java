5bfab23c71cf3146258244f55cf8deac6a4b96e6
Denis Bogdanov <bogdanov.math@gmail.com>
2022-05-12 20:33:11 +0300

    Java.

--- epi_judge_java/epi/SearchMaze.java
+++ epi_judge_java/epi/SearchMaze.java
@@ -1,21 +1,74 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.EpiUserType;
 import epi.test_framework.GenericTest;
 import epi.test_framework.TestFailure;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-public class SearchMaze {
-  @EpiUserType(ctorParams = {int.class, int.class})
+import java.util.*;
 
+public class SearchMaze {
+  private static final int[][] DIRECTIONS = new int[][]{{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+
+  public enum Color {WHITE, BLACK}
+
+  public static List<Coordinate> searchMaze(List<List<Color>> maze, Coordinate src, Coordinate dest) {
+    int rows = maze.size();
+    int columns = maze.get(0).size();
+
+    Coordinate[][] prev = new Coordinate[rows][columns];
+
+    Queue<Coordinate> q = new ArrayDeque<>();
+    q.offer(src);
+
+    while (!q.isEmpty()) {
+      int size = q.size();
+      for (int i = 0; i < size; i++) {
+        Coordinate polled = q.poll();
+
+        if (polled.equals(dest)) {
+          List<Coordinate> path = new ArrayList<>();
+          path.add(dest);
+
+          while (!dest.equals(src)) {
+            dest = prev[dest.x][dest.y];
+            path.add(dest);
+          }
+
+          Collections.reverse(path);
+          return path;
+        }
+
+        for (int[] dir : DIRECTIONS) {
+          int nextX = polled.x + dir[0];
+          int nextY = polled.y + dir[1];
+
+          if (nextX < 0 || nextX == rows || nextY < 0 || nextY == columns) continue;
+
+          if (maze.get(nextX).get(nextY) == Color.WHITE) {
+            if (prev[nextX][nextY] != null) continue;
+            prev[nextX][nextY] = polled;
+            q.offer(new Coordinate(nextX, nextY));
+          }
+        }
+      }
+    }
+
+    return List.of();
+  }
+
+  @EpiUserType(ctorParams = {int.class, int.class})
   public static class Coordinate {
     public int x, y;
 
     public Coordinate(int x, int y) {
       this.x = x;
       this.y = y;
+    }
+
+    @Override
+    public int hashCode() {
+      return Objects.hash(x, y);
     }
 
     @Override
@@ -28,25 +81,23 @@ public class SearchMaze {
         return false;
       }
 
-      Coordinate that = (Coordinate)o;
-      if (x != that.x || y != that.y) {
-        return false;
-      }
-      return true;
+      Coordinate that = (Coordinate) o;
+      return x == that.x && y == that.y;
+    }
+
+    @Override
+    public String toString() {
+      return "Coordinate{" +
+          "x=" + x +
+          ", y=" + y +
+          '}';
     }
   }
 
-  public enum Color { WHITE, BLACK }
-
-  public static List<Coordinate> searchMaze(List<List<Color>> maze,
-                                            Coordinate s, Coordinate e) {
-    // TODO - you fill in here.
-    return Collections.emptyList();
-  }
   public static boolean pathElementIsFeasible(List<List<Integer>> maze,
                                               Coordinate prev, Coordinate cur) {
     if (!(0 <= cur.x && cur.x < maze.size() && 0 <= cur.y &&
-          cur.y < maze.get(cur.x).size() && maze.get(cur.x).get(cur.y) == 0)) {
+        cur.y < maze.get(cur.x).size() && maze.get(cur.x).get(cur.y) == 0)) {
       return false;
     }
     return cur.x == prev.x + 1 && cur.y == prev.y ||
@@ -89,7 +140,8 @@ public class SearchMaze {
     System.exit(
         GenericTest
             .runFromAnnotations(args, "SearchMaze.java",
-                                new Object() {}.getClass().getEnclosingClass())
+                new Object() {
+                }.getClass().getEnclosingClass())
             .ordinal());
   }
 }
