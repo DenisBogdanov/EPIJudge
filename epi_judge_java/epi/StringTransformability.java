5d07a4c7ad98c66065c99e65cadd319a183fcd3a
Denis Bogdanov <bogdanov.math@gmail.com>
2022-05-13 18:04:56 +0300

    Java.

--- epi_judge_java/epi/StringTransformability.java
+++ epi_judge_java/epi/StringTransformability.java
@@ -9,6 +9,47 @@ public class StringTransformability {
 
   @EpiTest(testDataFile = "string_transformability.tsv")
   public static int transformString(Set<String> dict, String s, String t) {
+//    return verySlowSolution(dict, s, t);
+
+
+    Queue<StringWithDistance> q = new LinkedList<>();
+    dict.remove(s);
+    q.offer(new StringWithDistance(s, 0));
+
+    StringWithDistance strWithDist;
+    while ((strWithDist = q.poll()) != null) {
+      if (strWithDist.candidate.equals(t)) {
+        return strWithDist.distance;
+      }
+
+      String candidate = strWithDist.candidate;
+
+      for (int i = 0; i < candidate.length(); i++) {
+        String start = candidate.substring(0, i);
+        String end = i + 1 < candidate.length() ? candidate.substring(i + 1) : "";
+        for (int j = 0; j < 26; j++) {
+          String modStr = start + (char) ('a' + j) + end;
+          if (dict.remove(modStr)) {
+            q.add(new StringWithDistance(modStr, strWithDist.distance + 1));
+          }
+        }
+      }
+    }
+
+    return -1;
+  }
+
+  private static class StringWithDistance {
+    final String candidate;
+    final int distance;
+
+    StringWithDistance(String candidate, int distance) {
+      this.candidate = candidate;
+      this.distance = distance;
+    }
+  }
+
+  private static int verySlowSolution(Set<String> dict, String s, String t) {
     Map<String, List<String>> adjList = new HashMap<>();
     for (String a : dict) {
       List<String> neighbours = new ArrayList<>();
