1a13b081d3a597ac2e01b4d41d01fa8bb1fbc071
Denis Bogdanov <bogdanov.math@gmail.com>
2022-05-18 10:25:05 +0300

    Python.

--- epi_judge_python/is_number_palindromic.py
+++ epi_judge_python/is_number_palindromic.py
@@ -1,8 +1,23 @@
+import math
+
 from test_framework import generic_test
 
 
 def is_palindrome_number(x: int) -> bool:
-    # TODO - you fill in here.
+    if x <= 0:
+        return x == 0
+
+    num_digits = math.floor(math.log10(x)) + 1
+    most_significant_digit_mask = 10 ** (num_digits - 1)
+
+    for i in range(num_digits // 2):
+        if x // most_significant_digit_mask != x % 10:
+            return False
+
+        x %= most_significant_digit_mask
+        x //= 10
+        most_significant_digit_mask //= 100
+
     return True
 
 
