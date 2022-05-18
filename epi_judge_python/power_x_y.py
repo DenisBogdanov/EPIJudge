4a317a08473f93be01a6af36dc419a76f245f7d0
Denis Bogdanov <bogdanov.math@gmail.com>
2022-05-18 09:49:12 +0300

    Python.

--- epi_judge_python/power_x_y.py
+++ epi_judge_python/power_x_y.py
@@ -2,8 +2,15 @@ from test_framework import generic_test
 
 
 def power(x: float, y: int) -> float:
-    # TODO - you fill in here.
-    return 0.0
+    if y < 0:
+        x, y = 1 / x, -y
+    if y == 0:
+        return 1
+    if y & 1:
+        return x * power(x, y - 1)
+    else:
+        half_power = power(x, y >> 1)
+        return half_power * half_power
 
 
 if __name__ == '__main__':
