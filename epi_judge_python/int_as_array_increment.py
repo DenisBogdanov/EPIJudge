4e7cd6c0292062b62582f673c73f8be7e59a1471
Denis Bogdanov <bogdanov.math@gmail.com>
2022-05-21 17:16:58 +0300

    Python.

--- epi_judge_python/int_as_array_increment.py
+++ epi_judge_python/int_as_array_increment.py
@@ -3,9 +3,19 @@ from typing import List
 from test_framework import generic_test
 
 
-def plus_one(A: List[int]) -> List[int]:
-    # TODO - you fill in here.
-    return []
+def plus_one(arr: List[int]) -> List[int]:
+    arr[len(arr) - 1] += 1
+    for i in reversed(range(1, len(arr))):
+        if arr[i] != 10:
+            return arr
+        arr[i] = 0
+        arr[i - 1] += 1
+
+    if arr[0] == 10:
+        arr[0] = 1
+        arr.append(0)
+
+    return arr
 
 
 if __name__ == '__main__':
