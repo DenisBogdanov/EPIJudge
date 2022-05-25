16650a913985e8911c2d983ca6820ee4406a8351
Denis Bogdanov <bogdanov.math@gmail.com>
2022-05-25 20:10:50 +0300

    Python.

--- epi_judge_python/advance_by_offsets.py
+++ epi_judge_python/advance_by_offsets.py
@@ -3,9 +3,16 @@ from typing import List
 from test_framework import generic_test
 
 
-def can_reach_end(A: List[int]) -> bool:
-    # TODO - you fill in here.
-    return True
+def can_reach_end(arr: List[int]) -> bool:
+    next_furthest = 0
+    i = 0
+    last_index = len(arr) - 1
+
+    while i <= next_furthest < last_index:
+        next_furthest = max(next_furthest, i + arr[i])
+        i += 1
+
+    return next_furthest >= last_index
 
 
 if __name__ == '__main__':
