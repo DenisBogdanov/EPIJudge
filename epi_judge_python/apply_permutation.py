44963f725502318ebbad1c6f5eb45c833d0c34b9
Denis Bogdanov <bogdanov.math@gmail.com>
2022-05-25 21:51:37 +0300

    Python.

--- epi_judge_python/apply_permutation.py
+++ epi_judge_python/apply_permutation.py
@@ -3,9 +3,16 @@ from typing import List
 from test_framework import generic_test
 
 
-def apply_permutation(perm: List[int], A: List[int]) -> None:
-    # TODO - you fill in here.
-    return
+def apply_permutation(perm: List[int], arr: List[int]) -> None:
+    for i in range(len(arr)):
+        next_index = i
+        while perm[next_index] >= 0:
+            arr[i], arr[perm[next_index]] = arr[perm[next_index]], arr[i]
+            temp = perm[next_index]
+            perm[next_index] -= len(perm)
+            next_index = temp
+
+    perm[:] = [a + len(perm) for a in perm]
 
 
 def apply_permutation_wrapper(perm, A):
