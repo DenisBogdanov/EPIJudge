741595a8b999f0d524d8c6591957086b573c1b26
Denis Bogdanov <bogdanov.math@gmail.com>
2022-05-25 20:47:54 +0300

    Python.

--- epi_judge_python/buy_and_sell_stock_twice.py
+++ epi_judge_python/buy_and_sell_stock_twice.py
@@ -4,8 +4,24 @@ from test_framework import generic_test
 
 
 def buy_and_sell_stock_twice(prices: List[float]) -> float:
-    # TODO - you fill in here.
-    return 0.0
+    diff = []
+
+    result = 0.0
+
+    cur_min = prices[0]
+    for price in prices:
+        if price < cur_min:
+            cur_min = price
+        result = max(result, price - cur_min)
+        diff.append(result)
+
+    cur_max = prices[-1]
+    for i in range(len(prices) - 1, -1, -1):
+        if prices[i] > cur_max:
+            cur_max = prices[i]
+        result = max(result, cur_max - prices[i] + diff[i])
+
+    return result
 
 
 if __name__ == '__main__':
