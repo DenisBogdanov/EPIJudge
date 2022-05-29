b9507f48330f9a7f73602d8d3bfb69278b760428
Denis Bogdanov <bogdanov.math@gmail.com>
2022-05-29 17:28:25 +0300

    Python.

--- epi_judge_python/next_permutation.py
+++ epi_judge_python/next_permutation.py
@@ -4,8 +4,29 @@ from test_framework import generic_test
 
 
 def next_permutation(perm: List[int]) -> List[int]:
-    # TODO - you fill in here.
-    return []
+    inversion_point = len(perm) - 1
+    last_index = inversion_point
+
+    while inversion_point > 0 and perm[inversion_point] <= perm[inversion_point - 1]:
+        inversion_point -= 1
+
+    if inversion_point == 0:
+        return []
+
+    right = last_index
+    while perm[right] <= perm[inversion_point - 1]:
+        right -= 1
+
+    perm[inversion_point - 1], perm[right] = perm[right], perm[inversion_point - 1]
+
+    right = last_index
+
+    while inversion_point < right:
+        perm[inversion_point], perm[right] = perm[right], perm[inversion_point]
+        inversion_point += 1
+        right -= 1
+
+    return perm
 
 
 if __name__ == '__main__':
