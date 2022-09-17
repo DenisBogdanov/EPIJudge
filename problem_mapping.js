50d9357db9a2d5650ceab7578a12d8978bac4f8b
Denis Bogdanov <bogdanov.math@gmail.com>
2022-09-17 12:27:59 +0300

    Java.

--- problem_mapping.js
+++ problem_mapping.js
@@ -65,7 +65,7 @@ problem_mapping = {
                 "passed": 0,
                 "total": 10000
             },
-            "Python: closest_int_same_weight.py": { 
+            "Python: closest_int_same_weight.py": {
                 "passed": 0,
                 "total": 10000
             }
@@ -2462,7 +2462,7 @@ problem_mapping = {
                 "total": 101
             },
             "Java: SearchMaze.java": {
-                "passed": 0,
+                "passed": 101,
                 "total": 101
             },
             "Python: search_maze.py": {
