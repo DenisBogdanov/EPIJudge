53bd70b5320ed31d4a86095a5a01f93e8e45684f
Denis Bogdanov <bogdanov.math@gmail.com>
2022-11-02 17:22:22 +0400

    Java.

--- epi_judge_java/epi/ReverseSublist.java
+++ epi_judge_java/epi/ReverseSublist.java
@@ -1,20 +1,40 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
+
 public class ReverseSublist {
-  @EpiTest(testDataFile = "reverse_sublist.tsv")
 
-  public static ListNode<Integer> reverseSublist(ListNode<Integer> L, int start,
-                                                 int finish) {
-    // TODO - you fill in here.
-    return null;
-  }
+    @EpiTest(testDataFile = "reverse_sublist.tsv")
+    public static ListNode<Integer> reverseSublist(ListNode<Integer> head, int start, int finish) {
+        if (start == finish) return head;
 
-  public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "ReverseSublist.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
-  }
+        ListNode<Integer> dummy = new ListNode<>(-1, head);
+        ListNode<Integer> runner = dummy;
+
+        for (int i = 1; i < start; i++) {
+            runner = runner.next;
+        }
+
+        ListNode<Integer> prev = runner;
+        runner = runner.next;
+
+        for (int i = start; i < finish; i++) {
+            ListNode<Integer> next = runner.next;
+            runner.next = next.next;
+            next.next = runner;
+            prev.next = next;
+        }
+
+        return dummy.next;
+    }
+
+    public static void main(String[] args) {
+        System.exit(
+                GenericTest
+                        .runFromAnnotations(args, "ReverseSublist.java",
+                                new Object() {
+                                }.getClass().getEnclosingClass())
+                        .ordinal());
+    }
 }
