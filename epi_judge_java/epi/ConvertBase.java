8b038db08a518567fd5366352751e6201d4cd98a
Denis Bogdanov <bogdanov.math@gmail.com>
2022-11-02 12:10:00 +0400

    Java.

--- epi_judge_java/epi/ConvertBase.java
+++ epi_judge_java/epi/ConvertBase.java
@@ -1,9 +1,11 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
-public class ConvertBase {
-  @EpiTest(testDataFile = "convert_base.tsv")
 
+public class ConvertBase {
+
+  @EpiTest(testDataFile = "convert_base.tsv")
   public static String convertBase(String numAsString, int b1, int b2) {
     // TODO - you fill in here.
     return "";
@@ -11,9 +13,10 @@ public class ConvertBase {
 
   public static void main(String[] args) {
     System.exit(
-        GenericTest
-            .runFromAnnotations(args, "ConvertBase.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
+            GenericTest
+                    .runFromAnnotations(args, "ConvertBase.java",
+                            new Object() {
+                            }.getClass().getEnclosingClass())
+                    .ordinal());
   }
 }
--- epi_judge_java/epi/StringIntegerInterconversion.java
+++ epi_judge_java/epi/StringIntegerInterconversion.java
@@ -1,32 +1,59 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 import epi.test_framework.TestFailure;
+
 public class StringIntegerInterconversion {
 
-  public static String intToString(int x) {
-    // TODO - you fill in here.
-    return "0";
-  }
-  public static int stringToInt(String s) {
-    // TODO - you fill in here.
-    return 0;
-  }
-  @EpiTest(testDataFile = "string_integer_interconversion.tsv")
-  public static void wrapper(int x, String s) throws TestFailure {
-    if (Integer.parseInt(intToString(x)) != x) {
-      throw new TestFailure("Int to string conversion failed");
-    }
-    if (stringToInt(s) != x) {
-      throw new TestFailure("String to int conversion failed");
-    }
-  }
+    public static String intToString(int x) {
+        if (x == 0) return "0";
+        if (x == Integer.MIN_VALUE) return "-2147483648";
 
-  public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "StringIntegerInterconversion.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
-  }
+        boolean isNegative = x < 0;
+        x = Math.abs(x);
+
+        StringBuilder sb = new StringBuilder();
+        while (x > 0) {
+            sb.append(x % 10);
+            x /= 10;
+        }
+
+        return (isNegative ? '-' : "") + sb.reverse().toString();
+    }
+
+    public static int stringToInt(String s) {
+        int index = 0;
+        int sign = 1;
+        if (s.charAt(index) == '-') {
+            index++;
+            sign = -1;
+        } else if (s.charAt(index) == '+') {
+            index++;
+        }
+
+        int result = 0;
+        while (index < s.length()) {
+            result *= 10;
+            result += (s.charAt(index) - '0');
+            index++;
+        }
+
+        return sign * result;
+    }
+
+    @EpiTest(testDataFile = "string_integer_interconversion.tsv")
+    public static void wrapper(int x, String s) throws TestFailure {
+        if (Integer.parseInt(intToString(x)) != x) {
+            throw new TestFailure("Int to string conversion failed");
+        }
+        if (stringToInt(s) != x) {
+            throw new TestFailure("String to int conversion failed");
+        }
+    }
+
+    public static void main(String[] args) {
+        System.exit(GenericTest.runFromAnnotations(args, "StringIntegerInterconversion.java", new Object() {
+        }.getClass().getEnclosingClass()).ordinal());
+    }
 }
