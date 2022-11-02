aef27637ba8ddfd5c78a9008657a9071af92ae4f
Denis Bogdanov <bogdanov.math@gmail.com>
2022-11-02 11:21:59 +0400

    Java.

--- epi_judge_java/epi/BuyAndSellStock.java
+++ epi_judge_java/epi/BuyAndSellStock.java
@@ -1,20 +1,31 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 
 import java.util.List;
-public class BuyAndSellStock {
-  @EpiTest(testDataFile = "buy_and_sell_stock.tsv")
-  public static double computeMaxProfit(List<Double> prices) {
-    // TODO - you fill in here.
-    return 0.0;
-  }
 
-  public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "BuyAndSellStock.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
-  }
+public class BuyAndSellStock {
+
+    @EpiTest(testDataFile = "buy_and_sell_stock.tsv")
+    public static double computeMaxProfit(List<Double> prices) {
+        double currMin = prices.get(0);
+        double result = 0;
+
+        for (int i = 1; i < prices.size(); i++) {
+            result = Math.max(result, prices.get(i) - currMin);
+            currMin = Math.min(currMin, prices.get(i));
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.exit(
+                GenericTest
+                        .runFromAnnotations(args, "BuyAndSellStock.java",
+                                new Object() {
+                                }.getClass().getEnclosingClass())
+                        .ordinal());
+    }
 }
