5e0b538a3a2c2aa380e4ffeff15753e75c78530c
Denis Bogdanov <bogdanov.math@gmail.com>
2022-11-02 13:31:54 +0400

    Java.

--- epi_judge_java/epi/ConvertBase.java
+++ epi_judge_java/epi/ConvertBase.java
@@ -5,18 +5,14 @@ import epi.test_framework.GenericTest;
 
 public class ConvertBase {
 
-  @EpiTest(testDataFile = "convert_base.tsv")
-  public static String convertBase(String numAsString, int b1, int b2) {
-    // TODO - you fill in here.
-    return "";
-  }
+    @EpiTest(testDataFile = "convert_base.tsv")
+    public static String convertBase(String numAsString, int b1, int b2) {
+        // TODO - you fill in here.
+        return "";
+    }
 
-  public static void main(String[] args) {
-    System.exit(
-            GenericTest
-                    .runFromAnnotations(args, "ConvertBase.java",
-                            new Object() {
-                            }.getClass().getEnclosingClass())
-                    .ordinal());
-  }
+    public static void main(String[] args) {
+        System.exit(GenericTest.runFromAnnotations(args, "ConvertBase.java", new Object() {
+        }.getClass().getEnclosingClass()).ordinal());
+    }
 }
--- epi_judge_java/epi/SortedListsMerge.java
+++ epi_judge_java/epi/SortedListsMerge.java
@@ -1,20 +1,44 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
-public class SortedListsMerge {
-  @EpiTest(testDataFile = "sorted_lists_merge.tsv")
-  //@include
-  public static ListNode<Integer> mergeTwoSortedLists(ListNode<Integer> L1,
-                                                      ListNode<Integer> L2) {
-    // TODO - you fill in here.
-    return null;
-  }
 
-  public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "SortedListsMerge.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
-  }
+public class SortedListsMerge {
+
+    @EpiTest(testDataFile = "sorted_lists_merge.tsv")
+    public static ListNode<Integer> mergeTwoSortedLists(ListNode<Integer> head1, ListNode<Integer> head2) {
+        if (head1 == null) return head2;
+        if (head2 == null) return head1;
+
+        ListNode<Integer> dummy = new ListNode<>(-1, null);
+        ListNode<Integer> runner = dummy;
+        while (head1 != null && head2 != null) {
+            if (head1.data < head2.data) {
+                runner.next = head1;
+                head1 = head1.next;
+            } else {
+                runner.next = head2;
+                head2 = head2.next;
+            }
+
+            runner = runner.next;
+        }
+
+        if (head1 == null) {
+            runner.next = head2;
+        } else {
+            runner.next = head1;
+        }
+
+        return dummy.next;
+    }
+
+    public static void main(String[] args) {
+        System.exit(
+                GenericTest
+                        .runFromAnnotations(args, "SortedListsMerge.java",
+                                new Object() {
+                                }.getClass().getEnclosingClass())
+                        .ordinal());
+    }
 }
