25161ef074f312f9da4f792a9ba57a9269b0e68a
Denis Bogdanov <bogdanov.math@gmail.com>
2022-11-03 10:42:43 +0400

    Java.

--- epi_judge_java/epi/IntersectSortedArrays.java
+++ epi_judge_java/epi/IntersectSortedArrays.java
@@ -1,22 +1,35 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+
 public class IntersectSortedArrays {
-  @EpiTest(testDataFile = "intersect_sorted_arrays.tsv")
 
-  public static List<Integer> intersectTwoSortedArrays(List<Integer> A,
-                                                       List<Integer> B) {
-    // TODO - you fill in here.
-    return null;
-  }
+    @EpiTest(testDataFile = "intersect_sorted_arrays.tsv")
+    public static List<Integer> intersectTwoSortedArrays(List<Integer> nums1, List<Integer> nums2) {
+        List<Integer> result = new ArrayList<>();
+        if (nums1.isEmpty() || nums2.isEmpty()) return result;
 
-  public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "IntersectSortedArrays.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
-  }
+        if (Collections.binarySearch(nums2, nums1.get(0)) >= 0) result.add(nums1.get(0));
+
+        for (int i = 1; i < nums1.size(); i++) {
+            if (nums1.get(i).equals(nums1.get(i - 1))) continue;
+            if (Collections.binarySearch(nums2, nums1.get(i)) >= 0) result.add(nums1.get(i));
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.exit(
+                GenericTest
+                        .runFromAnnotations(args, "IntersectSortedArrays.java",
+                                new Object() {
+                                }.getClass().getEnclosingClass())
+                        .ordinal());
+    }
 }
