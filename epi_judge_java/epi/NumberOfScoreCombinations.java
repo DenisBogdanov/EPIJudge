cd814cd5924c3ef1685b54ca535de72ee8ba78fe
Denis Bogdanov <bogdanov.math@gmail.com>
2022-11-03 11:20:03 +0400

    Java.

--- epi_judge_java/epi/NumberOfScoreCombinations.java
+++ epi_judge_java/epi/NumberOfScoreCombinations.java
@@ -1,23 +1,36 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 
 import java.util.List;
+
 public class NumberOfScoreCombinations {
-  @EpiTest(testDataFile = "number_of_score_combinations.tsv")
 
-  public static int
-  numCombinationsForFinalScore(int finalScore,
-                               List<Integer> individualPlayScores) {
-    // TODO - you fill in here.
-    return 0;
-  }
+    @EpiTest(testDataFile = "number_of_score_combinations.tsv")
+    public static int numCombinationsForFinalScore(int finalScore, List<Integer> individualPlayScores) {
+        int n = individualPlayScores.size();
+        int[][] scoreToAmountOfCombinationsCache = new int[n][finalScore + 1];
 
-  public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "NumberOfScoreCombinations.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
-  }
+        for (int i = 0; i < n; i++) {
+            scoreToAmountOfCombinationsCache[i][0] = 1;
+            for (int j = 1; j <= finalScore; j++) {
+                scoreToAmountOfCombinationsCache[i][j] = i > 0 ? scoreToAmountOfCombinationsCache[i - 1][j] : 0;
+                if (j >= individualPlayScores.get(i)) {
+                    scoreToAmountOfCombinationsCache[i][j] += scoreToAmountOfCombinationsCache[i][j - individualPlayScores.get(i)];
+                }
+            }
+        }
+
+        return scoreToAmountOfCombinationsCache[n - 1][finalScore];
+    }
+
+    public static void main(String[] args) {
+        System.exit(
+                GenericTest
+                        .runFromAnnotations(args, "NumberOfScoreCombinations.java",
+                                new Object() {
+                                }.getClass().getEnclosingClass())
+                        .ordinal());
+    }
 }
