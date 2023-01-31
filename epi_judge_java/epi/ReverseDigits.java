52a54d77860aacbb82ed1a01832d121d41692af0
Denis Bogdanov <bogdanov.math@gmail.com>
2023-01-31 08:35:46 +0400

    Java.

--- epi_judge_java/epi/ReverseDigits.java
+++ epi_judge_java/epi/ReverseDigits.java
@@ -1,18 +1,37 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
-public class ReverseDigits {
-  @EpiTest(testDataFile = "reverse_digits.tsv")
-  public static long reverse(int x) {
-    // TODO - you fill in here.
-    return 0;
-  }
 
-  public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "ReverseDigits.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
-  }
+public class ReverseDigits {
+
+    @EpiTest(testDataFile = "reverse_digits.tsv")
+    public static long reverse(int x) {
+        boolean isNegative = false;
+
+        if (x < 0) {
+            isNegative = true;
+            x = -x;
+        }
+
+        long result = 0;
+
+        while (x > 0) {
+            result *= 10;
+            result += x % 10;
+
+            x /= 10;
+        }
+
+        return isNegative ? -result : result;
+    }
+
+    public static void main(String[] args) {
+        System.exit(
+                GenericTest
+                        .runFromAnnotations(args, "ReverseDigits.java",
+                                new Object() {
+                                }.getClass().getEnclosingClass())
+                        .ordinal());
+    }
 }
