43d6f24fee4abc9e19a0c9890e2669388b39ff13
Denis Bogdanov <bogdanov.math@gmail.com>
2023-01-31 08:32:43 +0400

    Java.

--- epi_judge_java/epi/PowerXY.java
+++ epi_judge_java/epi/PowerXY.java
@@ -1,18 +1,39 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
-public class PowerXY {
-  @EpiTest(testDataFile = "power_x_y.tsv")
-  public static double power(double x, int y) {
-    // TODO - you fill in here.
-    return 0.0;
-  }
 
-  public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "PowerXY.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
-  }
+public class PowerXY {
+
+    @EpiTest(testDataFile = "power_x_y.tsv")
+    public static double power(double x, int y) {
+        if (y < 0) {
+            x = 1 / x;
+            y = -y;
+        }
+
+        double result = 1.0;
+
+        double multiplier = x;
+
+        while (y > 0) {
+            if (y % 2 != 0) {
+                result *= multiplier;
+            }
+
+            multiplier *= multiplier;
+            y /= 2;
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.exit(
+                GenericTest
+                        .runFromAnnotations(args, "PowerXY.java",
+                                new Object() {
+                                }.getClass().getEnclosingClass())
+                        .ordinal());
+    }
 }
