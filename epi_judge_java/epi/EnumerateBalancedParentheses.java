86a4cf5fbce9a7fbdc2ccd889bee33b1a12ff486
Denis Bogdanov <bogdanov.math@gmail.com>
2023-02-05 11:44:43 +0400

    Java.

--- epi_judge_java/epi/EnumerateBalancedParentheses.java
+++ epi_judge_java/epi/EnumerateBalancedParentheses.java
@@ -13,23 +13,23 @@ public class EnumerateBalancedParentheses {
     @EpiTest(testDataFile = "enumerate_balanced_parentheses.tsv")
     public static List<String> generateBalancedParentheses(int numPairs) {
         List<String> result = new ArrayList<>();
-        recur(numPairs, numPairs, new StringBuilder(), result);
+        recur(numPairs, 0, 0, new StringBuilder(), result);
         return result;
     }
 
-    private static void recur(int openedCountNeeded, int closedCountNeeded, StringBuilder sb, List<String> result) {
-        if (openedCountNeeded == 0 && closedCountNeeded == 0) {
+    private static void recur(int numPairs, int openedCount, int closedCount, StringBuilder sb, List<String> result) {
+        if (openedCount == numPairs && closedCount == numPairs) {
             result.add(sb.toString());
         } else {
-            if (openedCountNeeded > 0) {
+            if (openedCount < numPairs) {
                 sb.append("(");
-                recur(openedCountNeeded - 1, closedCountNeeded, sb, result);
+                recur(numPairs, openedCount + 1, closedCount, sb, result);
                 sb.setLength(sb.length() - 1);
             }
 
-            if (openedCountNeeded < closedCountNeeded) {
+            if (openedCount > closedCount) {
                 sb.append(")");
-                recur(openedCountNeeded, closedCountNeeded - 1, sb, result);
+                recur(numPairs, openedCount, closedCount + 1, sb, result);
                 sb.setLength(sb.length() - 1);
             }
         }
