47dbfdc70d6fd2b351c1c26ccb7ae4dbab9a8c73
Denis Bogdanov <bogdanov.math@gmail.com>
2023-02-05 11:42:40 +0400

    Java.

--- epi_judge_java/epi/EnumerateBalancedParentheses.java
+++ epi_judge_java/epi/EnumerateBalancedParentheses.java
@@ -1,33 +1,56 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.EpiTestComparator;
 import epi.test_framework.GenericTest;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.function.BiPredicate;
+
 public class EnumerateBalancedParentheses {
-  @EpiTest(testDataFile = "enumerate_balanced_parentheses.tsv")
 
-  public static List<String> generateBalancedParentheses(int numPairs) {
-    // TODO - you fill in here.
-    return null;
-  }
-  @EpiTestComparator
-  public static boolean comp(List<String> expected, List<String> result) {
-    if (result == null) {
-      return false;
+    @EpiTest(testDataFile = "enumerate_balanced_parentheses.tsv")
+    public static List<String> generateBalancedParentheses(int numPairs) {
+        List<String> result = new ArrayList<>();
+        recur(numPairs, numPairs, new StringBuilder(), result);
+        return result;
     }
-    Collections.sort(expected);
-    Collections.sort(result);
-    return expected.equals(result);
-  }
 
-  public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "EnumerateBalancedParentheses.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
-  }
+    private static void recur(int openedCountNeeded, int closedCountNeeded, StringBuilder sb, List<String> result) {
+        if (openedCountNeeded == 0 && closedCountNeeded == 0) {
+            result.add(sb.toString());
+        } else {
+            if (openedCountNeeded > 0) {
+                sb.append("(");
+                recur(openedCountNeeded - 1, closedCountNeeded, sb, result);
+                sb.setLength(sb.length() - 1);
+            }
+
+            if (openedCountNeeded < closedCountNeeded) {
+                sb.append(")");
+                recur(openedCountNeeded, closedCountNeeded - 1, sb, result);
+                sb.setLength(sb.length() - 1);
+            }
+        }
+    }
+
+    @EpiTestComparator
+    public static boolean comp(List<String> expected, List<String> result) {
+        if (result == null) {
+            return false;
+        }
+        Collections.sort(expected);
+        Collections.sort(result);
+        return expected.equals(result);
+    }
+
+    public static void main(String[] args) {
+        System.exit(
+                GenericTest
+                        .runFromAnnotations(args, "EnumerateBalancedParentheses.java",
+                                new Object() {
+                                }.getClass().getEnclosingClass())
+                        .ordinal());
+    }
 }
