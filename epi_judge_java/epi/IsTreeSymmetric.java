e018dcaa0018a02864542b7cc297e9aa28bff0d0
Denis Bogdanov <bogdanov.math@gmail.com>
2023-02-06 08:18:35 +0400

    Java.

--- epi_judge_java/epi/IsTreeSymmetric.java
+++ epi_judge_java/epi/IsTreeSymmetric.java
@@ -1,19 +1,31 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
+
 public class IsTreeSymmetric {
-  @EpiTest(testDataFile = "is_tree_symmetric.tsv")
 
-  public static boolean isSymmetric(BinaryTreeNode<Integer> tree) {
-    // TODO - you fill in here.
-    return true;
-  }
+    @EpiTest(testDataFile = "is_tree_symmetric.tsv")
+    public static boolean isSymmetric(BinaryTreeNode<Integer> tree) {
+        if (tree == null) return true;
+        return isSymmetric(tree.left, tree.right);
+    }
 
-  public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "IsTreeSymmetric.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
-  }
+    private static boolean isSymmetric(BinaryTreeNode<Integer> left, BinaryTreeNode<Integer> right) {
+        if (left == null && right == null) return true;
+        if (left == null || right == null) return false;
+
+        return left.data.equals(right.data)
+                && isSymmetric(left.left, right.right)
+                && isSymmetric(left.right, right.left);
+    }
+
+    public static void main(String[] args) {
+        System.exit(
+                GenericTest
+                        .runFromAnnotations(args, "IsTreeSymmetric.java",
+                                new Object() {
+                                }.getClass().getEnclosingClass())
+                        .ordinal());
+    }
 }
