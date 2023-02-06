b0308c19568b2dd33433b3e8495c780abe9b2f95
Denis Bogdanov <bogdanov.math@gmail.com>
2023-02-06 08:13:55 +0400

    Java.

--- epi_judge_java/epi/IsTreeBalanced.java
+++ epi_judge_java/epi/IsTreeBalanced.java
@@ -3,7 +3,11 @@ package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class IsTreeBalanced {
+  private static final Map<BinaryTreeNode<Integer>, Integer> NODE_TO_HEIGHT_MAP = new HashMap<>();
 
   @EpiTest(testDataFile = "is_tree_balanced.tsv")
   public static boolean isBalanced(BinaryTreeNode<Integer> tree) {
@@ -14,8 +18,14 @@ public class IsTreeBalanced {
   }
 
   private static int height(BinaryTreeNode<Integer> node) {
+    Integer height = NODE_TO_HEIGHT_MAP.get(node);
+    if (height != null) return height;
+
     if (node == null) return 0;
-    return 1 + Math.max(height(node.left), height(node.right));
+    int result = 1 + Math.max(height(node.left), height(node.right));
+    NODE_TO_HEIGHT_MAP.put(node, result);
+
+    return result;
   }
 
   public static void main(String[] args) {
