b7f914febff25e6a003362b7b4bced41ac596bbf
Denis Bogdanov <bogdanov.math@gmail.com>
2023-02-06 08:08:55 +0400

    Java.

--- epi_judge_java/epi/IsTreeBalanced.java
+++ epi_judge_java/epi/IsTreeBalanced.java
@@ -1,20 +1,29 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
+
 public class IsTreeBalanced {
 
   @EpiTest(testDataFile = "is_tree_balanced.tsv")
-
   public static boolean isBalanced(BinaryTreeNode<Integer> tree) {
-    // TODO - you fill in here.
-    return true;
+    if (tree == null) return true;
+    return Math.abs(height(tree.left) - height(tree.right)) <= 1
+            && isBalanced(tree.left)
+            && isBalanced(tree.right);
+  }
+
+  private static int height(BinaryTreeNode<Integer> node) {
+    if (node == null) return 0;
+    return 1 + Math.max(height(node.left), height(node.right));
   }
 
   public static void main(String[] args) {
     System.exit(
-        GenericTest
-            .runFromAnnotations(args, "IsTreeBalanced.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
+            GenericTest
+                    .runFromAnnotations(args, "IsTreeBalanced.java",
+                            new Object() {
+                            }.getClass().getEnclosingClass())
+                    .ordinal());
   }
 }
