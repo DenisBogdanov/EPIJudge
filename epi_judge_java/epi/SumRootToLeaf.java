bc5935fb63206cc01d9199673542a41ce8a0ade4
Denis Bogdanov <bogdanov.math@gmail.com>
2023-02-10 08:25:06 +0400

    Java.

--- epi_judge_java/epi/SumRootToLeaf.java
+++ epi_judge_java/epi/SumRootToLeaf.java
@@ -1,19 +1,31 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
+
 public class SumRootToLeaf {
-  @EpiTest(testDataFile = "sum_root_to_leaf.tsv")
 
-  public static int sumRootToLeaf(BinaryTreeNode<Integer> tree) {
-    // TODO - you fill in here.
-    return 0;
-  }
+    @EpiTest(testDataFile = "sum_root_to_leaf.tsv")
+    public static int sumRootToLeaf(BinaryTreeNode<Integer> tree) {
+        return helper(0, tree);
+    }
 
-  public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "SumRootToLeaf.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
-  }
+    private static int helper(int currResult, BinaryTreeNode<Integer> node) {
+        if (node == null) return 0;
+        int result = currResult * 2;
+        result += node.data;
+
+        if (node.left == null && node.right == null) return result;
+
+        return helper(result, node.left) + helper(result, node.right);
+    }
+
+    public static void main(String[] args) {
+        System.exit(
+                GenericTest
+                        .runFromAnnotations(args, "SumRootToLeaf.java",
+                                new Object() {
+                                }.getClass().getEnclosingClass())
+                        .ordinal());
+    }
 }
