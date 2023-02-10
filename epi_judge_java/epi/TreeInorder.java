759ae08b644f72ac828876d76faeb03acc29bf38
Denis Bogdanov <bogdanov.math@gmail.com>
2023-02-10 08:54:52 +0400

    Java.

--- epi_judge_java/epi/TreeInorder.java
+++ epi_judge_java/epi/TreeInorder.java
@@ -1,32 +1,54 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
 import java.util.List;
+
 public class TreeInorder {
 
-  private static class NodeAndState {
-    public BinaryTreeNode<Integer> node;
-    public Boolean leftSubtreeTraversed;
+    @EpiTest(testDataFile = "tree_inorder.tsv")
+    public static List<Integer> inorderTraversal(BinaryTreeNode<Integer> tree) {
+        List<Integer> result = new ArrayList<>();
 
-    public NodeAndState(BinaryTreeNode<Integer> node,
-                        Boolean leftSubtreeTraversed) {
-      this.node = node;
-      this.leftSubtreeTraversed = leftSubtreeTraversed;
+        Deque<BinaryTreeNode<Integer>> stack = new ArrayDeque<>();
+        var runner = tree;
+
+        while (!stack.isEmpty() || runner != null) {
+            if (runner != null) {
+                stack.push(runner);
+                runner = runner.left;
+            } else {
+                runner = stack.pop();
+                result.add(runner.data);
+                runner = runner.right;
+            }
+        }
+
+        return result;
     }
-  }
 
-  @EpiTest(testDataFile = "tree_inorder.tsv")
-  public static List<Integer> inorderTraversal(BinaryTreeNode<Integer> tree) {
-    // TODO - you fill in here.
-    return null;
-  }
 
-  public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "TreeInorder.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
-  }
+    private static class NodeAndState {
+        public BinaryTreeNode<Integer> node;
+        public Boolean leftSubtreeTraversed;
+
+        public NodeAndState(BinaryTreeNode<Integer> node,
+                            Boolean leftSubtreeTraversed) {
+            this.node = node;
+            this.leftSubtreeTraversed = leftSubtreeTraversed;
+        }
+    }
+
+    public static void main(String[] args) {
+        System.exit(
+                GenericTest
+                        .runFromAnnotations(args, "TreeInorder.java",
+                                new Object() {
+                                }.getClass().getEnclosingClass())
+                        .ordinal());
+    }
 }
