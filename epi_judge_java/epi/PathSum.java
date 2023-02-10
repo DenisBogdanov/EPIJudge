38a10e8cd83478776f3d252ed7a64e78980aec86
Denis Bogdanov <bogdanov.math@gmail.com>
2023-02-10 08:37:41 +0400

    Java.

--- epi_judge_java/epi/PathSum.java
+++ epi_judge_java/epi/PathSum.java
@@ -1,20 +1,26 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
+
 public class PathSum {
-  @EpiTest(testDataFile = "path_sum.tsv")
 
-  public static boolean hasPathSum(BinaryTreeNode<Integer> tree,
-                                   int remainingWeight) {
-    // TODO - you fill in here.
-    return true;
-  }
+    @EpiTest(testDataFile = "path_sum.tsv")
+    public static boolean hasPathSum(BinaryTreeNode<Integer> tree, int remainingWeight) {
+        remainingWeight -= tree.data;
 
-  public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "PathSum.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
-  }
+        if (tree.left == null && tree.right == null) return remainingWeight == 0;
+
+        return (tree.left != null && hasPathSum(tree.left, remainingWeight))
+                || (tree.right != null && hasPathSum(tree.right, remainingWeight));
+    }
+
+    public static void main(String[] args) {
+        System.exit(
+                GenericTest
+                        .runFromAnnotations(args, "PathSum.java",
+                                new Object() {
+                                }.getClass().getEnclosingClass())
+                        .ordinal());
+    }
 }
