27a9e2225ebd24c67455167da76481e63cbe6793
Denis Bogdanov <bogdanov.math@gmail.com>
2023-02-13 07:47:26 +0400

    Java.

--- epi_judge_java/epi/TreeFromPreorderInorder.java
+++ epi_judge_java/epi/TreeFromPreorderInorder.java
@@ -1,22 +1,33 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 
 import java.util.List;
+
 public class TreeFromPreorderInorder {
-  @EpiTest(testDataFile = "tree_from_preorder_inorder.tsv")
 
-  public static BinaryTreeNode<Integer>
-  binaryTreeFromPreorderInorder(List<Integer> preorder, List<Integer> inorder) {
-    // TODO - you fill in here.
-    return null;
-  }
+    @EpiTest(testDataFile = "tree_from_preorder_inorder.tsv")
+    public static BinaryTreeNode<Integer> binaryTreeFromPreorderInorder(List<Integer> preorder, List<Integer> inorder) {
+        if (preorder.size() == 0) return null;
+        Integer rootValue = preorder.get(0);
+        BinaryTreeNode<Integer> root = new BinaryTreeNode<>(rootValue);
+        int indexOfRootValue = inorder.indexOf(rootValue);
+        root.left = binaryTreeFromPreorderInorder(preorder.subList(1, indexOfRootValue + 1),
+                inorder.subList(0, indexOfRootValue));
 
-  public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "TreeFromPreorderInorder.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
-  }
+        root.right = binaryTreeFromPreorderInorder(preorder.subList(indexOfRootValue + 1, preorder.size()),
+                inorder.subList(indexOfRootValue + 1, inorder.size()));
+
+        return root;
+    }
+
+    public static void main(String[] args) {
+        System.exit(
+                GenericTest
+                        .runFromAnnotations(args, "TreeFromPreorderInorder.java",
+                                new Object() {
+                                }.getClass().getEnclosingClass())
+                        .ordinal());
+    }
 }
