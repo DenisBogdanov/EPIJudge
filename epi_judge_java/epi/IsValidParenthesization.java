eccec2636f1b763d23fcfc6dddb4fde1657c9810
Denis Bogdanov <bogdanov.math@gmail.com>
2023-02-15 08:20:22 +0400

    Java.

--- epi_judge_java/epi/IsValidParenthesization.java
+++ epi_judge_java/epi/IsValidParenthesization.java
@@ -1,19 +1,39 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 public class IsValidParenthesization {
-  @EpiTest(testDataFile = "is_valid_parenthesization.tsv")
 
-  public static boolean isWellFormed(String s) {
-    // TODO - you fill in here.
-    return true;
-  }
+    @EpiTest(testDataFile = "is_valid_parenthesization.tsv")
+    public static boolean isWellFormed(String s) {
+        Deque<Character> brackets = new ArrayDeque<>();
 
-  public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "IsValidParenthesization.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
-  }
+        for (char c : s.toCharArray()) {
+            switch (c) {
+                case ')':
+                    if (brackets.isEmpty() || brackets.pop() != '(') return false;
+                    break;
+                case ']':
+                    if (brackets.isEmpty() || brackets.pop() != '[') return false;
+                    break;
+                case '}':
+                    if (brackets.isEmpty() || brackets.pop() != '{') return false;
+                    break;
+                default:
+                    brackets.push(c);
+                    break;
+            }
+        }
+
+        return brackets.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        System.exit(GenericTest.runFromAnnotations(args, "IsValidParenthesization.java", new Object() {
+        }.getClass().getEnclosingClass()).ordinal());
+    }
 }
