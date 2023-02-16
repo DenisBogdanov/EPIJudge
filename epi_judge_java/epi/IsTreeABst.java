fe4c57850dbcebc324dce24d7afc278af3dbbd23
Denis Bogdanov <bogdanov.math@gmail.com>
2023-02-16 08:12:18 +0400

    Java.

--- epi_judge_java/epi/IsTreeABst.java
+++ epi_judge_java/epi/IsTreeABst.java
@@ -1,19 +1,29 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
+
 public class IsTreeABst {
-  @EpiTest(testDataFile = "is_tree_a_bst.tsv")
 
-  public static boolean isBinaryTreeBST(BinaryTreeNode<Integer> tree) {
-    // TODO - you fill in here.
-    return true;
-  }
+    @EpiTest(testDataFile = "is_tree_a_bst.tsv")
+    public static boolean isBinaryTreeBST(BinaryTreeNode<Integer> tree) {
+        return helper(tree, Integer.MIN_VALUE, Integer.MAX_VALUE);
+    }
 
-  public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "IsTreeABst.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
-  }
+    private static boolean helper(BinaryTreeNode<Integer> node, int minValue, int maxValue) {
+        if (node == null) return true;
+        return node.data >= minValue
+                && node.data <= maxValue
+                && helper(node.left, minValue, node.data)
+                && helper(node.right, node.data, maxValue);
+    }
+
+    public static void main(String[] args) {
+        System.exit(
+                GenericTest
+                        .runFromAnnotations(args, "IsTreeABst.java",
+                                new Object() {
+                                }.getClass().getEnclosingClass())
+                        .ordinal());
+    }
 }
