4c4a2a511b22431208979b22167c62a86135df0e
Denis Bogdanov <bogdanov.math@gmail.com>
2023-02-16 08:27:55 +0400

    Java.

--- epi_judge_java/epi/KLargestValuesInBst.java
+++ epi_judge_java/epi/KLargestValuesInBst.java
@@ -1,33 +1,48 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.EpiTestComparator;
 import epi.test_framework.GenericTest;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.function.BiPredicate;
+
 public class KLargestValuesInBst {
-  @EpiTest(testDataFile = "k_largest_values_in_bst.tsv")
 
-  public static List<Integer> findKLargestInBst(BstNode<Integer> tree, int k) {
-    // TODO - you fill in here.
-    return Collections.emptyList();
-  }
-  @EpiTestComparator
-  public static boolean comp(List<Integer> expected, List<Integer> result) {
-    if (result == null) {
-      return false;
+    @EpiTest(testDataFile = "k_largest_values_in_bst.tsv")
+    public static List<Integer> findKLargestInBst(BstNode<Integer> tree, int k) {
+        List<Integer> result = new ArrayList<>();
+        inOrder(tree, result, k);
+        return result;
     }
-    Collections.sort(expected);
-    Collections.sort(result);
-    return expected.equals(result);
-  }
 
-  public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "KLargestValuesInBst.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
-  }
+    private static void inOrder(BstNode<Integer> tree, List<Integer> result, int k) {
+        if (tree == null) return;
+
+        inOrder(tree.right, result, k);
+        if (result.size() == k) return;
+        result.add(tree.data);
+        if (result.size() == k) return;
+        inOrder(tree.left, result, k);
+    }
+
+    @EpiTestComparator
+    public static boolean comp(List<Integer> expected, List<Integer> result) {
+        if (result == null) {
+            return false;
+        }
+        Collections.sort(expected);
+        Collections.sort(result);
+        return expected.equals(result);
+    }
+
+    public static void main(String[] args) {
+        System.exit(
+                GenericTest
+                        .runFromAnnotations(args, "KLargestValuesInBst.java",
+                                new Object() {
+                                }.getClass().getEnclosingClass())
+                        .ordinal());
+    }
 }
