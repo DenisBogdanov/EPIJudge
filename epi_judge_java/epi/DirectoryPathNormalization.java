b7d720fcc924782536b408ff2203880adcffe3b2
Denis Bogdanov <bogdanov.math@gmail.com>
2023-02-16 07:20:35 +0400

    Java.

--- epi_judge_java/epi/DirectoryPathNormalization.java
+++ epi_judge_java/epi/DirectoryPathNormalization.java
@@ -1,19 +1,51 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Deque;
+
 public class DirectoryPathNormalization {
-  @EpiTest(testDataFile = "directory_path_normalization.tsv")
 
-  public static String shortestEquivalentPath(String path) {
-    // TODO - you fill in here.
-    return "";
-  }
+    @EpiTest(testDataFile = "directory_path_normalization.tsv")
+    public static String shortestEquivalentPath(String path) {
+        boolean isAbsolute = path.startsWith("/");
+        Deque<String> partsDeque = new ArrayDeque<>();
 
-  public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "DirectoryPathNormalization.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
-  }
+        for (String part : path.split("/")) {
+            if (part.isEmpty()) continue;
+
+            switch (part) {
+                case "..":
+                    if (!isAbsolute && (partsDeque.isEmpty() || partsDeque.peek().equals(".."))) {
+                        partsDeque.push(part);
+                    } else {
+                        partsDeque.pop();
+                    }
+                    break;
+                case ".":
+                    break;
+                default:
+                    partsDeque.push(part);
+                    break;
+            }
+        }
+
+        ArrayList<String> parts = new ArrayList<>(partsDeque);
+        if (!isAbsolute && parts.isEmpty()) parts.add(".");
+        Collections.reverse(parts);
+        return (isAbsolute ? "/" : "") + String.join("/", parts);
+    }
+
+    public static void main(String[] args) {
+        System.exit(
+                GenericTest
+                        .runFromAnnotations(args, "DirectoryPathNormalization.java",
+                                new Object() {
+                                }.getClass().getEnclosingClass())
+                        .ordinal());
+    }
 }
