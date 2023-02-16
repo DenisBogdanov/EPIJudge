b055655bdf630b3766d0edef3d5aceb61a9b1973
Denis Bogdanov <bogdanov.math@gmail.com>
2023-02-16 07:34:15 +0400

    Java.

--- epi_judge_java/epi/TreeLevelOrder.java
+++ epi_judge_java/epi/TreeLevelOrder.java
@@ -1,22 +1,47 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 
+import java.util.ArrayDeque;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Queue;
+
 public class TreeLevelOrder {
-  @EpiTest(testDataFile = "tree_level_order.tsv")
 
-  public static List<List<Integer>>
-  binaryTreeDepthOrder(BinaryTreeNode<Integer> tree) {
-    // TODO - you fill in here.
-    return null;
-  }
+    @EpiTest(testDataFile = "tree_level_order.tsv")
+    public static List<List<Integer>> binaryTreeDepthOrder(BinaryTreeNode<Integer> tree) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (tree == null) return result;
 
-  public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "TreeLevelOrder.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
-  }
+        Queue<BinaryTreeNode<Integer>> q = new ArrayDeque<>();
+        q.offer(tree);
+
+        while (!q.isEmpty()) {
+            List<Integer> level = new ArrayList<>();
+            int size = q.size();
+
+            for (int i = 0; i < size; i++) {
+                BinaryTreeNode<Integer> polled = q.poll();
+                level.add(polled.data);
+
+                if (polled.left != null) q.offer(polled.left);
+                if (polled.right != null) q.offer(polled.right);
+            }
+
+            result.add(level);
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.exit(
+                GenericTest
+                        .runFromAnnotations(args, "TreeLevelOrder.java",
+                                new Object() {
+                                }.getClass().getEnclosingClass())
+                        .ordinal());
+    }
 }
