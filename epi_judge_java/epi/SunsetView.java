27520a9351f859016f35049b2a495a61118ce738
Denis Bogdanov <bogdanov.math@gmail.com>
2023-02-16 07:31:40 +0400

    Java.

--- epi_judge_java/epi/SunsetView.java
+++ epi_judge_java/epi/SunsetView.java
@@ -1,26 +1,54 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 
 import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
-public class SunsetView {
-  public static List<Integer>
-  examineBuildingsWithSunset(Iterator<Integer> sequence) {
-    // TODO - you fill in here.
-    return null;
-  }
-  @EpiTest(testDataFile = "sunset_view.tsv")
-  public static List<Integer>
-  examineBuildingsWithSunsetWrapper(List<Integer> sequence) {
-    return examineBuildingsWithSunset(sequence.iterator());
-  }
+import java.util.stream.Collectors;
 
-  public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "SunsetView.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
-  }
+public class SunsetView {
+
+    public static List<Integer> examineBuildingsWithSunset(Iterator<Integer> sequence) {
+        LinkedList<Building> result = new LinkedList<>();
+
+        int index = 0;
+        while (sequence.hasNext()) {
+            Integer nextHeight = sequence.next();
+
+            while (!result.isEmpty() && result.peek().height <= nextHeight) {
+                result.pop();
+            }
+
+            result.push(new Building(index, nextHeight));
+            index++;
+        }
+
+        return result.stream().map(b -> b.index).collect(Collectors.toList());
+    }
+
+    private static class Building {
+        final int index;
+        final int height;
+
+        public Building(int index, int height) {
+            this.index = index;
+            this.height = height;
+        }
+    }
+
+    @EpiTest(testDataFile = "sunset_view.tsv")
+    public static List<Integer> examineBuildingsWithSunsetWrapper(List<Integer> sequence) {
+        return examineBuildingsWithSunset(sequence.iterator());
+    }
+
+    public static void main(String[] args) {
+        System.exit(
+                GenericTest
+                        .runFromAnnotations(args, "SunsetView.java",
+                                new Object() {
+                                }.getClass().getEnclosingClass())
+                        .ordinal());
+    }
 }
