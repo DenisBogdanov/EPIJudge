965dad18f625ee0f31d423cb6aaf565880fe9f7b
Denis Bogdanov <bogdanov.math@gmail.com>
2023-02-17 08:38:43 +0400

    Java.

--- epi_judge_java/epi/NumberOfScoreCombinations.java
+++ epi_judge_java/epi/NumberOfScoreCombinations.java
@@ -1,23 +1,32 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 
 import java.util.List;
+
 public class NumberOfScoreCombinations {
-  @EpiTest(testDataFile = "number_of_score_combinations.tsv")
 
-  public static int
-  numCombinationsForFinalScore(int finalScore,
-                               List<Integer> individualPlayScores) {
-    // TODO - you fill in here.
-    return 0;
-  }
+    @EpiTest(testDataFile = "number_of_score_combinations.tsv")
+    public static int numCombinationsForFinalScore(int finalScore, List<Integer> individualPlayScores) {
+        int[] scoreToCombinationsDp = new int[finalScore + 1];
+        scoreToCombinationsDp[0] = 1;
 
-  public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "NumberOfScoreCombinations.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
-  }
+        for (Integer score : individualPlayScores) {
+            for (int i = score; i <= finalScore; i++) {
+                scoreToCombinationsDp[i] += scoreToCombinationsDp[i - score];
+            }
+        }
+
+        return scoreToCombinationsDp[finalScore];
+    }
+
+    public static void main(String[] args) {
+        System.exit(
+                GenericTest
+                        .runFromAnnotations(args, "NumberOfScoreCombinations.java",
+                                new Object() {
+                                }.getClass().getEnclosingClass())
+                        .ordinal());
+    }
 }
