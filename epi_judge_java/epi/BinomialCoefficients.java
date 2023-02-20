d0105fb22f91e8b2d6a3ca2743e33e5e620dacbb
Denis Bogdanov <bogdanov.math@gmail.com>
2023-02-20 08:08:05 +0400

    Java.

--- epi_judge_java/epi/BinomialCoefficients.java
+++ epi_judge_java/epi/BinomialCoefficients.java
@@ -1,19 +1,33 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
+
 public class BinomialCoefficients {
-  @EpiTest(testDataFile = "binomial_coefficients.tsv")
 
-  public static int computeBinomialCoefficient(int n, int k) {
-    // TODO - you fill in here.
-    return 0;
-  }
+    @EpiTest(testDataFile = "binomial_coefficients.tsv")
+    public static int computeBinomialCoefficient(int n, int k) {
+        if (n == k || n == 0 || k == 0) return 1;
 
-  public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "BinomialCoefficients.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
-  }
+        int[][] pascalTriangular = new int[n + 1][k + 1];
+        pascalTriangular[0][0] = 1;
+
+        for (int row = 1; row <= n; row++) {
+            pascalTriangular[row][0] = 1;
+            for (int col = 1; col <= k; col++) {
+                pascalTriangular[row][col] = pascalTriangular[row - 1][col - 1] + pascalTriangular[row - 1][col];
+            }
+        }
+
+        return pascalTriangular[n][k];
+    }
+
+    public static void main(String[] args) {
+        System.exit(
+                GenericTest
+                        .runFromAnnotations(args, "BinomialCoefficients.java",
+                                new Object() {
+                                }.getClass().getEnclosingClass())
+                        .ordinal());
+    }
 }
