7ddb4f213c05cbed6df76327a7701e4c69515353
Denis Bogdanov <bogdanov.math@gmail.com>
2023-02-21 08:36:27 +0400

    Java.

--- epi_judge_java/epi/Knapsack.java
+++ epi_judge_java/epi/Knapsack.java
@@ -1,34 +1,56 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.EpiUserType;
 import epi.test_framework.GenericTest;
 
+import java.util.Arrays;
 import java.util.List;
+
 public class Knapsack {
-  @EpiUserType(ctorParams = {Integer.class, Integer.class})
 
-  public static class Item {
-    public Integer weight;
-    public Integer value;
+    @EpiTest(testDataFile = "knapsack.tsv")
+    public static int optimumSubjectToCapacity(List<Item> items, int capacity) {
+        int[][] cache = new int[items.size()][capacity + 1];
+        for (int[] row : cache) {
+            Arrays.fill(row, -1);
+        }
 
-    public Item(Integer weight, Integer value) {
-      this.weight = weight;
-      this.value = value;
+        return helper(items, 0, capacity, cache);
     }
-  }
 
-  @EpiTest(testDataFile = "knapsack.tsv")
+    private static int helper(List<Item> items, int index, int capacity, int[][] cache) {
+        if (index == items.size()) return 0;
 
-  public static int optimumSubjectToCapacity(List<Item> items, int capacity) {
-    // TODO - you fill in here.
-    return 0;
-  }
+        if (cache[index][capacity] != -1) return cache[index][capacity];
 
-  public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "Knapsack.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
-  }
+        int result = 0;
+        if (items.get(index).weight <= capacity) {
+            result = items.get(index).value + helper(items, index + 1, capacity - items.get(index).weight, cache);
+        }
+
+        return cache[index][capacity] = Math.max(
+                result,
+                helper(items, index + 1, capacity, cache));
+    }
+
+    @EpiUserType(ctorParams = {Integer.class, Integer.class})
+    public static class Item {
+        public Integer weight;
+        public Integer value;
+
+        public Item(Integer weight, Integer value) {
+            this.weight = weight;
+            this.value = value;
+        }
+    }
+
+    public static void main(String[] args) {
+        System.exit(
+                GenericTest
+                        .runFromAnnotations(args, "Knapsack.java",
+                                new Object() {
+                                }.getClass().getEnclosingClass())
+                        .ordinal());
+    }
 }
