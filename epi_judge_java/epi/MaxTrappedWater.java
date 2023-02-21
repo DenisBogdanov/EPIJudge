435b872e42c5b3872a96a866231ddf41c9da9fdc
Denis Bogdanov <bogdanov.math@gmail.com>
2023-02-21 07:41:13 +0400

    Java.

--- epi_judge_java/epi/MaxTrappedWater.java
+++ epi_judge_java/epi/MaxTrappedWater.java
@@ -1,21 +1,38 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 
 import java.util.List;
+
 public class MaxTrappedWater {
-  @EpiTest(testDataFile = "max_trapped_water.tsv")
 
-  public static int getMaxTrappedWater(List<Integer> heights) {
-    // TODO - you fill in here.
-    return 0;
-  }
+    @EpiTest(testDataFile = "max_trapped_water.tsv")
+    public static int getMaxTrappedWater(List<Integer> heights) {
+        int result = 0;
 
-  public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "MaxTrappedWater.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
-  }
+        int left = 0;
+        int right = heights.size() - 1;
+
+        while (left <= right) {
+            result = Math.max(result, (right - left) * Math.min(heights.get(left), heights.get(right)));
+
+            if (heights.get(left) < heights.get(right)) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.exit(
+                GenericTest
+                        .runFromAnnotations(args, "MaxTrappedWater.java",
+                                new Object() {
+                                }.getClass().getEnclosingClass())
+                        .ordinal());
+    }
 }
