f3eea6f5adcde322d645ad44e935284b07860db7
Denis Bogdanov <bogdanov.math@gmail.com>
2023-02-21 07:11:24 +0400

    Java.

--- epi_judge_java/epi/MinimumPointsCoveringIntervals.java
+++ epi_judge_java/epi/MinimumPointsCoveringIntervals.java
@@ -1,33 +1,48 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.EpiUserType;
 import epi.test_framework.GenericTest;
 
+import java.util.Comparator;
 import java.util.List;
+
 public class MinimumPointsCoveringIntervals {
-  @EpiUserType(ctorParams = {int.class, int.class})
 
-  public static class Interval {
-    public int left, right;
+    @EpiTest(testDataFile = "minimum_points_covering_intervals.tsv")
+    public static Integer findMinimumVisits(List<Interval> intervals) {
+        if (intervals.isEmpty()) return 0;
+        intervals.sort(Comparator.comparingInt(interval -> interval.right));
 
-    public Interval(int l, int r) {
-      this.left = l;
-      this.right = r;
+        int result = 1;
+        int prevVisit = intervals.get(0).right;
+
+        for (Interval interval : intervals) {
+            if (interval.left > prevVisit) {
+                result++;
+                prevVisit = interval.right;
+            }
+        }
+
+        return result;
     }
-  }
 
-  @EpiTest(testDataFile = "minimum_points_covering_intervals.tsv")
+    @EpiUserType(ctorParams = {int.class, int.class})
+    public static class Interval {
+        public int left, right;
 
-  public static Integer findMinimumVisits(List<Interval> intervals) {
-    // TODO - you fill in here.
-    return 0;
-  }
+        public Interval(int l, int r) {
+            this.left = l;
+            this.right = r;
+        }
+    }
 
-  public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "MinimumPointsCoveringIntervals.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
-  }
+    public static void main(String[] args) {
+        System.exit(
+                GenericTest
+                        .runFromAnnotations(args, "MinimumPointsCoveringIntervals.java",
+                                new Object() {
+                                }.getClass().getEnclosingClass())
+                        .ordinal());
+    }
 }
