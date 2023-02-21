660f1e1acd2a891efe436a710d685dacb7aef707
Denis Bogdanov <bogdanov.math@gmail.com>
2023-02-21 07:16:20 +0400

    Java.

--- epi_judge_java/epi/ThreeSum.java
+++ epi_judge_java/epi/ThreeSum.java
@@ -1,21 +1,45 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 
+import java.util.Collections;
 import java.util.List;
+
 public class ThreeSum {
-  @EpiTest(testDataFile = "three_sum.tsv")
 
-  public static boolean hasThreeSum(List<Integer> A, int t) {
-    // TODO - you fill in here.
-    return true;
-  }
+    @EpiTest(testDataFile = "three_sum.tsv")
+    public static boolean hasThreeSum(List<Integer> list, int t) {
+        Collections.sort(list);
 
-  public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "ThreeSum.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
-  }
+        for (int i = 0; i < list.size(); i++) {
+            int remainingSum = t - list.get(i);
+
+            int left = i;
+            int right = list.size() - 1;
+
+            while (left <= right) {
+                int candidateSum = list.get(left) + list.get(right);
+
+                if (candidateSum > remainingSum) {
+                    right--;
+                } else if (candidateSum < remainingSum) {
+                    left++;
+                } else {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+        System.exit(
+                GenericTest
+                        .runFromAnnotations(args, "ThreeSum.java",
+                                new Object() {
+                                }.getClass().getEnclosingClass())
+                        .ordinal());
+    }
 }
