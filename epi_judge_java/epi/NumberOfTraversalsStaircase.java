33e9fff01470994ed2bb470fa441495b0463f0e6
Denis Bogdanov <bogdanov.math@gmail.com>
2023-02-22 08:38:06 +0400

    Java.

--- epi_judge_java/epi/NumberOfTraversalsStaircase.java
+++ epi_judge_java/epi/NumberOfTraversalsStaircase.java
@@ -1,19 +1,38 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
+
 public class NumberOfTraversalsStaircase {
-  @EpiTest(testDataFile = "number_of_traversals_staircase.tsv")
 
-  public static int numberOfWaysToTop(int top, int maximumStep) {
-    // TODO - you fill in here.
-    return 0;
-  }
+    @EpiTest(testDataFile = "number_of_traversals_staircase.tsv")
+    public static int numberOfWaysToTop(int top, int maximumStep) {
+        int[] waysToSizeDp = new int[top + 1];
+        waysToSizeDp[0] = 1;
 
-  public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "NumberOfTraversalsStaircase.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
-  }
+        int currSum = 1;
+        for (int i = 1; i <= Math.min(maximumStep, top); i++) {
+            waysToSizeDp[i] = currSum;
+            currSum *= 2;
+        }
+
+        currSum /= 2;
+
+        for (int i = maximumStep + 1; i <= top; i++) {
+            currSum *= 2;
+            currSum -= waysToSizeDp[i - maximumStep - 1];
+            waysToSizeDp[i] = currSum;
+        }
+
+        return waysToSizeDp[top];
+    }
+
+    public static void main(String[] args) {
+        System.exit(
+                GenericTest
+                        .runFromAnnotations(args, "NumberOfTraversalsStaircase.java",
+                                new Object() {
+                                }.getClass().getEnclosingClass())
+                        .ordinal());
+    }
 }
