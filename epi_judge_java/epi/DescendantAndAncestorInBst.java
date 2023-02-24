02d25aba9ab952f26df921efd86e2a1165edc50d
Denis Bogdanov <bogdanov.math@gmail.com>
2023-02-24 08:12:12 +0400

    Java.

--- epi_judge_java/epi/DescendantAndAncestorInBst.java
+++ epi_judge_java/epi/DescendantAndAncestorInBst.java
@@ -1,38 +1,55 @@
 package epi;
+
 import epi.test_framework.BinaryTreeUtils;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 import epi.test_framework.TimedExecutor;
+
 public class DescendantAndAncestorInBst {
 
-  public static boolean
-  pairIncludesAncestorAndDescendantOfM(BstNode<Integer> possibleAncOrDesc0,
-                                       BstNode<Integer> possibleAncOrDesc1,
-                                       BstNode<Integer> middle) {
-    // TODO - you fill in here.
-    return true;
-  }
-  @EpiTest(testDataFile = "descendant_and_ancestor_in_bst.tsv")
-  public static boolean pairIncludesAncestorAndDescendantOfMWrapper(
-      TimedExecutor executor, BstNode<Integer> tree, int possibleAncOrDesc0,
-      int possibleAncOrDesc1, int middle) throws Exception {
-    final BstNode<Integer> candidate0 =
-        BinaryTreeUtils.mustFindNode(tree, possibleAncOrDesc0);
-    final BstNode<Integer> candidate1 =
-        BinaryTreeUtils.mustFindNode(tree, possibleAncOrDesc1);
-    final BstNode<Integer> middleNode =
-        BinaryTreeUtils.mustFindNode(tree, middle);
+    public static boolean pairIncludesAncestorAndDescendantOfM(BstNode<Integer> possibleAncOrDesc0,
+                                                               BstNode<Integer> possibleAncOrDesc1,
+                                                               BstNode<Integer> middle) {
 
-    return executor.run(()
-                            -> pairIncludesAncestorAndDescendantOfM(
-                                candidate0, candidate1, middleNode));
-  }
+        if (possibleAncOrDesc0 == middle || possibleAncOrDesc1 == middle) return false;
 
-  public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "DescendantAndAncestorInBst.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
-  }
+        return (isDescendant(possibleAncOrDesc0, middle) && isDescendant(middle, possibleAncOrDesc1))
+                || (isDescendant(middle, possibleAncOrDesc0) && isDescendant(possibleAncOrDesc1, middle));
+    }
+
+    private static boolean isDescendant(BstNode<Integer> candidate, BstNode<Integer> node) {
+        if (node == null) return false;
+        if (candidate.data < node.data) {
+            return isDescendant(candidate, node.left);
+        } else if (candidate.data > node.data) {
+            return isDescendant(candidate, node.right);
+        } else {
+            return true;
+        }
+    }
+
+    @EpiTest(testDataFile = "descendant_and_ancestor_in_bst.tsv")
+    public static boolean pairIncludesAncestorAndDescendantOfMWrapper(
+            TimedExecutor executor, BstNode<Integer> tree, int possibleAncOrDesc0,
+            int possibleAncOrDesc1, int middle) throws Exception {
+        final BstNode<Integer> candidate0 =
+                BinaryTreeUtils.mustFindNode(tree, possibleAncOrDesc0);
+        final BstNode<Integer> candidate1 =
+                BinaryTreeUtils.mustFindNode(tree, possibleAncOrDesc1);
+        final BstNode<Integer> middleNode =
+                BinaryTreeUtils.mustFindNode(tree, middle);
+
+        return executor.run(()
+                -> pairIncludesAncestorAndDescendantOfM(
+                candidate0, candidate1, middleNode));
+    }
+
+    public static void main(String[] args) {
+        System.exit(
+                GenericTest
+                        .runFromAnnotations(args, "DescendantAndAncestorInBst.java",
+                                new Object() {
+                                }.getClass().getEnclosingClass())
+                        .ordinal());
+    }
 }
