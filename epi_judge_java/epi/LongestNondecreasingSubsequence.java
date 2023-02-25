39196230438cf3c00e25e031ece53d3034938bf0
Denis Bogdanov <bogdanov.math@gmail.com>
2023-02-25 09:19:20 +0400

    Java.

--- epi_judge_java/epi/LongestNondecreasingSubsequence.java
+++ epi_judge_java/epi/LongestNondecreasingSubsequence.java
@@ -1,21 +1,52 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 
+import java.util.ArrayList;
 import java.util.List;
+
 public class LongestNondecreasingSubsequence {
-  @EpiTest(testDataFile = "longest_nondecreasing_subsequence.tsv")
 
-  public static int longestNondecreasingSubsequenceLength(List<Integer> A) {
-    // TODO - you fill in here.
-    return 0;
-  }
+    @EpiTest(testDataFile = "longest_nondecreasing_subsequence.tsv")
+    public static int longestNondecreasingSubsequenceLength(List<Integer> list) {
+        List<Integer> longestSoFar = new ArrayList<>();
 
-  public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "LongestNondecreasingSubsequence.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
-  }
+        for (Integer num : list) {
+            if (longestSoFar.isEmpty() || num >= longestSoFar.get(longestSoFar.size() - 1)) {
+                longestSoFar.add(num);
+            } else {
+                int index = binarySearch(longestSoFar, num);
+                longestSoFar.set(index, num);
+            }
+        }
+
+        return longestSoFar.size();
+    }
+
+    private static int binarySearch(List<Integer> nums, int num) {
+        int left = -1;
+        int right = nums.size() - 1;
+
+        while (left + 1 < right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums.get(mid) > num) {
+                right = mid;
+            } else {
+                left = mid;
+            }
+        }
+
+        return right;
+    }
+
+    public static void main(String[] args) {
+        System.exit(
+                GenericTest
+                        .runFromAnnotations(args, "LongestNondecreasingSubsequence.java",
+                                new Object() {
+                                }.getClass().getEnclosingClass())
+                        .ordinal());
+    }
 }
