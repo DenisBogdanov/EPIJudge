2400903fadcbf7434f4cf82d01436be2b80f74a7
Denis Bogdanov <bogdanov.math@gmail.com>
2023-03-24 08:35:24 +0400

    Java.

--- epi_judge_java/epi/ReverseSublist.java
+++ epi_judge_java/epi/ReverseSublist.java
@@ -1,20 +1,46 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
+
 public class ReverseSublist {
-  @EpiTest(testDataFile = "reverse_sublist.tsv")
 
-  public static ListNode<Integer> reverseSublist(ListNode<Integer> L, int start,
-                                                 int finish) {
-    // TODO - you fill in here.
-    return null;
-  }
+    @EpiTest(testDataFile = "reverse_sublist.tsv")
+    public static ListNode<Integer> reverseSublist(ListNode<Integer> list, int start, int finish) {
+        if (start == finish) return list;
 
-  public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "ReverseSublist.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
-  }
+        ListNode<Integer> dummyNode = new ListNode<>(-1, list);
+        int index = 1;
+
+        ListNode<Integer> runner = dummyNode;
+
+        while (index < start) {
+            runner = runner.next;
+            index++;
+        }
+
+        ListNode<Integer> prevStartNode = runner;
+
+        runner = prevStartNode.next;
+
+        while (start < finish) {
+            ListNode<Integer> temp = runner.next;
+            runner.next = runner.next.next;
+            temp.next = prevStartNode.next;
+            prevStartNode.next = temp;
+
+            start++;
+        }
+
+        return dummyNode.next;
+    }
+
+    public static void main(String[] args) {
+        System.exit(
+                GenericTest
+                        .runFromAnnotations(args, "ReverseSublist.java",
+                                new Object() {
+                                }.getClass().getEnclosingClass())
+                        .ordinal());
+    }
 }
--- epi_judge_java/epi/SortedListsMerge.java
+++ epi_judge_java/epi/SortedListsMerge.java
@@ -1,20 +1,42 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
-public class SortedListsMerge {
-  @EpiTest(testDataFile = "sorted_lists_merge.tsv")
-  //@include
-  public static ListNode<Integer> mergeTwoSortedLists(ListNode<Integer> L1,
-                                                      ListNode<Integer> L2) {
-    // TODO - you fill in here.
-    return null;
-  }
 
-  public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "SortedListsMerge.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
-  }
+public class SortedListsMerge {
+
+    @EpiTest(testDataFile = "sorted_lists_merge.tsv")
+    public static ListNode<Integer> mergeTwoSortedLists(ListNode<Integer> list1, ListNode<Integer> list2) {
+        ListNode<Integer> dummy = new ListNode<>(-1, null);
+        ListNode<Integer> runner = dummy;
+
+        while (list1 != null && list2 != null) {
+            if (list1.data < list2.data) {
+                runner.next = new ListNode<>(list1.data, null);
+                list1 = list1.next;
+            } else {
+                runner.next = new ListNode<>(list2.data, null);
+                list2 = list2.next;
+            }
+
+            runner = runner.next;
+        }
+
+        if (list1 != null) {
+            runner.next = list1;
+        } else {
+            runner.next = list2;
+        }
+
+        return dummy.next;
+    }
+
+    public static void main(String[] args) {
+        System.exit(
+                GenericTest
+                        .runFromAnnotations(args, "SortedListsMerge.java",
+                                new Object() {
+                                }.getClass().getEnclosingClass())
+                        .ordinal());
+    }
 }
