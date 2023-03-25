06d2c09e6026bfbd38a7e2b99ed9acb18963157c
Denis Bogdanov <bogdanov.math@gmail.com>
2023-03-25 07:48:46 +0400

    Java.

--- epi_judge_java/epi/DoListsOverlap.java
+++ epi_judge_java/epi/DoListsOverlap.java
@@ -1,4 +1,5 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 import epi.test_framework.TestFailure;
@@ -6,93 +7,111 @@ import epi.test_framework.TimedExecutor;
 
 import java.util.HashSet;
 import java.util.Set;
+
 public class DoListsOverlap {
 
-  public static ListNode<Integer> overlappingLists(ListNode<Integer> l0,
-                                                   ListNode<Integer> l1) {
-    // TODO - you fill in here.
-    return null;
-  }
-  @EpiTest(testDataFile = "do_lists_overlap.tsv")
-  public static void
-  overlappingListsWrapper(TimedExecutor executor, ListNode<Integer> l0,
-                          ListNode<Integer> l1, ListNode<Integer> common,
-                          int cycle0, int cycle1) throws Exception {
-    if (common != null) {
-      if (l0 == null) {
-        l0 = common;
-      } else {
-        ListNode<Integer> it = l0;
-        while (it.next != null) {
-          it = it.next;
+    public static ListNode<Integer> overlappingLists(ListNode<Integer> list1, ListNode<Integer> list2) {
+        if (list1 == null || list2 == null) return null;
+
+        ListNode<Integer> cycleStart1 = IsListCyclic.hasCycle(list1);
+        ListNode<Integer> cycleStart2 = IsListCyclic.hasCycle(list2);
+
+        if (cycleStart1 != null && cycleStart2 != null) {
+            ListNode<Integer> runner2 = cycleStart2;
+            do {
+                if (runner2 == cycleStart1) return runner2;
+                runner2 = runner2.next;
+            } while (runner2 != cycleStart2);
+
+            return null;
+        } else if (cycleStart1 == null && cycleStart2 == null) {
+            return DoTerminatedListsOverlap.overlappingNoCycleLists(list1, list2);
         }
-        it.next = common;
-      }
 
-      if (l1 == null) {
-        l1 = common;
-      } else {
-        ListNode<Integer> it = l1;
-        while (it.next != null) {
-          it = it.next;
+        return null;
+    }
+
+    @EpiTest(testDataFile = "do_lists_overlap.tsv")
+    public static void
+    overlappingListsWrapper(TimedExecutor executor, ListNode<Integer> l0,
+                            ListNode<Integer> l1, ListNode<Integer> common,
+                            int cycle0, int cycle1) throws Exception {
+        if (common != null) {
+            if (l0 == null) {
+                l0 = common;
+            } else {
+                ListNode<Integer> it = l0;
+                while (it.next != null) {
+                    it = it.next;
+                }
+                it.next = common;
+            }
+
+            if (l1 == null) {
+                l1 = common;
+            } else {
+                ListNode<Integer> it = l1;
+                while (it.next != null) {
+                    it = it.next;
+                }
+                it.next = common;
+            }
         }
-        it.next = common;
-      }
-    }
 
-    if (cycle0 != -1 && l0 != null) {
-      ListNode<Integer> last = l0;
-      while (last.next != null) {
-        last = last.next;
-      }
-      ListNode<Integer> it = l0;
-      while (cycle0-- > 0) {
-        if (it == null) {
-          throw new RuntimeException("Invalid input data");
+        if (cycle0 != -1 && l0 != null) {
+            ListNode<Integer> last = l0;
+            while (last.next != null) {
+                last = last.next;
+            }
+            ListNode<Integer> it = l0;
+            while (cycle0-- > 0) {
+                if (it == null) {
+                    throw new RuntimeException("Invalid input data");
+                }
+                it = it.next;
+            }
+            last.next = it;
         }
-        it = it.next;
-      }
-      last.next = it;
-    }
 
-    if (cycle1 != -1 && l1 != null) {
-      ListNode<Integer> last = l1;
-      while (last.next != null) {
-        last = last.next;
-      }
-      ListNode<Integer> it = l1;
-      while (cycle1-- > 0) {
-        if (it == null) {
-          throw new RuntimeException("Invalid input data");
+        if (cycle1 != -1 && l1 != null) {
+            ListNode<Integer> last = l1;
+            while (last.next != null) {
+                last = last.next;
+            }
+            ListNode<Integer> it = l1;
+            while (cycle1-- > 0) {
+                if (it == null) {
+                    throw new RuntimeException("Invalid input data");
+                }
+                it = it.next;
+            }
+            last.next = it;
         }
-        it = it.next;
-      }
-      last.next = it;
+
+        Set<Integer> commonNodes = new HashSet<>();
+        ListNode<Integer> it = common;
+        while (it != null && !commonNodes.contains(it.data)) {
+            commonNodes.add(it.data);
+            it = it.next;
+        }
+
+        final ListNode<Integer> finalL0 = l0;
+        final ListNode<Integer> finalL1 = l1;
+        ListNode<Integer> result =
+                executor.run(() -> overlappingLists(finalL0, finalL1));
+
+        if (!((commonNodes.isEmpty() && result == null) ||
+                (result != null && commonNodes.contains(result.data)))) {
+            throw new TestFailure("Invalid result");
+        }
     }
 
-    Set<Integer> commonNodes = new HashSet<>();
-    ListNode<Integer> it = common;
-    while (it != null && !commonNodes.contains(it.data)) {
-      commonNodes.add(it.data);
-      it = it.next;
+    public static void main(String[] args) {
+        System.exit(
+                GenericTest
+                        .runFromAnnotations(args, "DoListsOverlap.java",
+                                new Object() {
+                                }.getClass().getEnclosingClass())
+                        .ordinal());
     }
-
-    final ListNode<Integer> finalL0 = l0;
-    final ListNode<Integer> finalL1 = l1;
-    ListNode<Integer> result =
-        executor.run(() -> overlappingLists(finalL0, finalL1));
-
-    if (!((commonNodes.isEmpty() && result == null) ||
-          (result != null && commonNodes.contains(result.data)))) {
-      throw new TestFailure("Invalid result");
-    }
-  }
-
-  public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "DoListsOverlap.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
-  }
 }
