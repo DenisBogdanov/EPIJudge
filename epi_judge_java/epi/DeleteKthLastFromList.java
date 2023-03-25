bb70f928b320e87be7dd97ccdc282c5ddc4a4f74
Denis Bogdanov <bogdanov.math@gmail.com>
2023-03-25 08:39:14 +0400

    Java.

--- epi_judge_java/epi/DeleteKthLastFromList.java
+++ epi_judge_java/epi/DeleteKthLastFromList.java
@@ -1,20 +1,36 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
+
 public class DeleteKthLastFromList {
-  @EpiTest(testDataFile = "delete_kth_last_from_list.tsv")
 
-  // Assumes L has at least k nodes, deletes the k-th last node in L.
-  public static ListNode<Integer> removeKthLast(ListNode<Integer> L, int k) {
-    // TODO - you fill in here.
-    return null;
-  }
+    @EpiTest(testDataFile = "delete_kth_last_from_list.tsv")
+    // Assumes L has at least k nodes, deletes the k-th last node in L.
+    public static ListNode<Integer> removeKthLast(ListNode<Integer> list, int k) {
+        ListNode<Integer> dummy = new ListNode<>(-1, list);
 
-  public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "DeleteKthLastFromList.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
-  }
+        ListNode<Integer> aheadRunner = list;
+        for (int i = 0; i < k; i++) {
+            aheadRunner = aheadRunner.next;
+        }
+
+        ListNode<Integer> runner = dummy;
+        while (aheadRunner != null) {
+            runner = runner.next;
+            aheadRunner = aheadRunner.next;
+        }
+
+        runner.next = runner.next.next;
+        return dummy.next;
+    }
+
+    public static void main(String[] args) {
+        System.exit(
+                GenericTest
+                        .runFromAnnotations(args, "DeleteKthLastFromList.java",
+                                new Object() {
+                                }.getClass().getEnclosingClass())
+                        .ordinal());
+    }
 }
