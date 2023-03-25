19c478aa904e867de0c322088d4d292f409a93de
Denis Bogdanov <bogdanov.math@gmail.com>
2023-03-25 08:43:20 +0400

    Java.

--- epi_judge_java/epi/RemoveDuplicatesFromSortedList.java
+++ epi_judge_java/epi/RemoveDuplicatesFromSortedList.java
@@ -1,19 +1,33 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
+
 public class RemoveDuplicatesFromSortedList {
-  @EpiTest(testDataFile = "remove_duplicates_from_sorted_list.tsv")
 
-  public static ListNode<Integer> removeDuplicates(ListNode<Integer> L) {
-    // TODO - you fill in here.
-    return null;
-  }
+    @EpiTest(testDataFile = "remove_duplicates_from_sorted_list.tsv")
+    public static ListNode<Integer> removeDuplicates(ListNode<Integer> list) {
+        if (list == null || list.next == null) return list;
 
-  public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "RemoveDuplicatesFromSortedList.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
-  }
+        ListNode<Integer> runner = list;
+
+        while (runner != null) {
+            while (runner.next != null && runner.data.equals(runner.next.data)) {
+                runner.next = runner.next.next;
+            }
+
+            runner = runner.next;
+        }
+
+        return list;
+    }
+
+    public static void main(String[] args) {
+        System.exit(
+                GenericTest
+                        .runFromAnnotations(args, "RemoveDuplicatesFromSortedList.java",
+                                new Object() {
+                                }.getClass().getEnclosingClass())
+                        .ordinal());
+    }
 }
