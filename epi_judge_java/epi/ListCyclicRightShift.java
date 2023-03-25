97314311adae509d6011e38a45b80b269c21af54
Denis Bogdanov <bogdanov.math@gmail.com>
2023-03-25 08:52:12 +0400

    Java.

--- epi_judge_java/epi/ListCyclicRightShift.java
+++ epi_judge_java/epi/ListCyclicRightShift.java
@@ -1,20 +1,45 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
+
 public class ListCyclicRightShift {
-  @EpiTest(testDataFile = "list_cyclic_right_shift.tsv")
 
-  public static ListNode<Integer> cyclicallyRightShiftList(ListNode<Integer> L,
-                                                           int k) {
-    // TODO - you fill in here.
-    return null;
-  }
+    @EpiTest(testDataFile = "list_cyclic_right_shift.tsv")
+    public static ListNode<Integer> cyclicallyRightShiftList(ListNode<Integer> list, int k) {
+        if (list == null || list.next == null) return list;
 
-  public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "ListCyclicRightShift.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
-  }
+        int length = 1;
+        ListNode<Integer> runner = list;
+        while (runner.next != null) {
+            runner = runner.next;
+            length++;
+        }
+
+        ListNode<Integer> tailNode = runner;
+
+        k %= length;
+        if (k == 0) return list;
+
+        int distFromStart = length - k - 1;
+        runner = list;
+        while (distFromStart-- > 0) {
+            runner = runner.next;
+        }
+
+        ListNode<Integer> newStart = runner.next;
+        runner.next = null;
+        tailNode.next = list;
+
+        return newStart;
+    }
+
+    public static void main(String[] args) {
+        System.exit(
+                GenericTest
+                        .runFromAnnotations(args, "ListCyclicRightShift.java",
+                                new Object() {
+                                }.getClass().getEnclosingClass())
+                        .ordinal());
+    }
 }
