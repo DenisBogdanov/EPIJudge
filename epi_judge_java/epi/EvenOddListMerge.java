ea68766559a046ab7b42f4e954127bb2032a1b8a
Denis Bogdanov <bogdanov.math@gmail.com>
2023-03-27 07:26:50 +0400

    Java.

--- epi_judge_java/epi/EvenOddListMerge.java
+++ epi_judge_java/epi/EvenOddListMerge.java
@@ -1,21 +1,45 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 
-import java.util.List;
 public class EvenOddListMerge {
-  @EpiTest(testDataFile = "even_odd_list_merge.tsv")
 
-  public static ListNode<Integer> evenOddMerge(ListNode<Integer> L) {
-    // TODO - you fill in here.
-    return null;
-  }
+    @EpiTest(testDataFile = "even_odd_list_merge.tsv")
+    public static ListNode<Integer> evenOddMerge(ListNode<Integer> list) {
+        if (list == null || list.next == null) return list;
 
-  public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "EvenOddListMerge.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
-  }
+        var even = list;
+        var odd = list.next;
+
+        var evenRunner = list;
+        var oddRunner = list.next;
+        var runner = list.next.next;
+
+        while (runner != null) {
+            evenRunner.next = runner;
+            evenRunner = evenRunner.next;
+            runner = runner.next;
+
+            if (runner != null) {
+                oddRunner.next = runner;
+                oddRunner = oddRunner.next;
+                runner = runner.next;
+            }
+        }
+
+        oddRunner.next = null;
+        evenRunner.next = odd;
+
+        return even;
+    }
+
+    public static void main(String[] args) {
+        System.exit(
+                GenericTest
+                        .runFromAnnotations(args, "EvenOddListMerge.java",
+                                new Object() {
+                                }.getClass().getEnclosingClass())
+                        .ordinal());
+    }
 }
