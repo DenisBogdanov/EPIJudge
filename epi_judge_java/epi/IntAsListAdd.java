d91077a33d6ed97952a3b75a6a30889304bd9102
Denis Bogdanov <bogdanov.math@gmail.com>
2023-03-27 08:15:35 +0400

    Java.

--- epi_judge_java/epi/IntAsListAdd.java
+++ epi_judge_java/epi/IntAsListAdd.java
@@ -1,20 +1,52 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
+
 public class IntAsListAdd {
-  @EpiTest(testDataFile = "int_as_list_add.tsv")
 
-  public static ListNode<Integer> addTwoNumbers(ListNode<Integer> L1,
-                                                ListNode<Integer> L2) {
-    // TODO - you fill in here.
-    return null;
-  }
+    @EpiTest(testDataFile = "int_as_list_add.tsv")
+    public static ListNode<Integer> addTwoNumbers(ListNode<Integer> list1, ListNode<Integer> list2) {
+        var dummy = new ListNode<>(-1, null);
+        var runner = dummy;
+        int carry = 0;
 
-  public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "IntAsListAdd.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
-  }
+        while (list1 != null || list2 != null) {
+            int sum = 0;
+            if (list1 != null) {
+                sum += list1.data;
+                list1 = list1.next;
+            }
+
+            if (list2 != null) {
+                sum += list2.data;
+                list2 = list2.next;
+            }
+
+            sum += carry;
+            carry = 0;
+            if (sum > 9) {
+                carry = 1;
+                sum %= 10;
+            }
+
+            runner.next = new ListNode<>(sum, null);
+            runner = runner.next;
+        }
+
+        if (carry == 1) {
+            runner.next = new ListNode<>(1, null);
+        }
+
+        return dummy.next;
+    }
+
+    public static void main(String[] args) {
+        System.exit(
+                GenericTest
+                        .runFromAnnotations(args, "IntAsListAdd.java",
+                                new Object() {
+                                }.getClass().getEnclosingClass())
+                        .ordinal());
+    }
 }
