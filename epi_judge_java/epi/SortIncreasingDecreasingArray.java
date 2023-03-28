6bba2c30d2b78343a0c2d84b939c8a3509f8a961
Denis Bogdanov <bogdanov.math@gmail.com>
2023-03-28 08:11:51 +0400

    Java.

--- epi_judge_java/epi/SortIncreasingDecreasingArray.java
+++ epi_judge_java/epi/SortIncreasingDecreasingArray.java
@@ -1,21 +1,53 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+
 public class SortIncreasingDecreasingArray {
-  @EpiTest(testDataFile = "sort_increasing_decreasing_array.tsv")
 
-  public static List<Integer> sortKIncreasingDecreasingArray(List<Integer> A) {
-    // TODO - you fill in here.
-    return null;
-  }
+    @EpiTest(testDataFile = "sort_increasing_decreasing_array.tsv")
+    public static List<Integer> sortKIncreasingDecreasingArray(List<Integer> list) {
+        List<List<Integer>> lists = new ArrayList<>();
 
-  public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "SortIncreasingDecreasingArray.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
-  }
+        int prev = Integer.MIN_VALUE;
+        List<Integer> currList = new ArrayList<>();
+        boolean isIncreasing = true;
+
+        for (Integer num : list) {
+            if (isIncreasing && num < prev) {
+                lists.add(currList);
+                isIncreasing = false;
+                currList = new ArrayList<>();
+            } else if (!isIncreasing && num > prev) {
+                Collections.reverse(currList);
+                lists.add(currList);
+                isIncreasing = true;
+                currList = new ArrayList<>();
+            }
+
+            prev = num;
+            currList.add(num);
+        }
+
+        if (!isIncreasing) {
+            Collections.reverse(currList);
+        }
+
+        lists.add(currList);
+
+        return SortedArraysMerge.mergeSortedArrays(lists);
+    }
+
+    public static void main(String[] args) {
+        System.exit(
+                GenericTest
+                        .runFromAnnotations(args, "SortIncreasingDecreasingArray.java",
+                                new Object() {
+                                }.getClass().getEnclosingClass())
+                        .ordinal());
+    }
 }
