68e3420121634453facfcb59bf494a8c189dcb6e
Denis Bogdanov <bogdanov.math@gmail.com>
2023-03-29 08:37:24 +0400

    Java.

--- epi_judge_java/epi/IntSquareRoot.java
+++ epi_judge_java/epi/IntSquareRoot.java
@@ -1,19 +1,37 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
+
 public class IntSquareRoot {
-  @EpiTest(testDataFile = "int_square_root.tsv")
 
-  public static int squareRoot(int k) {
-    // TODO - you fill in here.
-    return 0;
-  }
+    @EpiTest(testDataFile = "int_square_root.tsv")
+    public static int squareRoot(int k) {
+        long left = 0;
+        long right = (long) k + 1;
 
-  public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "IntSquareRoot.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
-  }
+        while (left + 1 < right) {
+            long mid = left + (right - left) / 2;
+
+            long square = mid * mid;
+            if (square == k) {
+                return (int) mid;
+            } else if (square < k) {
+                left = mid;
+            } else {
+                right = mid;
+            }
+        }
+
+        return (int) left;
+    }
+
+    public static void main(String[] args) {
+        System.exit(
+                GenericTest
+                        .runFromAnnotations(args, "IntSquareRoot.java",
+                                new Object() {
+                                }.getClass().getEnclosingClass())
+                        .ordinal());
+    }
 }
