25e97d1f5fd4cd9fb640e3b6e362d83f76d29bea
Denis Bogdanov <bogdanov.math@gmail.com>
2023-03-29 08:32:47 +0400

    Java.

--- epi_judge_java/epi/SearchShiftedSortedArray.java
+++ epi_judge_java/epi/SearchShiftedSortedArray.java
@@ -1,21 +1,40 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 
 import java.util.List;
+
 public class SearchShiftedSortedArray {
-  @EpiTest(testDataFile = "search_shifted_sorted_array.tsv")
 
-  public static int searchSmallest(List<Integer> A) {
-    // TODO - you fill in here.
-    return 0;
-  }
+    @EpiTest(testDataFile = "search_shifted_sorted_array.tsv")
+    public static int searchSmallest(List<Integer> list) {
+        int left = 0;
+        int right = list.size() - 1;
 
-  public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "SearchShiftedSortedArray.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
-  }
+        if (list.get(left) < list.get(right)) return left;
+
+        while (left + 1 < right) {
+            int midIndex = left + (right - left) / 2;
+
+            if (list.get(midIndex) > list.get(left)) {
+                left = midIndex;
+            } else if (list.get(midIndex) < list.get(left)) {
+                right = midIndex;
+            } else {
+                return list.get(left) < list.get(right) ? left : right;
+            }
+        }
+
+        return right;
+    }
+
+    public static void main(String[] args) {
+        System.exit(
+                GenericTest
+                        .runFromAnnotations(args, "SearchShiftedSortedArray.java",
+                                new Object() {
+                                }.getClass().getEnclosingClass())
+                        .ordinal());
+    }
 }
