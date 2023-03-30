b35af1344bf0a48289ed281792132dcb2e7bc618
Denis Bogdanov <bogdanov.math@gmail.com>
2023-03-30 08:12:48 +0400

    Java.

--- epi_judge_java/epi/SearchRowColSortedMatrix.java
+++ epi_judge_java/epi/SearchRowColSortedMatrix.java
@@ -1,21 +1,39 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 
 import java.util.List;
+
 public class SearchRowColSortedMatrix {
-  @EpiTest(testDataFile = "search_row_col_sorted_matrix.tsv")
 
-  public static boolean matrixSearch(List<List<Integer>> A, int x) {
-    // TODO - you fill in here.
-    return true;
-  }
+    @EpiTest(testDataFile = "search_row_col_sorted_matrix.tsv")
+    public static boolean matrixSearch(List<List<Integer>> matrix, int x) {
+        int rows = matrix.size();
+        int columns = matrix.get(0).size();
 
-  public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "SearchRowColSortedMatrix.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
-  }
+        int currRow = rows - 1;
+        int currCol = 0;
+
+        while (currRow >= 0 && currCol < columns) {
+            if (matrix.get(currRow).get(currCol) == x) {
+                return true;
+            } else if (matrix.get(currRow).get(currCol) < x) {
+                currCol++;
+            } else {
+                currRow--;
+            }
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+        System.exit(
+                GenericTest
+                        .runFromAnnotations(args, "SearchRowColSortedMatrix.java",
+                                new Object() {
+                                }.getClass().getEnclosingClass())
+                        .ordinal());
+    }
 }
