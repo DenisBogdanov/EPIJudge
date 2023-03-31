428611bdd935e5f9fe9de23cff5535610acc2173
Denis Bogdanov <bogdanov.math@gmail.com>
2023-03-31 08:47:55 +0400

    Java.

--- epi_judge_java/epi/NearestRepeatedEntries.java
+++ epi_judge_java/epi/NearestRepeatedEntries.java
@@ -1,21 +1,36 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
 public class NearestRepeatedEntries {
-  @EpiTest(testDataFile = "nearest_repeated_entries.tsv")
 
-  public static int findNearestRepetition(List<String> paragraph) {
-    // TODO - you fill in here.
-    return 0;
-  }
+    @EpiTest(testDataFile = "nearest_repeated_entries.tsv")
+    public static int findNearestRepetition(List<String> paragraph) {
+        Map<String, Integer> wordsToLastIndexMap = new HashMap<>();
+        int result = Integer.MAX_VALUE;
 
-  public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "NearestRepeatedEntries.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
-  }
+        for (int i = 0; i < paragraph.size(); i++) {
+            if (wordsToLastIndexMap.containsKey(paragraph.get(i))) {
+                result = Math.min(result, i - wordsToLastIndexMap.get(paragraph.get(i)));
+            }
+
+            wordsToLastIndexMap.put(paragraph.get(i), i);
+        }
+
+        return result == Integer.MAX_VALUE ? -1 : result;
+    }
+
+    public static void main(String[] args) {
+        System.exit(
+                GenericTest
+                        .runFromAnnotations(args, "NearestRepeatedEntries.java",
+                                new Object() {
+                                }.getClass().getEnclosingClass())
+                        .ordinal());
+    }
 }
