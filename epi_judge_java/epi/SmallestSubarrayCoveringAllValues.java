edd0ad40065cefb3eb768396e1a62b10f4be41f7
Denis Bogdanov <bogdanov.math@gmail.com>
2023-04-03 08:24:32 +0400

    Java.

--- epi_judge_java/epi/SmallestSubarrayCoveringAllValues.java
+++ epi_judge_java/epi/SmallestSubarrayCoveringAllValues.java
@@ -5,14 +5,51 @@ import epi.test_framework.GenericTest;
 import epi.test_framework.TestFailure;
 import epi.test_framework.TimedExecutor;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class SmallestSubarrayCoveringAllValues {
+
     public static Subarray findSmallestSequentiallyCoveringSubset(List<String> paragraph,
                                                                   List<String> keywords) {
         Subarray result = new Subarray(-1, -1);
+        Map<String, Integer> keywordToIndexMap = new HashMap<>();
+        List<Integer> latestOccurrence = new ArrayList<>(keywords.size());
+        List<Integer> shortestSubarrayLength = new ArrayList<>(keywords.size());
 
-        return new Subarray(0, 0);
+        for (int i = 0; i < keywords.size(); i++) {
+            keywordToIndexMap.put(keywords.get(i), i);
+            latestOccurrence.add(-1);
+            shortestSubarrayLength.add(Integer.MAX_VALUE);
+        }
+
+        int shortestDistance = Integer.MAX_VALUE;
+
+        for (int i = 0; i < paragraph.size(); i++) {
+            Integer index = keywordToIndexMap.get(paragraph.get(i));
+            if (index == null) continue;
+
+            if (index == 0) {
+                shortestSubarrayLength.set(0, 1);
+            } else if (shortestSubarrayLength.get(index - 1) != Integer.MAX_VALUE) {
+                int distanceToPrev = i - latestOccurrence.get(index - 1);
+                shortestSubarrayLength.set(index, distanceToPrev + shortestSubarrayLength.get(index - 1));
+            }
+
+            latestOccurrence.set(index, i);
+
+            if (index == keywords.size() - 1
+                    && shortestSubarrayLength.get(shortestSubarrayLength.size() - 1) < shortestDistance) {
+
+                shortestDistance = shortestSubarrayLength.get(shortestSubarrayLength.size() - 1);
+                result.start = i - shortestDistance + 1;
+                result.end = i;
+            }
+        }
+
+        return result;
     }
 
     public static class Subarray {
