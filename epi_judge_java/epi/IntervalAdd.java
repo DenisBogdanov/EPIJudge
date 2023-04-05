5b6cc08c9c8a145aea6a59ab3ac8248b4a9ae0d0
Denis Bogdanov <bogdanov.math@gmail.com>
2023-04-05 07:38:13 +0400

    Java.

--- epi_judge_java/epi/IntervalAdd.java
+++ epi_judge_java/epi/IntervalAdd.java
@@ -1,57 +1,91 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.EpiUserType;
 import epi.test_framework.GenericTest;
 
 import java.util.ArrayList;
 import java.util.List;
+
 public class IntervalAdd {
-  @EpiUserType(ctorParams = {int.class, int.class})
 
-  public static class Interval {
-    public int left, right;
+    @EpiTest(testDataFile = "interval_add.tsv")
+    public static List<Interval> addInterval(List<Interval> disjointIntervals, Interval newInterval) {
+        List<Interval> result = new ArrayList<>();
 
-    public Interval(int l, int r) {
-      this.left = l;
-      this.right = r;
+        int currInterval = 0;
+        int n = disjointIntervals.size();
+
+        while (currInterval < n
+                && disjointIntervals.get(currInterval).right < newInterval.left) {
+
+            result.add(disjointIntervals.get(currInterval));
+            currInterval++;
+        }
+
+        if (currInterval == n) {
+            result.add(newInterval);
+            return result;
+        }
+
+        int newIntervalLeft = Math.min(disjointIntervals.get(currInterval).left, newInterval.left);
+        while (currInterval < n && disjointIntervals.get(currInterval).left <= newInterval.right) {
+            currInterval++;
+        }
+
+        int newIntervalRight = newInterval.right;
+        if (currInterval > 0) {
+            newIntervalRight = Math.max(disjointIntervals.get(currInterval - 1).right, newIntervalRight);
+        }
+
+        result.add(new Interval(newIntervalLeft, newIntervalRight));
+
+        while (currInterval < n) {
+            result.add(disjointIntervals.get(currInterval));
+            currInterval++;
+        }
+
+        return result;
     }
 
-    @Override
-    public boolean equals(Object o) {
-      if (this == o) {
-        return true;
-      }
-      if (o == null || getClass() != o.getClass()) {
-        return false;
-      }
+    @EpiUserType(ctorParams = {int.class, int.class})
+    public static class Interval {
+        public int left, right;
 
-      Interval interval = (Interval)o;
+        public Interval(int l, int r) {
+            this.left = l;
+            this.right = r;
+        }
 
-      if (left != interval.left) {
-        return false;
-      }
-      return right == interval.right;
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) {
+                return true;
+            }
+            if (o == null || getClass() != o.getClass()) {
+                return false;
+            }
+
+            Interval interval = (Interval) o;
+
+            if (left != interval.left) {
+                return false;
+            }
+            return right == interval.right;
+        }
+
+        @Override
+        public String toString() {
+            return "[" + left + ", " + right + "]";
+        }
     }
 
-    @Override
-    public String toString() {
-      return "[" + left + ", " + right + "]";
+    public static void main(String[] args) {
+        System.exit(
+                GenericTest
+                        .runFromAnnotations(args, "IntervalAdd.java",
+                                new Object() {
+                                }.getClass().getEnclosingClass())
+                        .ordinal());
     }
-  }
-
-  @EpiTest(testDataFile = "interval_add.tsv")
-
-  public static List<Interval> addInterval(List<Interval> disjointIntervals,
-                                           Interval newInterval) {
-    // TODO - you fill in here.
-    return null;
-  }
-
-  public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "IntervalAdd.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
-  }
 }
