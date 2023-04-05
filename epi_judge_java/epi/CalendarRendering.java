6041eac91cccfb0a72e834825f850d8e6b500470
Denis Bogdanov <bogdanov.math@gmail.com>
2023-04-05 07:09:53 +0400

    Java.

--- epi_judge_java/epi/CalendarRendering.java
+++ epi_judge_java/epi/CalendarRendering.java
@@ -1,43 +1,61 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.EpiUserType;
 import epi.test_framework.GenericTest;
 
 import java.util.List;
+
 public class CalendarRendering {
-  @EpiUserType(ctorParams = {int.class, int.class})
 
-  public static class Event {
-    public int start, finish;
+    @EpiTest(testDataFile = "calendar_rendering.tsv")
+    public static int findMaxSimultaneousEvents(List<Event> events) {
+        if (events == null || events.isEmpty()) return 0;
+        int maxTime = events.stream().mapToInt(e -> e.finish).max().getAsInt();
+        int[] concurrentEvents = new int[maxTime + 2];
+        for (Event event : events) {
+            concurrentEvents[event.start]++;
+            concurrentEvents[event.finish + 1]--;
+        }
 
-    public Event(int start, int finish) {
-      this.start = start;
-      this.finish = finish;
+        for (int i = 1; i < concurrentEvents.length; i++) {
+            concurrentEvents[i] += concurrentEvents[i - 1];
+        }
+
+        int result = 0;
+        for (int concurrentEvent : concurrentEvents) {
+            result = Math.max(result, concurrentEvent);
+        }
+
+        return result;
     }
-  }
 
-  private static class Endpoint {
-    public int time;
-    public boolean isStart;
+    @EpiUserType(ctorParams = {int.class, int.class})
+    public static class Event {
+        public int start, finish;
 
-    Endpoint(int time, boolean isStart) {
-      this.time = time;
-      this.isStart = isStart;
+        public Event(int start, int finish) {
+            this.start = start;
+            this.finish = finish;
+        }
     }
-  }
 
-  @EpiTest(testDataFile = "calendar_rendering.tsv")
+    private static class Endpoint {
+        public int time;
+        public boolean isStart;
 
-  public static int findMaxSimultaneousEvents(List<Event> A) {
-    // TODO - you fill in here.
-    return 0;
-  }
+        Endpoint(int time, boolean isStart) {
+            this.time = time;
+            this.isStart = isStart;
+        }
+    }
 
-  public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "CalendarRendering.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
-  }
+    public static void main(String[] args) {
+        System.exit(
+                GenericTest
+                        .runFromAnnotations(args, "CalendarRendering.java",
+                                new Object() {
+                                }.getClass().getEnclosingClass())
+                        .ordinal());
+    }
 }
