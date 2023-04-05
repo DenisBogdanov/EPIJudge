bc5200b2f32f95ade2ef4144ca9e757721da4ae1
Denis Bogdanov <bogdanov.math@gmail.com>
2023-04-05 07:24:37 +0400

    Java.

--- epi_judge_java/epi/SmallestNonconstructibleValue.java
+++ epi_judge_java/epi/SmallestNonconstructibleValue.java
@@ -1,21 +1,36 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 
+import java.util.Collections;
 import java.util.List;
+
 public class SmallestNonconstructibleValue {
-  @EpiTest(testDataFile = "smallest_nonconstructible_value.tsv")
 
-  public static int smallestNonconstructibleValue(List<Integer> A) {
-    // TODO - you fill in here.
-    return 0;
-  }
+    @EpiTest(testDataFile = "smallest_nonconstructible_value.tsv")
+    public static int smallestNonconstructibleValue(List<Integer> nums) {
+        if (nums == null || nums.isEmpty()) return 1;
+        Collections.sort(nums);
 
-  public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "SmallestNonconstructibleValue.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
-  }
+        int currSum = 0;
+        for (Integer num : nums) {
+            if (num > currSum + 1) {
+                return currSum + 1;
+            }
+
+            currSum += num;
+        }
+
+        return currSum + 1;
+    }
+
+    public static void main(String[] args) {
+        System.exit(
+                GenericTest
+                        .runFromAnnotations(args, "SmallestNonconstructibleValue.java",
+                                new Object() {
+                                }.getClass().getEnclosingClass())
+                        .ordinal());
+    }
 }
