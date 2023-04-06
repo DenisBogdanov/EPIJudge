94c004c208d26f4cba6de8341a760cdd94609a9b
Denis Bogdanov <bogdanov.math@gmail.com>
2023-04-06 07:52:21 +0400

    Java.

--- epi_judge_java/epi/IsStringPalindromic.java
+++ epi_judge_java/epi/IsStringPalindromic.java
@@ -1,19 +1,31 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
+
 public class IsStringPalindromic {
-  @EpiTest(testDataFile = "is_string_palindromic.tsv")
 
-  public static boolean isPalindromic(String s) {
-    // TODO - you fill in here.
-    return true;
-  }
+    @EpiTest(testDataFile = "is_string_palindromic.tsv")
+    public static boolean isPalindromic(String s) {
+        int left = 0;
+        int right = s.length() - 1;
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                return false;
+            }
 
-  public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "IsStringPalindromic.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
-  }
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        System.exit(
+                GenericTest
+                        .runFromAnnotations(args, "IsStringPalindromic.java",
+                                new Object() {
+                                }.getClass().getEnclosingClass())
+                        .ordinal());
+    }
 }
