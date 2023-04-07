6adde307d246c413010da5b7a2741114a86a136c
Denis Bogdanov <bogdanov.math@gmail.com>
2023-04-07 08:04:12 +0400

    Java.

--- epi_judge_java/epi/SpreadsheetEncoding.java
+++ epi_judge_java/epi/SpreadsheetEncoding.java
@@ -1,19 +1,27 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
+
 public class SpreadsheetEncoding {
-  @EpiTest(testDataFile = "spreadsheet_encoding.tsv")
 
-  public static int ssDecodeColID(final String col) {
-    // TODO - you fill in here.
-    return 0;
-  }
+    @EpiTest(testDataFile = "spreadsheet_encoding.tsv")
+    public static int ssDecodeColID(final String col) {
+        int result = 0;
+        for (char c : col.toCharArray()) {
+            result *= 26;
+            result += c - 'A' + 1;
+        }
 
-  public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "SpreadsheetEncoding.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
-  }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.exit(
+                GenericTest
+                        .runFromAnnotations(args, "SpreadsheetEncoding.java",
+                                new Object() {
+                                }.getClass().getEnclosingClass())
+                        .ordinal());
+    }
 }
