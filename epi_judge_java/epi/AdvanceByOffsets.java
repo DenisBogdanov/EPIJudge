3a6e67845cd17d70ca7555ced5fca071d5763d6e
Denis Bogdanov <bogdanov.math@gmail.com>
2023-04-10 08:18:04 +0400

    Java.

--- epi_judge_java/epi/AdvanceByOffsets.java
+++ epi_judge_java/epi/AdvanceByOffsets.java
@@ -1,20 +1,32 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 
 import java.util.List;
-public class AdvanceByOffsets {
-  @EpiTest(testDataFile = "advance_by_offsets.tsv")
-  public static boolean canReachEnd(List<Integer> maxAdvanceSteps) {
-    // TODO - you fill in here.
-    return true;
-  }
 
-  public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "AdvanceByOffsets.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
-  }
+public class AdvanceByOffsets {
+
+    @EpiTest(testDataFile = "advance_by_offsets.tsv")
+    public static boolean canReachEnd(List<Integer> maxAdvanceSteps) {
+        int n = maxAdvanceSteps.size();
+
+        int currMax = maxAdvanceSteps.get(0);
+        for (int i = 1; i < n; i++) {
+            if (i > currMax) return false;
+            currMax = Math.max(currMax, i + maxAdvanceSteps.get(i));
+            if (currMax >= n) return true;
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        System.exit(
+                GenericTest
+                        .runFromAnnotations(args, "AdvanceByOffsets.java",
+                                new Object() {
+                                }.getClass().getEnclosingClass())
+                        .ordinal());
+    }
 }
