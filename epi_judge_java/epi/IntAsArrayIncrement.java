179576e8074ff52f63b43db48eafb8d8c91c0f7b
Denis Bogdanov <bogdanov.math@gmail.com>
2023-04-10 08:01:34 +0400

    Java.

--- epi_judge_java/epi/IntAsArrayIncrement.java
+++ epi_judge_java/epi/IntAsArrayIncrement.java
@@ -1,20 +1,38 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 
 import java.util.List;
-public class IntAsArrayIncrement {
-  @EpiTest(testDataFile = "int_as_array_increment.tsv")
-  public static List<Integer> plusOne(List<Integer> A) {
-    // TODO - you fill in here.
-    return null;
-  }
 
-  public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "IntAsArrayIncrement.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
-  }
+public class IntAsArrayIncrement {
+
+    @EpiTest(testDataFile = "int_as_array_increment.tsv")
+    public static List<Integer> plusOne(List<Integer> list) {
+        int carry = 1;
+        for (int i = list.size() - 1; i >= 0; i--) {
+            int sum = list.get(i) + carry;
+            if (sum > 9) {
+                sum %= 10;
+                carry = 1;
+                list.set(i, sum);
+            } else {
+                list.set(i, sum);
+                return list;
+            }
+        }
+
+        list.add(0, 1);
+
+        return list;
+    }
+
+    public static void main(String[] args) {
+        System.exit(
+                GenericTest
+                        .runFromAnnotations(args, "IntAsArrayIncrement.java",
+                                new Object() {
+                                }.getClass().getEnclosingClass())
+                        .ordinal());
+    }
 }
