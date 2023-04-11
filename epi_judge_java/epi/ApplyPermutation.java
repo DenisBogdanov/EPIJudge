4906a90973a4bd12b62f312a9512874cc27ee669
Denis Bogdanov <bogdanov.math@gmail.com>
2023-04-11 07:40:34 +0400

    Java.

--- epi_judge_java/epi/ApplyPermutation.java
+++ epi_judge_java/epi/ApplyPermutation.java
@@ -3,17 +3,24 @@ package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 
+import java.util.Collections;
 import java.util.List;
 
 public class ApplyPermutation {
     public static void applyPermutation(List<Integer> perm, List<Integer> list) {
-        int[] result = new int[list.size()];
-        for (int i = 0; i < perm.size(); i++) {
-            result[perm.get(i)] = list.get(i);
+        int n = list.size();
+        for (int i = 0; i < n; i++) {
+            int next = i;
+            while (perm.get(next) >= 0) {
+                Collections.swap(list, i, perm.get(next));
+                int temp = perm.get(next);
+                perm.set(next, perm.get(next) - perm.size());
+                next = temp;
+            }
         }
 
-        for (int i = 0; i < list.size(); i++) {
-            list.set(i, result[i]);
+        for (int i = 0; i < n; i++) {
+            perm.set(i, perm.get(i) + n);
         }
     }
 
