361e4272bab3a5c566c9fc0b08b0b47d364b2308
Denis Bogdanov <bogdanov.math@gmail.com>
2023-04-12 08:29:32 +0400

    Java.

--- epi_judge_java/epi/KthLargestInArray.java
+++ epi_judge_java/epi/KthLargestInArray.java
@@ -3,9 +3,9 @@ package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 
+import java.util.Collections;
 import java.util.List;
-import java.util.PriorityQueue;
-import java.util.Queue;
+import java.util.Random;
 
 public class KthLargestInArray {
     // The numbering starts from one, i.e., if A = [3,1,-1,2] then
@@ -13,15 +13,39 @@ public class KthLargestInArray {
     // findKthLargest(3, A) returns 1, and findKthLargest(4, A) returns -1.
     @EpiTest(testDataFile = "kth_largest_in_array.tsv")
     public static int findKthLargest(int k, List<Integer> list) {
-        Queue<Integer> minHeap = new PriorityQueue<>();
-        for (Integer num : list) {
-            minHeap.offer(num);
-            if (minHeap.size() > k) {
-                minHeap.poll();
+        int n = list.size();
+        k = n - k;
+
+        Random random = new Random();
+
+        int left = 0;
+        int right = n - 1;
+
+        while (true) {
+            int pivotIndex = left + random.nextInt(right - left + 1);
+            int pivot = list.get(pivotIndex);
+
+            Collections.swap(list, pivotIndex, left);
+            int lessIndex = left + 1;
+
+            for (int i = left + 1; i <= right; i++) {
+                if (list.get(i) < pivot) {
+                    Collections.swap(list, i, lessIndex);
+                    lessIndex++;
+                }
+            }
+
+            lessIndex--;
+            Collections.swap(list, left, lessIndex);
+
+            if (lessIndex == k) {
+                return pivot;
+            } else if (lessIndex < k) {
+                left = lessIndex;
+            } else {
+                right = lessIndex;
             }
         }
-
-        return minHeap.poll();
     }
 
     public static void main(String[] args) {
