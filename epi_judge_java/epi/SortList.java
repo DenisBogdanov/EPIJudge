67906efbd6f72267d70e5522beb8a19cf06f5977
Denis Bogdanov <bogdanov.math@gmail.com>
2023-04-13 07:08:19 +0400

    Java.

--- epi_judge_java/epi/SortList.java
+++ epi_judge_java/epi/SortList.java
@@ -1,19 +1,35 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
+
 public class SortList {
-  @EpiTest(testDataFile = "sort_list.tsv")
 
-  public static ListNode<Integer> stableSortList(ListNode<Integer> L) {
-    // TODO - you fill in here.
-    return null;
-  }
+    @EpiTest(testDataFile = "sort_list.tsv")
+    public static ListNode<Integer> stableSortList(ListNode<Integer> list) {
+        if (list == null || list.next == null) return list;
 
-  public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "SortList.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
-  }
+        ListNode<Integer> preSLow = null;
+        ListNode<Integer> slow = list;
+        ListNode<Integer> fast = list;
+
+        while (fast != null && fast.next != null) {
+            preSLow = slow;
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+
+        preSLow.next = null;
+
+        return SortedListsMerge.mergeTwoSortedLists(stableSortList(list), stableSortList(slow));
+    }
+
+    public static void main(String[] args) {
+        System.exit(
+                GenericTest
+                        .runFromAnnotations(args, "SortList.java",
+                                new Object() {
+                                }.getClass().getEnclosingClass())
+                        .ordinal());
+    }
 }
