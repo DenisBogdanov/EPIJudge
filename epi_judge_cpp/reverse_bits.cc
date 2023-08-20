8e827a78d5c543a1e37fffc9375e0b74054d977a
Denis Bogdanov <bogdanov.math@gmail.com>
2023-08-20 13:47:27 +0400

    C++.

--- epi_judge_cpp/reverse_bits.cc
+++ epi_judge_cpp/reverse_bits.cc
@@ -1,12 +1,23 @@
 #include "test_framework/generic_test.h"
+
+void SwapBits(unsigned long long int *x, int i, int j);
+
 unsigned long long ReverseBits(unsigned long long x) {
-  // TODO - you fill in here.
-  return 0;
+    for (int i = 0; i < 32; ++i) {
+        SwapBits(&x, i, 64 - i - 1);
+    }
+
+    return x;
 }
 
-int main(int argc, char* argv[]) {
-  std::vector<std::string> args{argv + 1, argv + argc};
-  std::vector<std::string> param_names{"x"};
-  return GenericTestMain(args, "reverse_bits.cc", "reverse_bits.tsv",
-                         &ReverseBits, DefaultComparator{}, param_names);
+void SwapBits(unsigned long long int *x, int i, int j) {
+    if (((*x >> i) & 1) == ((*x >> j) & 1)) return;
+    *x ^= 1LL << i | 1LL << j;
+}
+
+int main(int argc, char *argv[]) {
+    std::vector<std::string> args{argv + 1, argv + argc};
+    std::vector<std::string> param_names{"x"};
+    return GenericTestMain(args, "reverse_bits.cc", "reverse_bits.tsv",
+                           &ReverseBits, DefaultComparator{}, param_names);
 }
