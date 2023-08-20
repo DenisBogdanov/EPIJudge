6880ca9543e63315959a7f16119bf359d1d96296
Denis Bogdanov <bogdanov.math@gmail.com>
2023-08-20 13:22:55 +0400

    C++.

--- epi_judge_cpp/count_bits.cc
+++ epi_judge_cpp/count_bits.cc
@@ -1,19 +1,19 @@
 #include "test_framework/generic_test.h"
 
 short CountBits(unsigned int x) {
-  int result = 0;
+    short result = 0;
 
-  while (x) {
-      result++;
-      x &= x - 1;
-  }
+    while (x) {
+        result++;
+        x &= x - 1;
+    }
 
-  return result;
+    return result;
 }
 
-int main(int argc, char* argv[]) {
-  std::vector<std::string> args{argv + 1, argv + argc};
-  std::vector<std::string> param_names{"x"};
-  return GenericTestMain(args, "count_bits.cc", "count_bits.tsv", &CountBits,
-                         DefaultComparator{}, param_names);
+int main(int argc, char *argv[]) {
+    std::vector<std::string> args{argv + 1, argv + argc};
+    std::vector<std::string> param_names{"x"};
+    return GenericTestMain(args, "count_bits.cc", "count_bits.tsv", &CountBits,
+                           DefaultComparator{}, param_names);
 }
