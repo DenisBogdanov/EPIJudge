185c0243cf28c1db1cd6d9b97e37e60df767c212
Denis Bogdanov <bogdanov.math@gmail.com>
2023-08-29 08:03:30 +0400

    C++.

--- epi_judge_cpp/reverse_digits.cc
+++ epi_judge_cpp/reverse_digits.cc
@@ -1,12 +1,22 @@
 #include "test_framework/generic_test.h"
+
 long long Reverse(int x) {
-  // TODO - you fill in here.
-  return 0;
+    int multiplier = x < 0 ? -1 : 1;
+    x = abs(x);
+
+    long long result = 0;
+    while (x) {
+        result *= 10;
+        result += x % 10;
+        x /= 10;
+    }
+
+    return result * multiplier;
 }
 
-int main(int argc, char* argv[]) {
-  std::vector<std::string> args{argv + 1, argv + argc};
-  std::vector<std::string> param_names{"x"};
-  return GenericTestMain(args, "reverse_digits.cc", "reverse_digits.tsv",
-                         &Reverse, DefaultComparator{}, param_names);
+int main(int argc, char *argv[]) {
+    std::vector<std::string> args{argv + 1, argv + argc};
+    std::vector<std::string> param_names{"x"};
+    return GenericTestMain(args, "reverse_digits.cc", "reverse_digits.tsv",
+                           &Reverse, DefaultComparator{}, param_names);
 }
