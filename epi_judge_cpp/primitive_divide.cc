aa434a2ad39019fab3ba6a1b61d7cc74c6613a3a
Denis Bogdanov <bogdanov.math@gmail.com>
2023-08-29 07:50:42 +0400

    C++.

--- epi_judge_cpp/primitive_divide.cc
+++ epi_judge_cpp/primitive_divide.cc
@@ -1,12 +1,30 @@
 #include "test_framework/generic_test.h"
+
 int Divide(int x, int y) {
-  // TODO - you fill in here.
-  return 0;
+    int result = 0;
+
+    int dividend = 0;
+
+    for (int shift = 30; shift >= 0; --shift) {
+        dividend <<= 1;
+        result <<= 1;
+
+        if (x & (1 << shift)) {
+            dividend |= 1;
+        }
+
+        if (dividend >= y) {
+            result |= 1;
+            dividend -= y;
+        }
+    }
+
+    return result;
 }
 
-int main(int argc, char* argv[]) {
-  std::vector<std::string> args{argv + 1, argv + argc};
-  std::vector<std::string> param_names{"x", "y"};
-  return GenericTestMain(args, "primitive_divide.cc", "primitive_divide.tsv",
-                         &Divide, DefaultComparator{}, param_names);
+int main(int argc, char *argv[]) {
+    std::vector<std::string> args{argv + 1, argv + argc};
+    std::vector<std::string> param_names{"x", "y"};
+    return GenericTestMain(args, "primitive_divide.cc", "primitive_divide.tsv",
+                           &Divide, DefaultComparator{}, param_names);
 }
