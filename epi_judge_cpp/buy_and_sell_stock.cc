f339810433d4a0fd52f7ceffac89547a13be9bb2
Denis Bogdanov <bogdanov.math@gmail.com>
2023-09-01 07:37:02 +0400

    C++.

--- epi_judge_cpp/buy_and_sell_stock.cc
+++ epi_judge_cpp/buy_and_sell_stock.cc
@@ -1,16 +1,25 @@
 #include <vector>
 
 #include "test_framework/generic_test.h"
+
 using std::vector;
-double BuyAndSellStockOnce(const vector<double>& prices) {
-  // TODO - you fill in here.
-  return 0.0;
+
+double BuyAndSellStockOnce(const vector<double> &prices) {
+    double minSoFar = prices[0];
+    double result = 0.0;
+
+    for (int i = 1; i < prices.size(); ++i) {
+        minSoFar = std::min(minSoFar, prices[i]);
+        result = std::max(result, prices[i] - minSoFar);
+    }
+
+    return result;
 }
 
-int main(int argc, char* argv[]) {
-  std::vector<std::string> args{argv + 1, argv + argc};
-  std::vector<std::string> param_names{"prices"};
-  return GenericTestMain(args, "buy_and_sell_stock.cc",
-                         "buy_and_sell_stock.tsv", &BuyAndSellStockOnce,
-                         DefaultComparator{}, param_names);
+int main(int argc, char *argv[]) {
+    std::vector<std::string> args{argv + 1, argv + argc};
+    std::vector<std::string> param_names{"prices"};
+    return GenericTestMain(args, "buy_and_sell_stock.cc",
+                           "buy_and_sell_stock.tsv", &BuyAndSellStockOnce,
+                           DefaultComparator{}, param_names);
 }
