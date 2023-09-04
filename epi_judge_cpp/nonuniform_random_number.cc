a33ae1a76b6bae12fd644b45b05a4d83ce2e6baf
Denis Bogdanov <bogdanov.math@gmail.com>
2023-09-04 08:44:46 +0400

    C++.

--- epi_judge_cpp/nonuniform_random_number.cc
+++ epi_judge_cpp/nonuniform_random_number.cc
@@ -2,63 +2,73 @@
 #include <functional>
 #include <unordered_map>
 #include <vector>
+#include <random>
 
 #include "test_framework/generic_test.h"
 #include "test_framework/random_sequence_checker.h"
 #include "test_framework/timed_executor.h"
-using std::abs;
-using std::bind;
-using std::unordered_map;
-using std::vector;
-int NonuniformRandomNumberGeneration(const vector<int>& values,
-                                     const vector<double>& probabilities) {
-  // TODO - you fill in here.
-  return 0;
+
+using namespace std;
+
+int NonuniformRandomNumberGeneration(const vector<int> &values,
+                                     const vector<double> &probabilities) {
+
+    default_random_engine seed((random_device()) ());
+    const double uniform_0_1 = generate_canonical<double, numeric_limits<double>::digits>(seed);
+
+    double currSum = 0.0;
+    for (int i = 0; i < probabilities.size(); i++) {
+        currSum += probabilities[i];
+        if (uniform_0_1 < currSum) return values[i];
+    }
+
+    return -1;
 }
+
 bool NonuniformRandomNumberGenerationRunner(
-    TimedExecutor& executor, const vector<int>& values,
-    const vector<double>& probabilities) {
-  constexpr int kN = 1000000;
-  vector<int> results;
+        TimedExecutor &executor, const vector<int> &values,
+        const vector<double> &probabilities) {
+    constexpr int kN = 1000000;
+    vector<int> results;
 
-  executor.Run([&] {
-    for (int i = 0; i < kN; ++i) {
-      results.emplace_back(
-          NonuniformRandomNumberGeneration(values, probabilities));
-    }
-  });
+    executor.Run([&] {
+        for (int i = 0; i < kN; ++i) {
+            results.emplace_back(
+                    NonuniformRandomNumberGeneration(values, probabilities));
+        }
+    });
 
-  unordered_map<int, int> counts;
-  for (int result : results) {
-    ++counts[result];
-  }
-  for (int i = 0; i < values.size(); ++i) {
-    const int v = values[i];
-    const double p = probabilities[i];
-    if (kN * p < 50 || kN * (1.0 - p) < 50) {
-      continue;
+    unordered_map<int, int> counts;
+    for (int result: results) {
+        ++counts[result];
     }
-    const double sigma = sqrt(kN * p * (1.0 - p));
-    if (abs(counts[v] - (p * kN)) > 5 * sigma) {
-      return false;
+    for (int i = 0; i < values.size(); ++i) {
+        const int v = values[i];
+        const double p = probabilities[i];
+        if (kN * p < 50 || kN * (1.0 - p) < 50) {
+            continue;
+        }
+        const double sigma = sqrt(kN * p * (1.0 - p));
+        if (abs(counts[v] - (p * kN)) > 5 * sigma) {
+            return false;
+        }
     }
-  }
-  return true;
+    return true;
 }
 
 void NonuniformRandomNumberGenerationWrapper(
-    TimedExecutor& executor, const vector<int>& values,
-    const vector<double>& probabilities) {
-  RunFuncWithRetries(bind(NonuniformRandomNumberGenerationRunner,
-                          std::ref(executor), std::cref(values),
-                          std::cref(probabilities)));
+        TimedExecutor &executor, const vector<int> &values,
+        const vector<double> &probabilities) {
+    RunFuncWithRetries(bind(NonuniformRandomNumberGenerationRunner,
+                            std::ref(executor), std::cref(values),
+                            std::cref(probabilities)));
 }
 
-int main(int argc, char* argv[]) {
-  std::vector<std::string> args{argv + 1, argv + argc};
-  std::vector<std::string> param_names{"executor", "values", "probabilities"};
-  return GenericTestMain(args, "nonuniform_random_number.cc",
-                         "nonuniform_random_number.tsv",
-                         &NonuniformRandomNumberGenerationWrapper,
-                         DefaultComparator{}, param_names);
+int main(int argc, char *argv[]) {
+    std::vector<std::string> args{argv + 1, argv + argc};
+    std::vector<std::string> param_names{"executor", "values", "probabilities"};
+    return GenericTestMain(args, "nonuniform_random_number.cc",
+                           "nonuniform_random_number.tsv",
+                           &NonuniformRandomNumberGenerationWrapper,
+                           DefaultComparator{}, param_names);
 }
