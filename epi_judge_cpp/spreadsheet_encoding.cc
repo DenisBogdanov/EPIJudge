4544020af5a7ee8bb2cc3ba415f70ee19df3d381
Denis Bogdanov <bogdanov.math@gmail.com>
2023-09-11 07:16:42 +0400

    C++.

--- epi_judge_cpp/spreadsheet_encoding.cc
+++ epi_judge_cpp/spreadsheet_encoding.cc
@@ -1,17 +1,23 @@
 #include <string>
 
 #include "test_framework/generic_test.h"
+
 using std::string;
 
-int SSDecodeColID(const string& col) {
-  // TODO - you fill in here.
-  return 0;
+int SSDecodeColID(const string &col) {
+    int result = 0;
+    for (char c: col) {
+        result *= 26;
+        result += c - 'A' + 1;
+    }
+
+    return result;
 }
 
-int main(int argc, char* argv[]) {
-  std::vector<std::string> args{argv + 1, argv + argc};
-  std::vector<std::string> param_names{"col"};
-  return GenericTestMain(args, "spreadsheet_encoding.cc",
-                         "spreadsheet_encoding.tsv", &SSDecodeColID,
-                         DefaultComparator{}, param_names);
+int main(int argc, char *argv[]) {
+    std::vector<std::string> args{argv + 1, argv + argc};
+    std::vector<std::string> param_names{"col"};
+    return GenericTestMain(args, "spreadsheet_encoding.cc",
+                           "spreadsheet_encoding.tsv", &SSDecodeColID,
+                           DefaultComparator{}, param_names);
 }
