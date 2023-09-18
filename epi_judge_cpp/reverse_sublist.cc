2e436d158c4bab0d5b0b06dc482638e1bf095a43
Denis Bogdanov <bogdanov.math@gmail.com>
2023-09-18 07:35:56 +0400

    C++.

--- epi_judge_cpp/reverse_sublist.cc
+++ epi_judge_cpp/reverse_sublist.cc
@@ -1,15 +1,32 @@
 #include "list_node.h"
 #include "test_framework/generic_test.h"
 
-shared_ptr<ListNode<int>> ReverseSublist(shared_ptr<ListNode<int>> L, int start,
-                                         int finish) {
-  // TODO - you fill in here.
-  return nullptr;
+shared_ptr<ListNode<int>> ReverseSublist(shared_ptr<ListNode<int>> L, int start, int finish) {
+    if (start == finish) return L;
+
+    auto dummy = make_shared<ListNode<int>>(ListNode<int>{-1, L});
+
+    auto beforeStartNode = dummy;
+    int k = 1;
+    while (k++ < start) {
+        beforeStartNode = beforeStartNode->next;
+    }
+
+    auto runner = beforeStartNode->next;
+
+    while (start++ < finish) {
+        auto temp = runner->next;
+        runner->next = temp->next;
+        temp->next = beforeStartNode->next;
+        beforeStartNode->next = temp;
+    }
+
+    return dummy->next;
 }
 
-int main(int argc, char* argv[]) {
-  std::vector<std::string> args{argv + 1, argv + argc};
-  std::vector<std::string> param_names{"L", "start", "finish"};
-  return GenericTestMain(args, "reverse_sublist.cc", "reverse_sublist.tsv",
-                         &ReverseSublist, DefaultComparator{}, param_names);
+int main(int argc, char *argv[]) {
+    std::vector<std::string> args{argv + 1, argv + argc};
+    std::vector<std::string> param_names{"L", "start", "finish"};
+    return GenericTestMain(args, "reverse_sublist.cc", "reverse_sublist.tsv",
+                           &ReverseSublist, DefaultComparator{}, param_names);
 }
