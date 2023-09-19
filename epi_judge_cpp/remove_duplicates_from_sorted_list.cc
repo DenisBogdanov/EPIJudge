5226c54be4ee791905ce726b0675c9e9bf981d3a
Denis Bogdanov <bogdanov.math@gmail.com>
2023-09-19 06:45:57 +0400

    C++.

--- epi_judge_cpp/remove_duplicates_from_sorted_list.cc
+++ epi_judge_cpp/remove_duplicates_from_sorted_list.cc
@@ -2,17 +2,28 @@
 
 #include "list_node.h"
 #include "test_framework/generic_test.h"
+
 using std::shared_ptr;
 
-shared_ptr<ListNode<int>> RemoveDuplicates(const shared_ptr<ListNode<int>>& L) {
-  // TODO - you fill in here.
-  return nullptr;
+shared_ptr<ListNode<int>> RemoveDuplicates(const shared_ptr<ListNode<int>> &list) {
+    if (list == nullptr) return list;
+
+    auto runner = list;
+    while (runner != nullptr && runner->next != nullptr) {
+        if (runner->data != runner->next->data) {
+            runner = runner->next;
+        } else {
+            runner->next = runner->next->next;
+        }
+    }
+
+    return list;
 }
 
-int main(int argc, char* argv[]) {
-  std::vector<std::string> args{argv + 1, argv + argc};
-  std::vector<std::string> param_names{"L"};
-  return GenericTestMain(args, "remove_duplicates_from_sorted_list.cc",
-                         "remove_duplicates_from_sorted_list.tsv",
-                         &RemoveDuplicates, DefaultComparator{}, param_names);
+int main(int argc, char *argv[]) {
+    std::vector<std::string> args{argv + 1, argv + argc};
+    std::vector<std::string> param_names{"L"};
+    return GenericTestMain(args, "remove_duplicates_from_sorted_list.cc",
+                           "remove_duplicates_from_sorted_list.tsv",
+                           &RemoveDuplicates, DefaultComparator{}, param_names);
 }
