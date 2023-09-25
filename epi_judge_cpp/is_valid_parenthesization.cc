f9668fbeac4fe1b9f375ccff97432fd3ba52dc50
Denis Bogdanov <bogdanov.math@gmail.com>
2023-09-25 07:44:41 +0400

    C++.

--- epi_judge_cpp/is_valid_parenthesization.cc
+++ epi_judge_cpp/is_valid_parenthesization.cc
@@ -1,16 +1,41 @@
 #include <string>
 
 #include "test_framework/generic_test.h"
+
 using std::string;
-bool IsWellFormed(const string& s) {
-  // TODO - you fill in here.
-  return true;
+
+bool IsWellFormed(const string &s) {
+    std::stack<char> st;
+    for (auto c: s) {
+        switch (c) {
+            case ')':
+                if (st.empty()) return false;
+                if (st.top() != '(') return false;
+                st.pop();
+                break;
+            case ']':
+                if (st.empty()) return false;
+                if (st.top() != '[') return false;
+                st.pop();
+                break;
+            case '}':
+                if (st.empty()) return false;
+                if (st.top() != '{') return false;
+                st.pop();
+                break;
+            default:
+                st.push(c);
+                break;
+        }
+    }
+
+    return st.empty();
 }
 
-int main(int argc, char* argv[]) {
-  std::vector<std::string> args{argv + 1, argv + argc};
-  std::vector<std::string> param_names{"s"};
-  return GenericTestMain(args, "is_valid_parenthesization.cc",
-                         "is_valid_parenthesization.tsv", &IsWellFormed,
-                         DefaultComparator{}, param_names);
+int main(int argc, char *argv[]) {
+    std::vector<std::string> args{argv + 1, argv + argc};
+    std::vector<std::string> param_names{"s"};
+    return GenericTestMain(args, "is_valid_parenthesization.cc",
+                           "is_valid_parenthesization.tsv", &IsWellFormed,
+                           DefaultComparator{}, param_names);
 }
