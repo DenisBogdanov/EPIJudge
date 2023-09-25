698fd17a142d3e206f9a67aab67c508430941ba1
Denis Bogdanov <bogdanov.math@gmail.com>
2023-09-25 08:22:33 +0400

    C++.

--- epi_judge_cpp/sunset_view.cc
+++ epi_judge_cpp/sunset_view.cc
@@ -2,21 +2,48 @@
 #include <vector>
 
 #include "test_framework/generic_test.h"
+
 using std::vector;
+using std::stack;
+
 vector<int> ExamineBuildingsWithSunset(
-    vector<int>::const_iterator sequence_begin,
-    const vector<int>::const_iterator& sequence_end) {
-  // TODO - you fill in here.
-  return {};
-}
-vector<int> ExamineBuildingsWithSunsetWrapper(const vector<int>& sequence) {
-  return ExamineBuildingsWithSunset(cbegin(sequence), cend(sequence));
+        vector<int>::const_iterator sequence_begin,
+        const vector<int>::const_iterator &sequence_end) {
+
+    struct Building {
+        int id;
+        int height;
+    };
+
+    stack<Building> order;
+    int index = 0;
+
+    while (sequence_begin != sequence_end) {
+        int building_height = *sequence_begin++;
+        while (!order.empty() && building_height >= order.top().height) {
+            order.pop();
+        }
+
+        order.emplace(Building{index++, building_height});
+    }
+
+    vector<int> result;
+    while (!order.empty()) {
+        result.emplace_back(order.top().id);
+        order.pop();
+    }
+
+    return result;
 }
 
-int main(int argc, char* argv[]) {
-  std::vector<std::string> args{argv + 1, argv + argc};
-  std::vector<std::string> param_names{"sequence"};
-  return GenericTestMain(args, "sunset_view.cc", "sunset_view.tsv",
-                         &ExamineBuildingsWithSunsetWrapper,
-                         DefaultComparator{}, param_names);
+vector<int> ExamineBuildingsWithSunsetWrapper(const vector<int> &sequence) {
+    return ExamineBuildingsWithSunset(cbegin(sequence), cend(sequence));
+}
+
+int main(int argc, char *argv[]) {
+    std::vector<std::string> args{argv + 1, argv + argc};
+    std::vector<std::string> param_names{"sequence"};
+    return GenericTestMain(args, "sunset_view.cc", "sunset_view.tsv",
+                           &ExamineBuildingsWithSunsetWrapper,
+                           DefaultComparator{}, param_names);
 }
