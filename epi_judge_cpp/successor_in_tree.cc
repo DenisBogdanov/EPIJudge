3e0c05786b2a4e9f16b4b446a648467f6d8c4ff1
Denis Bogdanov <bogdanov.math@gmail.com>
2023-10-02 22:22:27 +0400

    C++.

--- epi_judge_cpp/successor_in_tree.cc
+++ epi_judge_cpp/successor_in_tree.cc
@@ -2,21 +2,40 @@
 #include "test_framework/binary_tree_utils.h"
 #include "test_framework/generic_test.h"
 
-BinaryTreeNode<int>* FindSuccessor(
-    const unique_ptr<BinaryTreeNode<int>>& node) {
-  // TODO - you fill in here.
-  return nullptr;
-}
-int FindSuccessorWrapper(const unique_ptr<BinaryTreeNode<int>>& tree,
-                         int node_idx) {
-  auto result = FindSuccessor(MustFindNode(tree, node_idx));
-  return result ? result->data : -1;
+BinaryTreeNode<int> *FindSuccessor(const unique_ptr<BinaryTreeNode<int>> &node) {
+
+    if (node->right != nullptr) {
+        auto *runner = node->right.get();
+        while (runner->left != nullptr) {
+            runner = runner->left.get();
+        }
+
+        return runner;
+    } else {
+        auto *parent = node->parent;
+        auto *curr = node.get();
+
+        while (parent != nullptr) {
+            if (parent->left.get() == curr) break;
+
+            curr = parent;
+            parent = parent->parent;
+        }
+
+        return parent;
+    }
 }
 
-int main(int argc, char* argv[]) {
-  std::vector<std::string> args{argv + 1, argv + argc};
-  std::vector<std::string> param_names{"tree", "node_idx"};
-  return GenericTestMain(args, "successor_in_tree.cc", "successor_in_tree.tsv",
-                         &FindSuccessorWrapper, DefaultComparator{},
-                         param_names);
+int FindSuccessorWrapper(const unique_ptr<BinaryTreeNode<int>> &tree,
+                         int node_idx) {
+    auto result = FindSuccessor(MustFindNode(tree, node_idx));
+    return result ? result->data : -1;
+}
+
+int main(int argc, char *argv[]) {
+    std::vector<std::string> args{argv + 1, argv + argc};
+    std::vector<std::string> param_names{"tree", "node_idx"};
+    return GenericTestMain(args, "successor_in_tree.cc", "successor_in_tree.tsv",
+                           &FindSuccessorWrapper, DefaultComparator{},
+                           param_names);
 }
