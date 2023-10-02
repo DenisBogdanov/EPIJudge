12a7750329252095cf006db21db659ebebfda2ef
Denis Bogdanov <bogdanov.math@gmail.com>
2023-10-02 20:33:53 +0400

    C++.

--- epi_judge_cpp/is_tree_balanced.cc
+++ epi_judge_cpp/is_tree_balanced.cc
@@ -1,13 +1,33 @@
 #include "binary_tree_node.h"
 #include "test_framework/generic_test.h"
-bool IsBalanced(const unique_ptr<BinaryTreeNode<int>>& tree) {
-  // TODO - you fill in here.
-  return true;
+
+struct NodeInfo {
+    bool isBalanced;
+    int height;
+};
+
+NodeInfo calc(const unique_ptr<BinaryTreeNode<int>> &node);
+
+bool IsBalanced(const unique_ptr<BinaryTreeNode<int>> &tree) {
+    return calc(tree).isBalanced;
 }
 
-int main(int argc, char* argv[]) {
-  std::vector<std::string> args{argv + 1, argv + argc};
-  std::vector<std::string> param_names{"tree"};
-  return GenericTestMain(args, "is_tree_balanced.cc", "is_tree_balanced.tsv",
-                         &IsBalanced, DefaultComparator{}, param_names);
+NodeInfo calc(const unique_ptr<BinaryTreeNode<int>> &node) {
+    if (node == nullptr) return {true, 0};
+
+    auto leftInfo = calc(node->left);
+    if (!leftInfo.isBalanced) return {false, -1};
+
+    auto rightInfo = calc(node->right);
+    if (!rightInfo.isBalanced) return {false, -1};
+
+    if (abs(leftInfo.height - rightInfo.height) > 1) return {false, -1};
+    return {true, std::max(leftInfo.height, rightInfo.height) + 1};
+}
+
+int main(int argc, char *argv[]) {
+    std::vector<std::string> args{argv + 1, argv + argc};
+    std::vector<std::string> param_names{"tree"};
+    return GenericTestMain(args, "is_tree_balanced.cc", "is_tree_balanced.tsv",
+                           &IsBalanced, DefaultComparator{}, param_names);
 }
