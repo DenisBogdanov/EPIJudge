b82a4d0bfc8a7735b48ef79d0403d17303cfab13
Denis Bogdanov <bogdanov.math@gmail.com>
2023-10-02 21:37:41 +0400

    C++.

--- epi_judge_cpp/path_sum.cc
+++ epi_judge_cpp/path_sum.cc
@@ -2,17 +2,20 @@
 
 #include "binary_tree_node.h"
 #include "test_framework/generic_test.h"
+
 using std::unique_ptr;
 
-bool HasPathSum(const unique_ptr<BinaryTreeNode<int>>& tree,
-                int remaining_weight) {
-  // TODO - you fill in here.
-  return true;
+bool HasPathSum(const unique_ptr<BinaryTreeNode<int>> &tree, int remaining_weight) {
+    if (tree == nullptr) return false;
+    if (tree->left == nullptr && tree->right == nullptr) return remaining_weight == tree->data;
+
+    return HasPathSum(tree->left, remaining_weight - tree->data) ||
+           HasPathSum(tree->right, remaining_weight - tree->data);
 }
 
-int main(int argc, char* argv[]) {
-  std::vector<std::string> args{argv + 1, argv + argc};
-  std::vector<std::string> param_names{"tree", "remaining_weight"};
-  return GenericTestMain(args, "path_sum.cc", "path_sum.tsv", &HasPathSum,
-                         DefaultComparator{}, param_names);
+int main(int argc, char *argv[]) {
+    std::vector<std::string> args{argv + 1, argv + argc};
+    std::vector<std::string> param_names{"tree", "remaining_weight"};
+    return GenericTestMain(args, "path_sum.cc", "path_sum.tsv", &HasPathSum,
+                           DefaultComparator{}, param_names);
 }
