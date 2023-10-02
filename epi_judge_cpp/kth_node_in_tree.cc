d2bc3983c23b05ca185863de57234a1884116008
Denis Bogdanov <bogdanov.math@gmail.com>
2023-10-02 22:02:07 +0400

    C++.

--- epi_judge_cpp/kth_node_in_tree.cc
+++ epi_judge_cpp/kth_node_in_tree.cc
@@ -7,66 +7,86 @@
 #include "test_framework/generic_test.h"
 #include "test_framework/serialization_traits.h"
 #include "test_framework/test_failure.h"
+
 using std::unique_ptr;
+using std::stack;
+
 using test_framework::BinaryTreeSerializationTrait;
-template <typename T>
+template<typename T>
 struct BinaryTreeNode {
-  T data;
-  unique_ptr<BinaryTreeNode<T>> left, right;
-  int size;
+    T data;
+    unique_ptr<BinaryTreeNode<T>> left, right;
+    int size;
 };
 
-const BinaryTreeNode<int>* FindKthNodeBinaryTree(
-    const unique_ptr<BinaryTreeNode<int>>& tree, int k) {
-  // TODO - you fill in here.
-  return nullptr;
-}
-namespace test_framework {
-template <typename KeyT>
-struct SerializationTrait<std::unique_ptr<BinaryTreeNode<KeyT>>>
-    : BinaryTreeSerializationTrait<std::unique_ptr<BinaryTreeNode<KeyT>>,
-                                   false> {
-  using serialization_type = std::unique_ptr<BinaryTreeNode<KeyT>>;
-  using base =
-      BinaryTreeSerializationTrait<std::unique_ptr<BinaryTreeNode<KeyT>>,
-                                   false>;
+const BinaryTreeNode<int> *FindKthNodeBinaryTree(const unique_ptr<BinaryTreeNode<int>> &tree, int k) {
+    int count = 0;
+    stack<const BinaryTreeNode<int> *> st;
+    const auto *curr = tree.get();
 
-  static serialization_type Parse(const json& json_object) {
-    auto tree = base::Parse(json_object);
-    InitSize(tree);
-    return std::move(tree);
-  }
-
- private:
-  static int InitSize(const serialization_type& node) {
-    if (!node) {
-      return 0;
+    while (!st.empty() || curr) {
+        if (curr) {
+            st.push(curr);
+            curr = curr->left.get();
+        } else {
+            curr = st.top();
+            st.pop();
+            if (++count == k) return curr;
+            curr = curr->right.get();
+        }
     }
-    node->size = 1 + InitSize(node->left) + InitSize(node->right);
-    return node->size;
-  }
+
+    return nullptr;
+}
+
+namespace test_framework {
+    template<typename KeyT>
+    struct SerializationTrait<std::unique_ptr<BinaryTreeNode < KeyT>>>
+    : BinaryTreeSerializationTrait<std::unique_ptr<BinaryTreeNode < KeyT>>,
+    false> {
+    using serialization_type = std::unique_ptr<BinaryTreeNode < KeyT>>;
+    using base =
+            BinaryTreeSerializationTrait<std::unique_ptr<BinaryTreeNode < KeyT>>,
+    false>;
+
+    static serialization_type Parse(const json &json_object) {
+        auto tree = base::Parse(json_object);
+        InitSize(tree);
+        return std::move(tree);
+    }
+
+    private:
+
+    static int InitSize(const serialization_type &node) {
+        if (!node) {
+            return 0;
+        }
+        node->size = 1 + InitSize(node->left) + InitSize(node->right);
+        return node->size;
+    }
 };
 
 namespace meta {
-template <typename KeyT>
-struct IsBinaryTreeImpl<std::unique_ptr<BinaryTreeNode<KeyT>>>
-    : std::true_type {};
+    template<typename KeyT>
+    struct IsBinaryTreeImpl<std::unique_ptr<BinaryTreeNode<KeyT>>>
+            : std::true_type {
+    };
 }  // namespace meta
 }  // namespace test_framework
 
-int FindKthNodeBinaryTreeWrapper(const unique_ptr<BinaryTreeNode<int>>& tree,
+int FindKthNodeBinaryTreeWrapper(const unique_ptr<BinaryTreeNode<int>> &tree,
                                  int k) {
-  auto result = FindKthNodeBinaryTree(tree, k);
-  if (!result) {
-    throw TestFailure("Result can't be nullptr");
-  }
-  return result->data;
+    auto result = FindKthNodeBinaryTree(tree, k);
+    if (!result) {
+        throw TestFailure("Result can't be nullptr");
+    }
+    return result->data;
 }
 
-int main(int argc, char* argv[]) {
-  std::vector<std::string> args{argv + 1, argv + argc};
-  std::vector<std::string> param_names{"tree", "k"};
-  return GenericTestMain(args, "kth_node_in_tree.cc", "kth_node_in_tree.tsv",
-                         &FindKthNodeBinaryTreeWrapper, DefaultComparator{},
-                         param_names);
+int main(int argc, char *argv[]) {
+    std::vector<std::string> args{argv + 1, argv + argc};
+    std::vector<std::string> param_names{"tree", "k"};
+    return GenericTestMain(args, "kth_node_in_tree.cc", "kth_node_in_tree.tsv",
+                           &FindKthNodeBinaryTreeWrapper, DefaultComparator{},
+                           param_names);
 }
