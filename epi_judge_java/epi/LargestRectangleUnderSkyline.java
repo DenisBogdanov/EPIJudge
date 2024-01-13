eae3a53f88f345ceda6fb8a145532b7afc55a835
Denis Bogdanov <bogdanov.math@gmail.com>
2024-01-13 21:27:28 +0400

    Java.

--- epi_judge_java/epi/LargestRectangleUnderSkyline.java
+++ epi_judge_java/epi/LargestRectangleUnderSkyline.java
@@ -11,55 +11,17 @@ public class LargestRectangleUnderSkyline {
 
     @EpiTest(testDataFile = "largest_rectangle_under_skyline.tsv")
     public static int calculateLargestRectangle(List<Integer> heights) {
-        Deque<Building> buildings = new ArrayDeque<>();
+        Deque<Integer> indicesStack = new ArrayDeque<>();
 
         int result = 0;
 
         for (int i = 0; i < heights.size(); i++) {
             int currHeight = heights.get(i);
 
-            int indexOfRemoved = i;
 
-            while (!buildings.isEmpty()) {
-                Building peeked = buildings.peek();
-                if (peeked.height == currHeight) {
-                    result = Math.max(result, peeked.height * (i - peeked.index + 1));
-                    break;
-                } else if (peeked.height > currHeight) {
-                    Building popped = buildings.pop();
-                    indexOfRemoved = popped.index;
-                    result = Math.max(result, currHeight * (i - popped.index + 1));
-                } else {
-                    result = Math.max(result, peeked.height * (i - peeked.index + 1));
-                    buildings.push(new Building(indexOfRemoved, currHeight));
-                    break;
-                }
-            }
-
-            if (buildings.isEmpty()) {
-                buildings.push(new Building(0, currHeight));
-            }
         }
 
         return result;
-    }
-
-    private static class Building {
-        final int index;
-        final int height;
-
-        public Building(int index, int height) {
-            this.index = index;
-            this.height = height;
-        }
-
-        @Override
-        public String toString() {
-            return "Building{" +
-                    "index=" + index +
-                    ", height=" + height +
-                    '}';
-        }
     }
 
     public static void main(String[] args) {
