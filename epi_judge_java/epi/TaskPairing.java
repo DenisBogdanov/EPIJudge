9aca9165c42bf18dc9e2c4968a41dc47c18ea97c
Denis Bogdanov <bogdanov.math@gmail.com>
2024-01-13 19:36:34 +0400

    Java.

--- epi_judge_java/epi/TaskPairing.java
+++ epi_judge_java/epi/TaskPairing.java
@@ -1,54 +1,67 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.EpiUserType;
 import epi.test_framework.GenericTest;
 
+import java.util.ArrayList;
 import java.util.List;
+
 public class TaskPairing {
-  @EpiUserType(ctorParams = {Integer.class, Integer.class})
+    @EpiTest(testDataFile = "task_pairing.tsv")
+    public static List<PairedTasks> optimumTaskAssignment(List<Integer> taskDurations) {
+        int n = taskDurations.size();
+        List<PairedTasks> result = new ArrayList<>(n / 2);
 
-  public static class PairedTasks {
-    public Integer task1;
-    public Integer task2;
+        taskDurations.sort(null);
+        int left = 0;
+        int right = n - 1;
 
-    public PairedTasks(Integer task1, Integer task2) {
-      this.task1 = task1;
-      this.task2 = task2;
+        while (left < right) {
+            result.add(new PairedTasks(taskDurations.get(left), taskDurations.get(right)));
+            left++;
+            right--;
+        }
+
+        return result;
     }
 
-    @Override
-    public boolean equals(Object o) {
-      if (this == o) {
-        return true;
-      }
-      if (o == null || getClass() != o.getClass()) {
-        return false;
-      }
+    @EpiUserType(ctorParams = {Integer.class, Integer.class})
+    public static class PairedTasks {
+        public Integer task1;
+        public Integer task2;
 
-      PairedTasks that = (PairedTasks)o;
+        public PairedTasks(Integer task1, Integer task2) {
+            this.task1 = task1;
+            this.task2 = task2;
+        }
 
-      return task1.equals(that.task1) && task2.equals(that.task2);
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) {
+                return true;
+            }
+            if (o == null || getClass() != o.getClass()) {
+                return false;
+            }
+
+            PairedTasks that = (PairedTasks) o;
+
+            return task1.equals(that.task1) && task2.equals(that.task2);
+        }
+
+        @Override
+        public String toString() {
+            return "[" + task1 + ", " + task2 + "]";
+        }
     }
 
-    @Override
-    public String toString() {
-      return "[" + task1 + ", " + task2 + "]";
+    public static void main(String[] args) {
+        System.exit(
+                GenericTest
+                        .runFromAnnotations(args, "TaskPairing.java",
+                                new Object() {
+                                }.getClass().getEnclosingClass())
+                        .ordinal());
     }
-  }
-
-  @EpiTest(testDataFile = "task_pairing.tsv")
-
-  public static List<PairedTasks>
-  optimumTaskAssignment(List<Integer> taskDurations) {
-    // TODO - you fill in here.
-    return null;
-  }
-
-  public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "TaskPairing.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
-  }
 }
