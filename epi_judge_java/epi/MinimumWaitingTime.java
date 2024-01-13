60cda923ca0e41331b7455be6c3a304ad35feea0
Denis Bogdanov <bogdanov.math@gmail.com>
2024-01-13 19:45:01 +0400

    Java.

--- epi_judge_java/epi/MinimumWaitingTime.java
+++ epi_judge_java/epi/MinimumWaitingTime.java
@@ -1,21 +1,34 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 
 import java.util.List;
+
 public class MinimumWaitingTime {
-  @EpiTest(testDataFile = "minimum_waiting_time.tsv")
 
-  public static int minimumTotalWaitingTime(List<Integer> serviceTimes) {
-    // TODO - you fill in here.
-    return 0;
-  }
+    @EpiTest(testDataFile = "minimum_waiting_time.tsv")
+    public static int minimumTotalWaitingTime(List<Integer> serviceTimes) {
+        // 1  2  3  5
+        // 3 x 1 + 2 x 2 + 1 x 3
+        // 0  1  3  6
 
-  public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "MinimumWaitingTime.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
-  }
+        serviceTimes.sort(null);
+        int waitingTime = 0;
+        int n = serviceTimes.size();
+        for (int i = 0; i < n; i++) {
+            waitingTime += serviceTimes.get(i) * (n - i - 1);
+        }
+
+        return waitingTime;
+    }
+
+    public static void main(String[] args) {
+        System.exit(
+                GenericTest
+                        .runFromAnnotations(args, "MinimumWaitingTime.java",
+                                new Object() {
+                                }.getClass().getEnclosingClass())
+                        .ordinal());
+    }
 }
