44431b4cc9d2230dcc960f248bea08f595634d3d
Denis Bogdanov <bogdanov.math@gmail.com>
2024-01-13 14:48:05 +0400

    Java.

--- epi_judge_java/epi/IsValidParenthesization.java
+++ epi_judge_java/epi/IsValidParenthesization.java
@@ -1,19 +1,45 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 public class IsValidParenthesization {
-  @EpiTest(testDataFile = "is_valid_parenthesization.tsv")
 
-  public static boolean isWellFormed(String s) {
-    // TODO - you fill in here.
-    return true;
-  }
+    @EpiTest(testDataFile = "is_valid_parenthesization.tsv")
+    public static boolean isWellFormed(String s) {
+        Deque<Character> stack = new ArrayDeque<>();
+        for (char c : s.toCharArray()) {
+            switch (c) {
+                case '}':
+                    if (stack.isEmpty()) return false;
+                    if (stack.pop() != '{') return false;
+                    break;
+                case ']':
+                    if (stack.isEmpty()) return false;
+                    if (stack.pop() != '[') return false;
+                    break;
+                case ')':
+                    if (stack.isEmpty()) return false;
+                    if (stack.pop() != '(') return false;
+                    break;
+                default:
+                    stack.push(c);
+                    break;
+            }
+        }
 
-  public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "IsValidParenthesization.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
-  }
+        return stack.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        System.exit(
+                GenericTest
+                        .runFromAnnotations(args, "IsValidParenthesization.java",
+                                new Object() {
+                                }.getClass().getEnclosingClass())
+                        .ordinal());
+    }
 }
