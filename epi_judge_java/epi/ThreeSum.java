f07cbf6fd350009d9b940cd9a0bcbb5cc844bfe8
Denis Bogdanov <bogdanov.math@gmail.com>
2024-01-13 19:57:45 +0400

    Java.

--- epi_judge_java/epi/ThreeSum.java
+++ epi_judge_java/epi/ThreeSum.java
@@ -1,21 +1,42 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+
 public class ThreeSum {
-  @EpiTest(testDataFile = "three_sum.tsv")
 
-  public static boolean hasThreeSum(List<Integer> A, int t) {
-    // TODO - you fill in here.
-    return true;
-  }
+    @EpiTest(testDataFile = "three_sum.tsv")
+    public static boolean hasThreeSum(List<Integer> list, int t) {
+        Set<Integer> set = new HashSet<>(list);
+        if (t % 3 == 0 && set.contains(t / 3)) return true;
 
-  public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "ThreeSum.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
-  }
+        int n = list.size();
+        for (int i = 0; i < n; i++) {
+            int a = list.get(i);
+            if (set.contains(t - 2 * a) || ((t - a) % 2 == 0 && set.contains((t - a) / 2))) {
+                return true;
+            }
+
+            for (int j = i + 1; j < n; j++) {
+                if (set.contains(t - a - list.get(j))) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+        System.exit(
+                GenericTest
+                        .runFromAnnotations(args, "ThreeSum.java",
+                                new Object() {
+                                }.getClass().getEnclosingClass())
+                        .ordinal());
+    }
 }
