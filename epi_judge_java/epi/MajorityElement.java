ffe2dd384e02f8974e39d294c8b6d218ef728dd6
Denis Bogdanov <bogdanov.math@gmail.com>
2024-01-13 20:01:56 +0400

    Java.

--- epi_judge_java/epi/MajorityElement.java
+++ epi_judge_java/epi/MajorityElement.java
@@ -1,25 +1,44 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 
 import java.util.Iterator;
 import java.util.List;
+
 public class MajorityElement {
 
-  public static String majoritySearch(Iterator<String> stream) {
-    // TODO - you fill in here.
-    return "";
-  }
-  @EpiTest(testDataFile = "majority_element.tsv")
-  public static String majoritySearchWrapper(List<String> stream) {
-    return majoritySearch(stream.iterator());
-  }
+    public static String majoritySearch(Iterator<String> stream) {
+        int count = 0;
+        String result = stream.next();
 
-  public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "MajorityElement.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
-  }
+        while (stream.hasNext()) {
+            String next = stream.next();
+            if (next.equals(result)) {
+                count++;
+            } else {
+                count--;
+                if (count < 0) {
+                    result = next;
+                    count = 0;
+                }
+            }
+        }
+
+        return result;
+    }
+
+    @EpiTest(testDataFile = "majority_element.tsv")
+    public static String majoritySearchWrapper(List<String> stream) {
+        return majoritySearch(stream.iterator());
+    }
+
+    public static void main(String[] args) {
+        System.exit(
+                GenericTest
+                        .runFromAnnotations(args, "MajorityElement.java",
+                                new Object() {
+                                }.getClass().getEnclosingClass())
+                        .ordinal());
+    }
 }
