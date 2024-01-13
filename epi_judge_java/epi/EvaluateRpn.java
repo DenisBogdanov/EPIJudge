3d8372cd04977f59799d851d2888b93343fc8a4d
Denis Bogdanov <bogdanov.math@gmail.com>
2024-01-13 14:25:52 +0400

    Java.

--- epi_judge_java/epi/EvaluateRpn.java
+++ epi_judge_java/epi/EvaluateRpn.java
@@ -1,19 +1,50 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 public class EvaluateRpn {
-  @EpiTest(testDataFile = "evaluate_rpn.tsv")
 
-  public static int eval(String expression) {
-    // TODO - you fill in here.
-    return 0;
-  }
+    @EpiTest(testDataFile = "evaluate_rpn.tsv")
+    public static int eval(String expression) {
+        String[] symbols = expression.split(",");
 
-  public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "EvaluateRpn.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
-  }
+        Deque<Integer> stack = new ArrayDeque<>();
+
+        for (String symbol : symbols) {
+            switch (symbol) {
+                case "+":
+                    stack.push(stack.pop() + stack.pop());
+                    break;
+                case "-":
+                    Integer a = stack.pop();
+                    stack.push(stack.pop() - a);
+                    break;
+                case "*":
+                    stack.push(stack.pop() * stack.pop());
+                    break;
+                case "/":
+                    Integer b = stack.pop();
+                    stack.push(stack.pop() / b);
+                    break;
+                default:
+                    stack.push(Integer.parseInt(symbol));
+                    break;
+            }
+        }
+
+        return stack.pop();
+    }
+
+    public static void main(String[] args) {
+        System.exit(
+                GenericTest
+                        .runFromAnnotations(args, "EvaluateRpn.java",
+                                new Object() {
+                                }.getClass().getEnclosingClass())
+                        .ordinal());
+    }
 }
