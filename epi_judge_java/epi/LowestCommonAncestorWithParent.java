28e423e3041c6b6592826e249f540a373bc8a3a6
Denis Bogdanov <bogdanov.math@gmail.com>
2024-01-14 07:58:06 +0400

    Java.

--- epi_judge_java/epi/LowestCommonAncestorWithParent.java
+++ epi_judge_java/epi/LowestCommonAncestorWithParent.java
@@ -1,35 +1,65 @@
 package epi;
+
 import epi.test_framework.BinaryTreeUtils;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 import epi.test_framework.TestFailure;
 import epi.test_framework.TimedExecutor;
+
 public class LowestCommonAncestorWithParent {
 
-  public static BinaryTree<Integer> lca(BinaryTree<Integer> node0,
-                                        BinaryTree<Integer> node1) {
-    // TODO - you fill in here.
-    return null;
-  }
-  @EpiTest(testDataFile = "lowest_common_ancestor.tsv")
-  public static int lcaWrapper(TimedExecutor executor, BinaryTree<Integer> tree,
-                               Integer key0, Integer key1) throws Exception {
-    BinaryTree<Integer> node0 = BinaryTreeUtils.mustFindNode(tree, key0);
-    BinaryTree<Integer> node1 = BinaryTreeUtils.mustFindNode(tree, key1);
+    public static BinaryTree<Integer> lca(BinaryTree<Integer> node0, BinaryTree<Integer> node1) {
+        int depth0 = findDepth(node0);
+        int depth1 = findDepth(node1);
 
-    BinaryTree<Integer> result = executor.run(() -> lca(node0, node1));
+        if (depth0 > depth1) {
+            for (int i = 0; i < depth0 - depth1; i++) {
+                node0 = node0.parent;
+            }
+        } else {
+            for (int i = 0; i < depth1 - depth0; i++) {
+                node1 = node1.parent;
+            }
+        }
 
-    if (result == null) {
-      throw new TestFailure("Result can not be null");
+        while (node0 != node1) {
+            node0 = node0.parent;
+            node1 = node1.parent;
+        }
+
+        return node0;
     }
-    return result.data;
-  }
 
-  public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "LowestCommonAncestorWithParent.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
-  }
+    private static int findDepth(BinaryTree<Integer> node0) {
+        int result = 0;
+        while (node0 != null) {
+            result++;
+            node0 = node0.parent;
+        }
+
+        return result;
+    }
+
+    @EpiTest(testDataFile = "lowest_common_ancestor.tsv")
+    public static int lcaWrapper(TimedExecutor executor, BinaryTree<Integer> tree,
+                                 Integer key0, Integer key1) throws Exception {
+        BinaryTree<Integer> node0 = BinaryTreeUtils.mustFindNode(tree, key0);
+        BinaryTree<Integer> node1 = BinaryTreeUtils.mustFindNode(tree, key1);
+
+        BinaryTree<Integer> result = executor.run(() -> lca(node0, node1));
+
+        if (result == null) {
+            throw new TestFailure("Result can not be null");
+        }
+        return result.data;
+    }
+
+    public static void main(String[] args) {
+        System.exit(
+                GenericTest
+                        .runFromAnnotations(args, "LowestCommonAncestorWithParent.java",
+                                new Object() {
+                                }.getClass().getEnclosingClass())
+                        .ordinal());
+    }
 }
