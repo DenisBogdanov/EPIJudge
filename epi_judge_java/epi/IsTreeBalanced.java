e027b1bdc3fff52c10f02e8e6dfaec5c99f753eb
Denis Bogdanov <bogdanov.math@gmail.com>
2024-01-14 06:50:00 +0400

    Java.

--- epi_judge_java/epi/IsTreeBalanced.java
+++ epi_judge_java/epi/IsTreeBalanced.java
@@ -1,20 +1,46 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class IsTreeBalanced {
+    private static boolean isBalanced = true;
 
-  @EpiTest(testDataFile = "is_tree_balanced.tsv")
+    @EpiTest(testDataFile = "is_tree_balanced.tsv")
+    public static boolean isBalanced(BinaryTreeNode<Integer> tree) {
+        isBalanced = true;
+        Map<BinaryTreeNode<Integer>, Integer> nodeToHeightMap = new HashMap<>();
+        findHeights(tree, nodeToHeightMap);
+        return isBalanced;
+    }
 
-  public static boolean isBalanced(BinaryTreeNode<Integer> tree) {
-    // TODO - you fill in here.
-    return true;
-  }
+    private static int findHeights(BinaryTreeNode<Integer> tree, Map<BinaryTreeNode<Integer>, Integer> nodeToHeightMap) {
+        if (tree == null) return 0;
 
-  public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "IsTreeBalanced.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
-  }
+        Integer height = nodeToHeightMap.get(tree);
+        if (height != null) return height;
+
+        int left = findHeights(tree.left, nodeToHeightMap);
+        int right = findHeights(tree.right, nodeToHeightMap);
+
+        if (Math.abs(left - right) > 1) {
+            isBalanced = false;
+        }
+
+        height = Math.max(left, right) + 1;
+        nodeToHeightMap.put(tree, height);
+        return height;
+    }
+
+    public static void main(String[] args) {
+        System.exit(
+                GenericTest
+                        .runFromAnnotations(args, "IsTreeBalanced.java",
+                                new Object() {
+                                }.getClass().getEnclosingClass())
+                        .ordinal());
+    }
 }
