11d0136b601f8d002ab01cbde8d2529b8baf9a42
Denis Bogdanov <bogdanov.math@gmail.com>
2024-01-15 07:22:33 +0400

    Java.

--- epi_judge_java/epi/TreeFromPreorderWithNull.java
+++ epi_judge_java/epi/TreeFromPreorderWithNull.java
@@ -1,37 +1,53 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 import epi.test_framework.TimedExecutor;
 
 import java.util.ArrayList;
 import java.util.List;
+
 public class TreeFromPreorderWithNull {
-  public static BinaryTreeNode<Integer>
-  reconstructPreorder(List<Integer> preorder) {
-    // TODO - you fill in here.
-    return null;
-  }
-  @EpiTest(testDataFile = "tree_from_preorder_with_null.tsv")
-  public static BinaryTreeNode<Integer>
-  reconstructPreorderWrapper(TimedExecutor executor, List<String> strings)
-      throws Exception {
-    List<Integer> ints = new ArrayList<>();
-    for (String s : strings) {
-      if (s.equals("null")) {
-        ints.add(null);
-      } else {
-        ints.add(Integer.parseInt(s));
-      }
+    private static int index;
+
+    public static BinaryTreeNode<Integer> reconstructPreorder(List<Integer> preorder) {
+        index = 0;
+        return helper(preorder);
     }
 
-    return executor.run(() -> reconstructPreorder(ints));
-  }
+    private static BinaryTreeNode<Integer> helper(List<Integer> preorder) {
+        Integer num = preorder.get(index);
+        index++;
 
-  public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "TreeFromPreorderWithNull.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
-  }
+        if (num == null) return null;
+
+        BinaryTreeNode<Integer> left = helper(preorder);
+        BinaryTreeNode<Integer> right = helper(preorder);
+        return new BinaryTreeNode<>(num, left, right);
+    }
+
+    @EpiTest(testDataFile = "tree_from_preorder_with_null.tsv")
+    public static BinaryTreeNode<Integer>
+    reconstructPreorderWrapper(TimedExecutor executor, List<String> strings)
+            throws Exception {
+        List<Integer> ints = new ArrayList<>();
+        for (String s : strings) {
+            if (s.equals("null")) {
+                ints.add(null);
+            } else {
+                ints.add(Integer.parseInt(s));
+            }
+        }
+
+        return executor.run(() -> reconstructPreorder(ints));
+    }
+
+    public static void main(String[] args) {
+        System.exit(
+                GenericTest
+                        .runFromAnnotations(args, "TreeFromPreorderWithNull.java",
+                                new Object() {
+                                }.getClass().getEnclosingClass())
+                        .ordinal());
+    }
 }
