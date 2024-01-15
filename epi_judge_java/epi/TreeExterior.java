6bd89f3ce6115b1b90ea543bcaec68e17272a493
Denis Bogdanov <bogdanov.math@gmail.com>
2024-01-15 07:44:52 +0400

    Java.

--- epi_judge_java/epi/TreeExterior.java
+++ epi_judge_java/epi/TreeExterior.java
@@ -1,46 +1,85 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 import epi.test_framework.TestFailure;
 import epi.test_framework.TimedExecutor;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
+
 public class TreeExterior {
 
-  public static List<BinaryTreeNode<Integer>>
-  exteriorBinaryTree(BinaryTreeNode<Integer> tree) {
-    // TODO - you fill in here.
-    return Collections.emptyList();
-  }
-  private static List<Integer> createOutputList(List<BinaryTreeNode<Integer>> L)
-      throws TestFailure {
-    if (L.contains(null)) {
-      throw new TestFailure("Resulting list contains null");
+    public static List<BinaryTreeNode<Integer>> exteriorBinaryTree(BinaryTreeNode<Integer> tree) {
+        List<BinaryTreeNode<Integer>> result = new ArrayList<>();
+        if (tree == null) return result;
+
+        result.add(tree);
+        left(tree.left, result);
+        leaves(tree.left, result);
+        leaves(tree.right, result);
+        right(tree.right, result);
+
+        return result;
     }
-    List<Integer> output = new ArrayList<>();
-    for (BinaryTreeNode<Integer> l : L) {
-      output.add(l.data);
+
+    private static void left(BinaryTreeNode<Integer> node, List<BinaryTreeNode<Integer>> result) {
+        if (node == null || (node.left == null && node.right == null)) return;
+
+        result.add(node);
+
+        if (node.left != null) left(node.left, result);
+        else left(node.right, result);
     }
-    return output;
-  }
 
-  @EpiTest(testDataFile = "tree_exterior.tsv")
-  public static List<Integer>
-  exteriorBinaryTreeWrapper(TimedExecutor executor,
-                            BinaryTreeNode<Integer> tree) throws Exception {
-    List<BinaryTreeNode<Integer>> result =
-        executor.run(() -> exteriorBinaryTree(tree));
+    private static void leaves(BinaryTreeNode<Integer> node, List<BinaryTreeNode<Integer>> result) {
+        if (node == null) return;
 
-    return createOutputList(result);
-  }
+        if (node.left == null && node.right == null) {
+            result.add(node);
+        }
 
-  public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "TreeExterior.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
-  }
+        leaves(node.left, result);
+        leaves(node.right, result);
+    }
+
+    private static void right(BinaryTreeNode<Integer> node, List<BinaryTreeNode<Integer>> result) {
+        if (node == null || (node.left == null && node.right == null)) return;
+
+        if (node.right != null) right(node.right, result);
+        else right(node.left, result);
+
+        result.add(node);
+    }
+
+    private static List<Integer> createOutputList(List<BinaryTreeNode<Integer>> L)
+            throws TestFailure {
+        if (L.contains(null)) {
+            throw new TestFailure("Resulting list contains null");
+        }
+        List<Integer> output = new ArrayList<>();
+        for (BinaryTreeNode<Integer> l : L) {
+            output.add(l.data);
+        }
+        return output;
+    }
+
+    @EpiTest(testDataFile = "tree_exterior.tsv")
+    public static List<Integer>
+    exteriorBinaryTreeWrapper(TimedExecutor executor,
+                              BinaryTreeNode<Integer> tree) throws Exception {
+        List<BinaryTreeNode<Integer>> result =
+                executor.run(() -> exteriorBinaryTree(tree));
+
+        return createOutputList(result);
+    }
+
+    public static void main(String[] args) {
+        System.exit(
+                GenericTest
+                        .runFromAnnotations(args, "TreeExterior.java",
+                                new Object() {
+                                }.getClass().getEnclosingClass())
+                        .ordinal());
+    }
 }
