24a27fc3241a9d4d89ade3c46ac5c2a4171c2219
Denis Bogdanov <bogdanov.math@gmail.com>
2024-01-16 07:25:15 +0400

    Java.

--- epi_judge_java/epi/ReverseWords.java
+++ epi_judge_java/epi/ReverseWords.java
@@ -1,28 +1,59 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 import epi.test_framework.TimedExecutor;
+
 public class ReverseWords {
 
-  public static void reverseWords(char[] input) {
-    // TODO - you fill in here.
-    return;
-  }
-  @EpiTest(testDataFile = "reverse_words.tsv")
-  public static String reverseWordsWrapper(TimedExecutor executor, String s)
-      throws Exception {
-    char[] sCopy = s.toCharArray();
+    public static void reverseWords(char[] input) {
+        int n = input.length;
+        reverse(input, 0, n - 1);
+        int prev = 0;
+        while (prev < n && input[prev] == ' ') {
+            prev++;
+        }
 
-    executor.run(() -> reverseWords(sCopy));
+        for (int i = prev + 1; i < n; i++) {
+            if (input[i] == ' ') {
+                reverse(input, prev, i - 1);
+                prev = i + 1;
+            }
+        }
 
-    return String.valueOf(sCopy);
-  }
+        reverse(input, prev, n - 1);
+    }
 
-  public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "ReverseWords.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
-  }
+    private static void reverse(char[] arr, int left, int right) {
+        while (left < right) {
+            swap(arr, left, right);
+            left++;
+            right--;
+        }
+    }
+
+    private static void swap(char[] array, int a, int b) {
+        char temp = array[a];
+        array[a] = array[b];
+        array[b] = temp;
+    }
+
+    @EpiTest(testDataFile = "reverse_words.tsv")
+    public static String reverseWordsWrapper(TimedExecutor executor, String s)
+            throws Exception {
+        char[] sCopy = s.toCharArray();
+
+        executor.run(() -> reverseWords(sCopy));
+
+        return String.valueOf(sCopy);
+    }
+
+    public static void main(String[] args) {
+        System.exit(
+                GenericTest
+                        .runFromAnnotations(args, "ReverseWords.java",
+                                new Object() {
+                                }.getClass().getEnclosingClass())
+                        .ordinal());
+    }
 }
