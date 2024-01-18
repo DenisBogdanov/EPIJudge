5b4c80c08128af4f430559e676989ee83aa8d1c2
Denis Bogdanov <bogdanov.math@gmail.com>
2024-01-18 09:47:06 +0400

    Java.

--- epi_judge_java/epi/PhoneNumberMnemonic.java
+++ epi_judge_java/epi/PhoneNumberMnemonic.java
@@ -1,33 +1,55 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.EpiTestComparator;
 import epi.test_framework.GenericTest;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.function.BiPredicate;
+
 public class PhoneNumberMnemonic {
-  @EpiTest(testDataFile = "phone_number_mnemonic.tsv")
+    private static final String[] MAPPING = {"0", "1", "ABC", "DEF", "GHI",
+            "JKL", "MNO", "PQRS", "TUV", "WXYZ"};
 
-  public static List<String> phoneMnemonic(String phoneNumber) {
-    // TODO - you fill in here.
-    return null;
-  }
-  @EpiTestComparator
-  public static boolean comp(List<String> expected, List<String> result) {
-    if (result == null) {
-      return false;
+    @EpiTest(testDataFile = "phone_number_mnemonic.tsv")
+    public static List<String> phoneMnemonic(String phoneNumber) {
+        List<String> result = new ArrayList<>();
+
+        recur(phoneNumber, 0, new StringBuilder(), result);
+
+        return result;
     }
-    Collections.sort(expected);
-    Collections.sort(result);
-    return expected.equals(result);
-  }
 
-  public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "PhoneNumberMnemonic.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
-  }
+    private static void recur(String phoneNumber, int index, StringBuilder sb, List<String> result) {
+        if (sb.length() == phoneNumber.length()) {
+            result.add(sb.toString());
+        } else {
+            int mappingIndex = phoneNumber.charAt(index) - '0';
+            for (char c : MAPPING[mappingIndex].toCharArray()) {
+                sb.append(c);
+                recur(phoneNumber, index + 1, sb, result);
+                sb.setLength(sb.length() - 1);
+            }
+        }
+    }
+
+    @EpiTestComparator
+    public static boolean comp(List<String> expected, List<String> result) {
+        if (result == null) {
+            return false;
+        }
+        Collections.sort(expected);
+        Collections.sort(result);
+        return expected.equals(result);
+    }
+
+    public static void main(String[] args) {
+        System.exit(
+                GenericTest
+                        .runFromAnnotations(args, "PhoneNumberMnemonic.java",
+                                new Object() {
+                                }.getClass().getEnclosingClass())
+                        .ordinal());
+    }
 }
