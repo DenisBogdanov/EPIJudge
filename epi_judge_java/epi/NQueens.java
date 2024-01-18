4a491d9aeeb45ffac03a0c5b6154ef5db9963eb0
Denis Bogdanov <bogdanov.math@gmail.com>
2024-01-18 09:59:01 +0400

    Java.

--- epi_judge_java/epi/NQueens.java
+++ epi_judge_java/epi/NQueens.java
@@ -1,34 +1,65 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.EpiTestComparator;
 import epi.test_framework.GenericTest;
 import epi.test_framework.LexicographicalListComparator;
 
+import java.util.ArrayList;
 import java.util.List;
-import java.util.function.BiPredicate;
+
 public class NQueens {
-  @EpiTest(testDataFile = "n_queens.tsv")
 
-  public static List<List<Integer>> nQueens(int n) {
-    // TODO - you fill in here.
-    return null;
-  }
-  @EpiTestComparator
-  public static boolean comp(List<List<Integer>> expected,
-                             List<List<Integer>> result) {
-    if (result == null) {
-      return false;
+    @EpiTest(testDataFile = "n_queens.tsv")
+    public static List<List<Integer>> nQueens(int n) {
+        List<List<Integer>> result = new ArrayList<>();
+        recur(0, n, new ArrayList<>(), result);
+        return result;
     }
-    expected.sort(new LexicographicalListComparator<>());
-    result.sort(new LexicographicalListComparator<>());
-    return expected.equals(result);
-  }
 
-  public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "NQueens.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
-  }
+    private static void recur(int currColumn, int n, List<Integer> currCombination, List<List<Integer>> result) {
+        if (currColumn == n) {
+            result.add(new ArrayList<>(currCombination));
+        } else {
+            for (int row = 0; row < n; row++) {
+                if (ok(currCombination, row, n)) {
+                    currCombination.add(row);
+                    recur(currColumn + 1, n, currCombination, result);
+                    currCombination.remove(currCombination.size() - 1);
+                }
+            }
+        }
+    }
+
+    private static boolean ok(List<Integer> currCombination, int row, int n) {
+        int currColumn = currCombination.size();
+        for (int c = 0; c < currColumn; c++) {
+            int r = currCombination.get(c);
+            if (r == row) return false;
+            if (r - c == row - currColumn) return false;
+            if (r + c == row + currColumn) return false;
+        }
+
+        return true;
+    }
+
+    @EpiTestComparator
+    public static boolean comp(List<List<Integer>> expected,
+                               List<List<Integer>> result) {
+        if (result == null) {
+            return false;
+        }
+        expected.sort(new LexicographicalListComparator<>());
+        result.sort(new LexicographicalListComparator<>());
+        return expected.equals(result);
+    }
+
+    public static void main(String[] args) {
+        System.exit(
+                GenericTest
+                        .runFromAnnotations(args, "NQueens.java",
+                                new Object() {
+                                }.getClass().getEnclosingClass())
+                        .ordinal());
+    }
 }
