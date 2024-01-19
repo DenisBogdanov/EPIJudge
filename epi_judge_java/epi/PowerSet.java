9a683982d115395ee24b866cf5b8c9e43be471fe
Denis Bogdanov <bogdanov.math@gmail.com>
2024-01-19 10:49:09 +0400

    Java.

--- epi_judge_java/epi/PowerSet.java
+++ epi_judge_java/epi/PowerSet.java
@@ -1,40 +1,58 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.EpiTestComparator;
 import epi.test_framework.GenericTest;
 import epi.test_framework.LexicographicalListComparator;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+
 public class PowerSet {
-  @EpiTest(testDataFile = "power_set.tsv")
 
-  public static List<List<Integer>> generatePowerSet(List<Integer> inputSet) {
-    // TODO - you fill in here.
-    return null;
-  }
-  @EpiTestComparator
-  public static boolean comp(List<List<Integer>> expected,
-                             List<List<Integer>> result) {
-    if (result == null) {
-      return false;
-    }
-    for (List<Integer> l : expected) {
-      Collections.sort(l);
-    }
-    expected.sort(new LexicographicalListComparator<>());
-    for (List<Integer> l : result) {
-      Collections.sort(l);
-    }
-    result.sort(new LexicographicalListComparator<>());
-    return expected.equals(result);
-  }
+    @EpiTest(testDataFile = "power_set.tsv")
+    public static List<List<Integer>> generatePowerSet(List<Integer> inputSet) {
+        List<List<Integer>> result = new ArrayList<>();
+        int n = inputSet.size();
 
-  public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "PowerSet.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
-  }
+        for (int i = 0; i < (1 << n); i++) {
+            List<Integer> next = new ArrayList<>();
+            for (int j = 0; j < n; j++) {
+                if ((i & (1 << j)) != 0) {
+                    next.add(inputSet.get(j));
+                }
+            }
+
+            result.add(next);
+        }
+
+        return result;
+    }
+
+    @EpiTestComparator
+    public static boolean comp(List<List<Integer>> expected,
+                               List<List<Integer>> result) {
+        if (result == null) {
+            return false;
+        }
+        for (List<Integer> l : expected) {
+            Collections.sort(l);
+        }
+        expected.sort(new LexicographicalListComparator<>());
+        for (List<Integer> l : result) {
+            Collections.sort(l);
+        }
+        result.sort(new LexicographicalListComparator<>());
+        return expected.equals(result);
+    }
+
+    public static void main(String[] args) {
+        System.exit(
+                GenericTest
+                        .runFromAnnotations(args, "PowerSet.java",
+                                new Object() {
+                                }.getClass().getEnclosingClass())
+                        .ordinal());
+    }
 }
