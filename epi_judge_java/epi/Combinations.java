208c13092e082b42cd7ac6011ecd20c10d21a7af
Denis Bogdanov <bogdanov.math@gmail.com>
2024-01-19 20:12:50 +0400

    Java.

--- epi_judge_java/epi/Combinations.java
+++ epi_judge_java/epi/Combinations.java
@@ -1,35 +1,51 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.EpiTestComparator;
 import epi.test_framework.GenericTest;
 import epi.test_framework.LexicographicalListComparator;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.function.BiPredicate;
+
 public class Combinations {
-  @EpiTest(testDataFile = "combinations.tsv")
 
-  public static List<List<Integer>> combinations(int n, int k) {
-    // TODO - you fill in here.
-    return null;
-  }
-  @EpiTestComparator
-  public static boolean comp(List<List<Integer>> expected,
-                             List<List<Integer>> result) {
-    if (result == null) {
-      return false;
+    @EpiTest(testDataFile = "combinations.tsv")
+    public static List<List<Integer>> combinations(int n, int k) {
+        List<List<Integer>> result = new ArrayList<>();
+        for (int mask = 0; mask < (1 << n); mask++) {
+            if (Integer.bitCount(mask) == k) {
+                List<Integer> comb = new ArrayList<>();
+                for (int i = 0; i < n; i++) {
+                    if ((mask & (1 << i)) != 0) {
+                        comb.add(i + 1);
+                    }
+                }
+
+                result.add(comb);
+            }
+        }
+
+        return result;
     }
-    expected.sort(new LexicographicalListComparator<>());
-    result.sort(new LexicographicalListComparator<>());
-    return expected.equals(result);
-  }
 
-  public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "Combinations.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
-  }
+    @EpiTestComparator
+    public static boolean comp(List<List<Integer>> expected,
+                               List<List<Integer>> result) {
+        if (result == null) {
+            return false;
+        }
+        expected.sort(new LexicographicalListComparator<>());
+        result.sort(new LexicographicalListComparator<>());
+        return expected.equals(result);
+    }
+
+    public static void main(String[] args) {
+        System.exit(
+                GenericTest
+                        .runFromAnnotations(args, "Combinations.java",
+                                new Object() {
+                                }.getClass().getEnclosingClass())
+                        .ordinal());
+    }
 }
