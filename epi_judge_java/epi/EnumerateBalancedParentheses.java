ac161f3692713e55dbabf6f76b49681fa69d0c48
Denis Bogdanov <bogdanov.math@gmail.com>
2024-01-20 15:07:21 +0400

    Java.

--- epi_judge_java/epi/EnumerateBalancedParentheses.java
+++ epi_judge_java/epi/EnumerateBalancedParentheses.java
@@ -1,33 +1,54 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.EpiTestComparator;
 import epi.test_framework.GenericTest;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.function.BiPredicate;
+
 public class EnumerateBalancedParentheses {
-  @EpiTest(testDataFile = "enumerate_balanced_parentheses.tsv")
 
-  public static List<String> generateBalancedParentheses(int numPairs) {
-    // TODO - you fill in here.
-    return null;
-  }
-  @EpiTestComparator
-  public static boolean comp(List<String> expected, List<String> result) {
-    if (result == null) {
-      return false;
+    @EpiTest(testDataFile = "enumerate_balanced_parentheses.tsv")
+    public static List<String> generateBalancedParentheses(int numPairs) {
+        List<String> result = new ArrayList<>();
+
+        recur("", numPairs, numPairs, result);
+
+        return result;
     }
-    Collections.sort(expected);
-    Collections.sort(result);
-    return expected.equals(result);
-  }
 
-  public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "EnumerateBalancedParentheses.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
-  }
+    private static void recur(String prefix, int remainingOpened, int remainingClosed, List<String> result) {
+        if (remainingClosed == 0) {
+            result.add(prefix);
+        } else {
+            if (remainingClosed > remainingOpened) {
+                recur(prefix + ")", remainingOpened, remainingClosed - 1, result);
+            }
+
+            if (remainingOpened > 0) {
+                recur(prefix + "(", remainingOpened - 1, remainingClosed, result);
+            }
+        }
+    }
+
+    @EpiTestComparator
+    public static boolean comp(List<String> expected, List<String> result) {
+        if (result == null) {
+            return false;
+        }
+        Collections.sort(expected);
+        Collections.sort(result);
+        return expected.equals(result);
+    }
+
+    public static void main(String[] args) {
+        System.exit(
+                GenericTest
+                        .runFromAnnotations(args, "EnumerateBalancedParentheses.java",
+                                new Object() {
+                                }.getClass().getEnclosingClass())
+                        .ordinal());
+    }
 }
