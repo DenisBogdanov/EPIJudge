4465b2025f77dad58745ba912f39739f7de8698f
Denis Bogdanov <bogdanov.math@gmail.com>
2024-01-22 18:38:15 +0400

    Java.

--- epi_judge_java/epi/TwoSortedArraysMerge.java
+++ epi_judge_java/epi/TwoSortedArraysMerge.java
@@ -1,27 +1,49 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 
 import java.util.List;
+
 public class TwoSortedArraysMerge {
 
-  public static void mergeTwoSortedArrays(List<Integer> A, int m,
-                                          List<Integer> B, int n) {
-    // TODO - you fill in here.
-    return;
-  }
-  @EpiTest(testDataFile = "two_sorted_arrays_merge.tsv")
-  public static List<Integer>
-  mergeTwoSortedArraysWrapper(List<Integer> A, int m, List<Integer> B, int n) {
-    mergeTwoSortedArrays(A, m, B, n);
-    return A;
-  }
+    public static void mergeTwoSortedArrays(List<Integer> a, int m, List<Integer> b, int n) {
+        int currLast = m + n - 1;
+        int i = m - 1;
+        int j = n - 1;
 
-  public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "TwoSortedArraysMerge.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
-  }
+        while (i >= 0 || j >= 0) {
+            if (j == -1) {
+                a.set(currLast, a.get(i));
+                i--;
+            } else if (i == -1) {
+                a.set(currLast, b.get(j));
+                j--;
+            } else if (a.get(i) >= b.get(j)) {
+                a.set(currLast, a.get(i));
+                i--;
+            } else {
+                a.set(currLast, b.get(j));
+                j--;
+            }
+
+            currLast--;
+        }
+    }
+
+    @EpiTest(testDataFile = "two_sorted_arrays_merge.tsv")
+    public static List<Integer>
+    mergeTwoSortedArraysWrapper(List<Integer> A, int m, List<Integer> B, int n) {
+        mergeTwoSortedArrays(A, m, B, n);
+        return A;
+    }
+
+    public static void main(String[] args) {
+        System.exit(
+                GenericTest
+                        .runFromAnnotations(args, "TwoSortedArraysMerge.java",
+                                new Object() {
+                                }.getClass().getEnclosingClass())
+                        .ordinal());
+    }
 }
