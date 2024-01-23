9944bff2d8786cb9e840ef501659b5b46fc40e99
Denis Bogdanov <bogdanov.math@gmail.com>
2024-01-23 13:55:08 +0400

    Java.

--- epi_judge_java/epi/CalendarRendering.java
+++ epi_judge_java/epi/CalendarRendering.java
@@ -1,43 +1,61 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.EpiUserType;
 import epi.test_framework.GenericTest;
 
 import java.util.List;
+
 public class CalendarRendering {
-  @EpiUserType(ctorParams = {int.class, int.class})
 
-  public static class Event {
-    public int start, finish;
+    @EpiTest(testDataFile = "calendar_rendering.tsv")
+    public static int findMaxSimultaneousEvents(List<Event> events) {
+        int latestEnd = 0;
+        for (Event event : events) {
+            latestEnd = Math.max(latestEnd, event.finish);
+        }
 
-    public Event(int start, int finish) {
-      this.start = start;
-      this.finish = finish;
+        int[] amountOfEventsByDay = new int[latestEnd + 2];
+        for (Event event : events) {
+            amountOfEventsByDay[event.start]++;
+            amountOfEventsByDay[event.finish + 1]--;
+        }
+
+        int result = amountOfEventsByDay[0];
+        for (int i = 1; i < amountOfEventsByDay.length; i++) {
+            amountOfEventsByDay[i] += amountOfEventsByDay[i - 1];
+            result = Math.max(result, amountOfEventsByDay[i]);
+        }
+
+        return result;
     }
-  }
 
-  private static class Endpoint {
-    public int time;
-    public boolean isStart;
+    @EpiUserType(ctorParams = {int.class, int.class})
+    public static class Event {
+        public int start, finish;
 
-    Endpoint(int time, boolean isStart) {
-      this.time = time;
-      this.isStart = isStart;
+        public Event(int start, int finish) {
+            this.start = start;
+            this.finish = finish;
+        }
     }
-  }
 
-  @EpiTest(testDataFile = "calendar_rendering.tsv")
+    private static class Endpoint {
+        public int time;
+        public boolean isStart;
 
-  public static int findMaxSimultaneousEvents(List<Event> A) {
-    // TODO - you fill in here.
-    return 0;
-  }
+        Endpoint(int time, boolean isStart) {
+            this.time = time;
+            this.isStart = isStart;
+        }
+    }
 
-  public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "CalendarRendering.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
-  }
+    public static void main(String[] args) {
+        System.exit(
+                GenericTest
+                        .runFromAnnotations(args, "CalendarRendering.java",
+                                new Object() {
+                                }.getClass().getEnclosingClass())
+                        .ordinal());
+    }
 }
