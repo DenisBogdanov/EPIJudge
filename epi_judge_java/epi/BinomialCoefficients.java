03e35caa78007641604ecf6ae7dca324c010ce5f
Denis Bogdanov <bogdanov.math@gmail.com>
2024-01-24 20:20:08 +0400

    Java.

--- epi_judge_java/epi/BinomialCoefficients.java
+++ epi_judge_java/epi/BinomialCoefficients.java
@@ -1,19 +1,44 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
+
 public class BinomialCoefficients {
-  @EpiTest(testDataFile = "binomial_coefficients.tsv")
 
-  public static int computeBinomialCoefficient(int n, int k) {
-    // TODO - you fill in here.
-    return 0;
-  }
+    @EpiTest(testDataFile = "binomial_coefficients.tsv")
+    public static int computeBinomialCoefficient(int n, int k) {
+        if (k == 0) return 1;
 
-  public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "BinomialCoefficients.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
-  }
+        int[][] dp = new int[n + 1][k + 1];
+        for (int i = 0; i < n; i++) {
+            dp[i][0] = 1;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= k; j++) {
+                dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
+            }
+        }
+
+        return dp[n][k];
+    }
+
+    private static class Pair {
+        final int n;
+        final int k;
+
+        public Pair(int n, int k) {
+            this.n = n;
+            this.k = k;
+        }
+    }
+
+    public static void main(String[] args) {
+        System.exit(
+                GenericTest
+                        .runFromAnnotations(args, "BinomialCoefficients.java",
+                                new Object() {
+                                }.getClass().getEnclosingClass())
+                        .ordinal());
+    }
 }
