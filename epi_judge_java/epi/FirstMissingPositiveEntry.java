8beb83b2678d41592c2d7e5a1f2835a82e0cbbf3
Denis Bogdanov <bogdanov.math@gmail.com>
2024-01-25 13:30:44 +0400

    Java.

--- epi_judge_java/epi/FirstMissingPositiveEntry.java
+++ epi_judge_java/epi/FirstMissingPositiveEntry.java
@@ -1,21 +1,31 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 public class FirstMissingPositiveEntry {
-  @EpiTest(testDataFile = "first_missing_positive_entry.tsv")
 
-  public static int findFirstMissingPositive(List<Integer> A) {
-    // TODO - you fill in here.
-    return 0;
-  }
+    @EpiTest(testDataFile = "first_missing_positive_entry.tsv")
+    public static int findFirstMissingPositive(List<Integer> a) {
+        Set<Integer> set = a.stream().filter(num -> num > 0).collect(Collectors.toSet());
 
-  public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "FirstMissingPositiveEntry.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
-  }
+        int candidate = 1;
+        while (true) {
+            if (!set.contains(candidate)) return candidate;
+            candidate++;
+        }
+    }
+
+    public static void main(String[] args) {
+        System.exit(
+                GenericTest
+                        .runFromAnnotations(args, "FirstMissingPositiveEntry.java",
+                                new Object() {
+                                }.getClass().getEnclosingClass())
+                        .ordinal());
+    }
 }
