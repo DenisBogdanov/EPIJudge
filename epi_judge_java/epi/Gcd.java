2ee6f6587348305126605e15aa944d3fa0145779
Denis Bogdanov <bogdanov.math@gmail.com>
2024-01-25 13:28:20 +0400

    Java.

--- epi_judge_java/epi/Gcd.java
+++ epi_judge_java/epi/Gcd.java
@@ -1,19 +1,27 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
+
 public class Gcd {
-  @EpiTest(testDataFile = "gcd.tsv")
 
-  public static long GCD(long x, long y) {
-    // TODO - you fill in here.
-    return 0;
-  }
+    @EpiTest(testDataFile = "gcd.tsv")
+    public static long GCD(long x, long y) {
+        while (y != 0) {
+            long temp = y;
+            y = x % y;
+            x = temp;
+        }
 
-  public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "Gcd.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
-  }
+        return x;
+    }
+
+    public static void main(String[] args) {
+        System.exit(
+                GenericTest
+                        .runFromAnnotations(args, "Gcd.java",
+                                new Object() {
+                                }.getClass().getEnclosingClass())
+                        .ordinal());
+    }
 }
