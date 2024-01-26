41e47116789df97ca16e43047b5799c5b492e5c1
Denis Bogdanov <bogdanov.math@gmail.com>
2024-01-26 20:30:31 +0400

    Java.

--- epi_judge_java/epi/MatrixConnectedRegions.java
+++ epi_judge_java/epi/MatrixConnectedRegions.java
@@ -4,12 +4,54 @@ import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 import epi.test_framework.TimedExecutor;
 
+import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Queue;
 
 public class MatrixConnectedRegions {
+    private static int[][] DIRECTIONS = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+
     public static void flipColor(int x, int y, List<List<Boolean>> image) {
-        // p. 370
+        int rows = image.size();
+        int columns = image.get(0).size();
+
+        boolean newColor = !image.get(x).get(y);
+
+        Queue<Coordinate> q = new ArrayDeque<>();
+        q.offer(new Coordinate(x, y));
+        image.get(x).set(y, null);
+
+        while (!q.isEmpty()) {
+            Coordinate polled = q.poll();
+
+            for (int[] direction : DIRECTIONS) {
+                int newX = direction[0] + polled.r;
+                int newY = direction[1] + polled.c;
+
+                if (newX < 0 || newX >= rows || newY < 0 || newY >= columns) continue;
+                if (image.get(newX).get(newY) == null || image.get(newX).get(newY) == newColor) continue;
+
+                image.get(newX).set(newY, newColor);
+                q.offer(new Coordinate(newX, newY));
+            }
+        }
+
+        for (List<Boolean> row : image) {
+            for (int i = 0; i < columns; i++) {
+                if (row.get(i) == null) row.set(i, newColor);
+            }
+        }
+    }
+
+    private static class Coordinate {
+        final int r;
+        final int c;
+
+        public Coordinate(int r, int c) {
+            this.r = r;
+            this.c = c;
+        }
     }
 
     @EpiTest(testDataFile = "painting.tsv")
