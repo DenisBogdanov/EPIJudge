ebed2d8d3a8f499f86b87e53750a6094e72e9633
Denis Bogdanov <bogdanov.math@gmail.com>
2024-01-27 19:09:24 +0400

    Java.

--- epi_judge_java/epi/PrimeSieve.java
+++ epi_judge_java/epi/PrimeSieve.java
@@ -1,21 +1,41 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
-public class PrimeSieve {
-  @EpiTest(testDataFile = "prime_sieve.tsv")
-  // Given n, return all primes up to and including n.
-  public static List<Integer> generatePrimes(int n) {
-    // TODO - you fill in here.
-    return null;
-  }
 
-  public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "PrimeSieve.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
-  }
+public class PrimeSieve {
+
+    @EpiTest(testDataFile = "prime_sieve.tsv")
+    // Given n, return all primes up to and including n.
+    public static List<Integer> generatePrimes(int n) {
+        List<Integer> result = new ArrayList<>();
+        boolean[] isPrime = new boolean[n + 1];
+        Arrays.fill(isPrime, true);
+        isPrime[0] = false;
+        isPrime[1] = false;
+
+        for (int i = 2; i <= n; i++) {
+            if (isPrime[i]) {
+                result.add(i);
+                for (int j = 2 * i; j <= n; j += i) {
+                    isPrime[j] = false;
+                }
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.exit(
+                GenericTest
+                        .runFromAnnotations(args, "PrimeSieve.java",
+                                new Object() {
+                                }.getClass().getEnclosingClass())
+                        .ordinal());
+    }
 }
