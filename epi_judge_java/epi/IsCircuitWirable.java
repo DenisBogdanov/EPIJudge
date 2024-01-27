f43feb880728c5a45d8c5af3f547c9b14119db02
Denis Bogdanov <bogdanov.math@gmail.com>
2024-01-27 13:13:17 +0400

    Java.

--- epi_judge_java/epi/IsCircuitWirable.java
+++ epi_judge_java/epi/IsCircuitWirable.java
@@ -1,4 +1,5 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.EpiUserType;
 import epi.test_framework.GenericTest;
@@ -6,51 +7,77 @@ import epi.test_framework.TimedExecutor;
 
 import java.util.ArrayList;
 import java.util.List;
+
 public class IsCircuitWirable {
 
-  public static class GraphVertex {
-    public int d = -1;
-    public List<GraphVertex> edges = new ArrayList<>();
-  }
+    public static boolean isAnyPlacementFeasible(List<GraphVertex> graph) {
+        for (GraphVertex graphVertex : graph) {
+            if (graphVertex.d != -1) continue;
+            graphVertex.d = 0;
 
-  public static boolean isAnyPlacementFeasible(List<GraphVertex> graph) {
-    // TODO - you fill in here.
-    return true;
-  }
-  @EpiUserType(ctorParams = {int.class, int.class})
-  public static class Edge {
-    public int from;
-    public int to;
+            if (!isPossible(graphVertex)) {
+                return false;
+            }
+        }
 
-    public Edge(int from, int to) {
-      this.from = from;
-      this.to = to;
-    }
-  }
-
-  @EpiTest(testDataFile = "is_circuit_wirable.tsv")
-  public static boolean isAnyPlacementFeasibleWrapper(TimedExecutor executor,
-                                                      int k, List<Edge> edges)
-      throws Exception {
-    if (k <= 0)
-      throw new RuntimeException("Invalid k value");
-    List<GraphVertex> graph = new ArrayList<>();
-    for (int i = 0; i < k; i++)
-      graph.add(new GraphVertex());
-    for (Edge e : edges) {
-      if (e.from < 0 || e.from >= k || e.to < 0 || e.to >= k)
-        throw new RuntimeException("Invalid vertex index");
-      graph.get(e.from).edges.add(graph.get(e.to));
+        return true;
     }
 
-    return executor.run(() -> isAnyPlacementFeasible(graph));
-  }
+    private static boolean isPossible(GraphVertex graphVertex) {
+        int d = graphVertex.d;
+        for (GraphVertex neighbour : graphVertex.edges) {
+            if (neighbour.d == d) return false;
+            if (neighbour.d == -1) {
+                neighbour.d = 1 - d;
+                if (!isPossible(neighbour)) {
+                    return false;
+                }
+            }
+        }
 
-  public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "IsCircuitWirable.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
-  }
+        return true;
+    }
+
+    public static class GraphVertex {
+        public int d = -1;
+        public List<GraphVertex> edges = new ArrayList<>();
+    }
+
+    @EpiUserType(ctorParams = {int.class, int.class})
+    public static class Edge {
+        public int from;
+        public int to;
+
+        public Edge(int from, int to) {
+            this.from = from;
+            this.to = to;
+        }
+    }
+
+    @EpiTest(testDataFile = "is_circuit_wirable.tsv")
+    public static boolean isAnyPlacementFeasibleWrapper(TimedExecutor executor,
+                                                        int k, List<Edge> edges)
+            throws Exception {
+        if (k <= 0)
+            throw new RuntimeException("Invalid k value");
+        List<GraphVertex> graph = new ArrayList<>();
+        for (int i = 0; i < k; i++)
+            graph.add(new GraphVertex());
+        for (Edge e : edges) {
+            if (e.from < 0 || e.from >= k || e.to < 0 || e.to >= k)
+                throw new RuntimeException("Invalid vertex index");
+            graph.get(e.from).edges.add(graph.get(e.to));
+        }
+
+        return executor.run(() -> isAnyPlacementFeasible(graph));
+    }
+
+    public static void main(String[] args) {
+        System.exit(
+                GenericTest
+                        .runFromAnnotations(args, "IsCircuitWirable.java",
+                                new Object() {
+                                }.getClass().getEnclosingClass())
+                        .ordinal());
+    }
 }
