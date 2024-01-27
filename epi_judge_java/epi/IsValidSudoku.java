2b476d735f6c162ade75aea078114fbecc139c0d
Denis Bogdanov <bogdanov.math@gmail.com>
2024-01-27 22:50:13 +0400

    Java.

--- epi_judge_java/epi/IsValidSudoku.java
+++ epi_judge_java/epi/IsValidSudoku.java
@@ -1,22 +1,41 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+
 public class IsValidSudoku {
-  @EpiTest(testDataFile = "is_valid_sudoku.tsv")
 
-  // Check if a partially filled matrix has any conflicts.
-  public static boolean isValidSudoku(List<List<Integer>> partialAssignment) {
-    // TODO - you fill in here.
-    return true;
-  }
+    @EpiTest(testDataFile = "is_valid_sudoku.tsv")
+    // Check if a partially filled matrix has any conflicts.
+    public static boolean isValidSudoku(List<List<Integer>> partialAssignment) {
+        Set<String> set = new HashSet<>();
+        for (int r = 0; r < 9; r++) {
+            for (int c = 0; c < 9; c++) {
+                int value = partialAssignment.get(r).get(c);
+                if (value > 0) {
+                    if (!set.add("Seen " + value + " on " + r + " row")
+                            || !set.add("Seen " + value + " on " + c + " column")
+                            || !set.add("Seen " + value + " on " + (r / 3 * 3 + c / 3) + " subgrid")) {
 
-  public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "IsValidSudoku.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
-  }
+                        return false;
+                    }
+                }
+            }
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        System.exit(
+                GenericTest
+                        .runFromAnnotations(args, "IsValidSudoku.java",
+                                new Object() {
+                                }.getClass().getEnclosingClass())
+                        .ordinal());
+    }
 }
