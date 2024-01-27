e5ea2ed34101177feab2e36c93a52b3f38939fc1
Denis Bogdanov <bogdanov.math@gmail.com>
2024-01-28 00:44:14 +0400

    Java.

--- epi_judge_java/epi/PascalTriangle.java
+++ epi_judge_java/epi/PascalTriangle.java
@@ -1,21 +1,41 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 
+import java.util.ArrayList;
 import java.util.List;
+
 public class PascalTriangle {
-  @EpiTest(testDataFile = "pascal_triangle.tsv")
 
-  public static List<List<Integer>> generatePascalTriangle(int numRows) {
-    // TODO - you fill in here.
-    return null;
-  }
+    @EpiTest(testDataFile = "pascal_triangle.tsv")
+    public static List<List<Integer>> generatePascalTriangle(int numRows) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (numRows == 0) return result;
 
-  public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "PascalTriangle.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
-  }
+        result.add(List.of(1));
+
+        for (int i = 1; i < numRows; i++) {
+            List<Integer> prev = result.get(result.size() - 1);
+            List<Integer> level = new ArrayList<>();
+            level.add(1);
+            for (int j = 0; j < prev.size() - 1; j++) {
+                level.add(prev.get(j) + prev.get(j + 1));
+            }
+
+            level.add(1);
+            result.add(level);
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.exit(
+                GenericTest
+                        .runFromAnnotations(args, "PascalTriangle.java",
+                                new Object() {
+                                }.getClass().getEnclosingClass())
+                        .ordinal());
+    }
 }
