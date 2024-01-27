638c4e6e5283a9f4b65fdae05451d86679f430f8
Denis Bogdanov <bogdanov.math@gmail.com>
2024-01-27 15:37:01 +0400

    Java.

--- epi_judge_java/epi/IntAsArrayIncrement.java
+++ epi_judge_java/epi/IntAsArrayIncrement.java
@@ -1,20 +1,43 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
-public class IntAsArrayIncrement {
-  @EpiTest(testDataFile = "int_as_array_increment.tsv")
-  public static List<Integer> plusOne(List<Integer> A) {
-    // TODO - you fill in here.
-    return null;
-  }
 
-  public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "IntAsArrayIncrement.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
-  }
+public class IntAsArrayIncrement {
+
+    @EpiTest(testDataFile = "int_as_array_increment.tsv")
+    public static List<Integer> plusOne(List<Integer> list) {
+        int carry = 1;
+        List<Integer> result = new ArrayList<>();
+
+        for (int i = list.size() - 1; i >= 0; i--) {
+            int sum = list.get(i) + carry;
+            if (sum > 9) {
+                sum = 0;
+                carry = 1;
+            } else {
+                carry = 0;
+            }
+
+            result.add(sum);
+        }
+
+        if (carry != 0) result.add(carry);
+        Collections.reverse(result);
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.exit(
+                GenericTest
+                        .runFromAnnotations(args, "IntAsArrayIncrement.java",
+                                new Object() {
+                                }.getClass().getEnclosingClass())
+                        .ordinal());
+    }
 }
