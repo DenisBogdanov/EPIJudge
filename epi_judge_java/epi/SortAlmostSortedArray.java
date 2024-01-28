ad4678dbe12f46ebabfc2e9590faffc12b1a6dcf
Denis Bogdanov <bogdanov.math@gmail.com>
2024-01-28 17:11:48 +0400

    Java.

--- epi_judge_java/epi/SortAlmostSortedArray.java
+++ epi_judge_java/epi/SortAlmostSortedArray.java
@@ -1,27 +1,47 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
 public class SortAlmostSortedArray {
 
-  public static List<Integer>
-  sortApproximatelySortedData(Iterator<Integer> sequence, int k) {
-    // TODO - you fill in here.
-    return null;
-  }
-  @EpiTest(testDataFile = "sort_almost_sorted_array.tsv")
-  public static List<Integer>
-  sortApproximatelySortedDataWrapper(List<Integer> sequence, int k) {
-    return sortApproximatelySortedData(sequence.iterator(), k);
-  }
+    public static List<Integer> sortApproximatelySortedData(Iterator<Integer> sequence, int k) {
+        List<Integer> result = new ArrayList<>();
+        Queue<Integer> q = new PriorityQueue<>();
 
-  public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "SortAlmostSortedArray.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
-  }
+        while (sequence.hasNext()) {
+            q.offer(sequence.next());
+
+            if (q.size() > k) {
+                result.add(q.poll());
+            }
+        }
+
+        while (!q.isEmpty()) {
+            result.add(q.poll());
+        }
+
+        return result;
+    }
+
+    @EpiTest(testDataFile = "sort_almost_sorted_array.tsv")
+    public static List<Integer>
+    sortApproximatelySortedDataWrapper(List<Integer> sequence, int k) {
+        return sortApproximatelySortedData(sequence.iterator(), k);
+    }
+
+    public static void main(String[] args) {
+        System.exit(
+                GenericTest
+                        .runFromAnnotations(args, "SortAlmostSortedArray.java",
+                                new Object() {
+                                }.getClass().getEnclosingClass())
+                        .ordinal());
+    }
 }
