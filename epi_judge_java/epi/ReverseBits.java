7444e8c5f2012d6b3f43806ddcc148f7e74b0d1a
Denis Bogdanov <bogdanov.math@gmail.com>
2024-01-28 10:25:27 +0400

    Java.

--- epi_judge_java/epi/ReverseBits.java
+++ epi_judge_java/epi/ReverseBits.java
@@ -1,18 +1,27 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
-public class ReverseBits {
-  @EpiTest(testDataFile = "reverse_bits.tsv")
-  public static long reverseBits(long x) {
-    // TODO - you fill in here.
-    return 0;
-  }
 
-  public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "ReverseBits.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
-  }
+public class ReverseBits {
+
+    @EpiTest(testDataFile = "reverse_bits.tsv")
+    public static long reverseBits(long x) {
+        int left = 0;
+        int right = 63;
+        while (left < right) {
+            x = SwapBits.swapBits(x, left++, right--);
+        }
+
+        return x;
+    }
+
+    public static void main(String[] args) {
+        System.exit(
+                GenericTest
+                        .runFromAnnotations(args, "ReverseBits.java",
+                                new Object() {
+                                }.getClass().getEnclosingClass())
+                        .ordinal());
+    }
 }
