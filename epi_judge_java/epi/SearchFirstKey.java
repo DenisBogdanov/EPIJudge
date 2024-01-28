bbc50ca74a1908f744aa7bd575a0da3ab8345eb7
Denis Bogdanov <bogdanov.math@gmail.com>
2024-01-28 20:11:33 +0400

    Java.

--- epi_judge_java/epi/SearchFirstKey.java
+++ epi_judge_java/epi/SearchFirstKey.java
@@ -1,21 +1,38 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 
 import java.util.List;
+
 public class SearchFirstKey {
-  @EpiTest(testDataFile = "search_first_key.tsv")
 
-  public static int searchFirstOfK(List<Integer> A, int k) {
-    // TODO - you fill in here.
-    return 0;
-  }
+    @EpiTest(testDataFile = "search_first_key.tsv")
+    public static int searchFirstOfK(List<Integer> nums, int k) {
+        if (nums.isEmpty()) return -1;
 
-  public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "SearchFirstKey.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
-  }
+        int left = -1;
+        int right = nums.size() - 1;
+
+        while (left + 1 < right) {
+            int candidate = (left + right) / 2;
+
+            if (nums.get(candidate) >= k) {
+                right = candidate;
+            } else {
+                left = candidate;
+            }
+        }
+
+        return nums.get(left + 1) == k ? left + 1 : -1;
+    }
+
+    public static void main(String[] args) {
+        System.exit(
+                GenericTest
+                        .runFromAnnotations(args, "SearchFirstKey.java",
+                                new Object() {
+                                }.getClass().getEnclosingClass())
+                        .ordinal());
+    }
 }
