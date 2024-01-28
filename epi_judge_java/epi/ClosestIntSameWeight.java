a0b8816402e1d66cd8611030bac562a94db56d37
Denis Bogdanov <bogdanov.math@gmail.com>
2024-01-28 10:45:11 +0400

    Java.

--- epi_judge_java/epi/ClosestIntSameWeight.java
+++ epi_judge_java/epi/ClosestIntSameWeight.java
@@ -1,18 +1,28 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
-public class ClosestIntSameWeight {
-  @EpiTest(testDataFile = "closest_int_same_weight.tsv")
-  public static long closestIntSameBitCount(long x) {
-    // TODO - you fill in here.
-    return 0;
-  }
 
-  public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "ClosestIntSameWeight.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
-  }
+public class ClosestIntSameWeight {
+
+    @EpiTest(testDataFile = "closest_int_same_weight.tsv")
+    public static long closestIntSameBitCount(long x) {
+        for (int i = 0; i < 63; i++) {
+            if (((x >> i) & 1) != ((x >> (i + 1)) & 1)) {
+                x = SwapBits.swapBits(x, i, i + 1);
+                break;
+            }
+        }
+
+        return x;
+    }
+
+    public static void main(String[] args) {
+        System.exit(
+                GenericTest
+                        .runFromAnnotations(args, "ClosestIntSameWeight.java",
+                                new Object() {
+                                }.getClass().getEnclosingClass())
+                        .ordinal());
+    }
 }
