bbb07b4d2affda6afdceb2fe3577d2a098ef3ed4
Denis Bogdanov <bogdanov.math@gmail.com>
2024-01-28 11:30:03 +0400

    Java.

--- epi_judge_java/epi/PrimitiveDivide.java
+++ epi_judge_java/epi/PrimitiveDivide.java
@@ -1,18 +1,33 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
-public class PrimitiveDivide {
-  @EpiTest(testDataFile = "primitive_divide.tsv")
-  public static int divide(int x, int y) {
-    // TODO - you fill in here.
-    return 0;
-  }
 
-  public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "PrimitiveDivide.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
-  }
+public class PrimitiveDivide {
+
+    @EpiTest(testDataFile = "primitive_divide.tsv")
+    public static int divide(int x, int y) {
+        int result = 0;
+        int pow = 32;
+
+        while (x >= y) {
+            while ((y << pow) > x) {
+                pow--;
+            }
+
+            result += 1 << pow;
+            x -= (y << pow);
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.exit(
+                GenericTest
+                        .runFromAnnotations(args, "PrimitiveDivide.java",
+                                new Object() {
+                                }.getClass().getEnclosingClass())
+                        .ordinal());
+    }
 }
