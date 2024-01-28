45c002662eb75b61cd5b6f3eb922c94113d5c8bc
Denis Bogdanov <bogdanov.math@gmail.com>
2024-01-28 17:55:48 +0400

    Java.

--- epi_judge_java/epi/OnlineMedian.java
+++ epi_judge_java/epi/OnlineMedian.java
@@ -1,24 +1,57 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.Iterator;
 import java.util.List;
-public class OnlineMedian {
-  public static List<Double> onlineMedian(Iterator<Integer> sequence) {
-    // TODO - you fill in here.
-    return null;
-  }
-  @EpiTest(testDataFile = "online_median.tsv")
-  public static List<Double> onlineMedianWrapper(List<Integer> sequence) {
-    return onlineMedian(sequence.iterator());
-  }
+import java.util.PriorityQueue;
+import java.util.Queue;
 
-  public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "OnlineMedian.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
-  }
+public class OnlineMedian {
+
+    public static List<Double> onlineMedian(Iterator<Integer> sequence) {
+        Queue<Integer> q1 = new PriorityQueue<>(Comparator.reverseOrder());
+        Queue<Integer> q2 = new PriorityQueue<>();
+
+        List<Double> result = new ArrayList<>();
+
+        while (sequence.hasNext()) {
+            int next = sequence.next();
+
+            q1.offer(next);
+            while (q1.size() > q2.size() + 1) {
+                q2.offer(q1.poll());
+            }
+
+            if (!q2.isEmpty() && q1.peek() > q2.peek()) {
+                q1.offer(q2.poll());
+                q2.offer(q1.poll());
+            }
+
+            if (q1.size() == q2.size()) {
+                result.add((q1.peek() + q2.peek()) / 2.0);
+            } else {
+                result.add(Double.valueOf(q1.peek()));
+            }
+        }
+
+        return result;
+    }
+
+    @EpiTest(testDataFile = "online_median.tsv")
+    public static List<Double> onlineMedianWrapper(List<Integer> sequence) {
+        return onlineMedian(sequence.iterator());
+    }
+
+    public static void main(String[] args) {
+        System.exit(
+                GenericTest
+                        .runFromAnnotations(args, "OnlineMedian.java",
+                                new Object() {
+                                }.getClass().getEnclosingClass())
+                        .ordinal());
+    }
 }
