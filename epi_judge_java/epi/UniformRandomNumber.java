213d6bdcc7ce3460dd6b2ce14776370643685013
Denis Bogdanov <bogdanov.math@gmail.com>
2024-01-28 12:57:55 +0400

    Java.

--- epi_judge_java/epi/UniformRandomNumber.java
+++ epi_judge_java/epi/UniformRandomNumber.java
@@ -1,55 +1,76 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 import epi.test_framework.RandomSequenceChecker;
-import epi.test_framework.TestFailure;
 import epi.test_framework.TimedExecutor;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+
 public class UniformRandomNumber {
-  private static int zeroOneRandom() {
-    Random gen = new Random();
-    return gen.nextInt(2);
-  }
 
-  public static int uniformRandom(int lowerBound, int upperBound) {
-    // TODO - you fill in here.
-    return 0;
-  }
-  private static boolean uniformRandomRunner(TimedExecutor executor,
-                                             int lowerBound, int upperBound)
-      throws Exception {
-    List<Integer> results = new ArrayList<>();
+    public static int uniformRandom(int lowerBound, int upperBound) {
+        int diff = upperBound - lowerBound + 1;
 
-    executor.run(() -> {
-      for (int i = 0; i < 100000; ++i) {
-        results.add(uniformRandom(lowerBound, upperBound));
-      }
-    });
+        int random = getRandom(diff);
 
-    List<Integer> sequence = new ArrayList<>();
-    for (Integer result : results) {
-      sequence.add(result - lowerBound);
+        return lowerBound + random;
     }
-    return RandomSequenceChecker.checkSequenceIsUniformlyRandom(
-        sequence, upperBound - lowerBound + 1, 0.01);
-  }
 
-  @EpiTest(testDataFile = "uniform_random_number.tsv")
-  public static void uniformRandomWrapper(TimedExecutor executor,
-                                          int lowerBound, int upperBound)
-      throws Exception {
-    RandomSequenceChecker.runFuncWithRetries(
-        () -> uniformRandomRunner(executor, lowerBound, upperBound));
-  }
+    private static int getRandom(int diff) {
+        while (true) {
+            int result = 0;
+            int pow = 1;
+            while (pow < diff) {
+                pow *= 2;
+                result *= 2;
+                result += zeroOneRandom();
+            }
 
-  public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "UniformRandomNumber.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
-  }
+            if (result < diff) return result;
+        }
+    }
+
+    private static int zeroOneRandom() {
+        Random gen = new Random();
+        return gen.nextInt(2);
+    }
+
+    private static boolean uniformRandomRunner(TimedExecutor executor,
+                                               int lowerBound, int upperBound)
+            throws Exception {
+        List<Integer> results = new ArrayList<>();
+
+        executor.run(() -> {
+            for (int i = 0; i < 100000; ++i) {
+                results.add(uniformRandom(lowerBound, upperBound));
+            }
+        });
+
+        List<Integer> sequence = new ArrayList<>();
+        for (Integer result : results) {
+            sequence.add(result - lowerBound);
+        }
+        return RandomSequenceChecker.checkSequenceIsUniformlyRandom(
+                sequence, upperBound - lowerBound + 1, 0.01);
+    }
+
+    @EpiTest(testDataFile = "uniform_random_number.tsv")
+    public static void uniformRandomWrapper(TimedExecutor executor,
+                                            int lowerBound, int upperBound)
+            throws Exception {
+        RandomSequenceChecker.runFuncWithRetries(
+                () -> uniformRandomRunner(executor, lowerBound, upperBound));
+    }
+
+    public static void main(String[] args) {
+        System.exit(
+                GenericTest
+                        .runFromAnnotations(args, "UniformRandomNumber.java",
+                                new Object() {
+                                }.getClass().getEnclosingClass())
+                        .ordinal());
+    }
 }
