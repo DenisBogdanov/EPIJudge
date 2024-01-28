3342c5e0e4424186e8cf8330d2d6eef217b9feb2
Denis Bogdanov <bogdanov.math@gmail.com>
2024-01-28 20:04:29 +0400

    Java.

--- epi_judge_java/epi/KLargestInHeap.java
+++ epi_judge_java/epi/KLargestInHeap.java
@@ -1,34 +1,79 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.EpiTestComparator;
 import epi.test_framework.GenericTest;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.function.BiPredicate;
+
 public class KLargestInHeap {
-  @EpiTest(testDataFile = "k_largest_in_heap.tsv")
 
-  public static List<Integer> kLargestInBinaryHeap(List<Integer> A, int k) {
-    // TODO - you fill in here.
-    return null;
-  }
-  @EpiTestComparator
-  public static boolean comp(List<Integer> expected, List<Integer> result) {
-    if (result == null) {
-      return false;
+    @EpiTest(testDataFile = "k_largest_in_heap.tsv")
+    public static List<Integer> kLargestInBinaryHeap(List<Integer> nums, int k) {
+        sort(nums, k);
+        return nums.subList(0, k);
     }
-    Collections.sort(expected);
-    Collections.sort(result);
-    return expected.equals(result);
-  }
 
-  public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "KLargestInHeap.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
-  }
+    private static void sort(List<Integer> nums, int k) {
+        sort(nums, 0, nums.size() - 1, k);
+    }
+
+    private static void sort(List<Integer> nums, int left, int right, int k) {
+        if (left >= right) return;
+
+        int pivotIndex = partition(nums, left, right);
+
+        if (pivotIndex == k) {
+            return;
+        } else if (pivotIndex < k) {
+            sort(nums, pivotIndex + 1, right, k);
+        } else {
+            sort(nums, left, pivotIndex - 1, k);
+        }
+    }
+
+    private static int partition(List<Integer> nums, int left, int right) {
+        int pivot = nums.get(left);
+
+        int i = left + 1;
+        int j = right;
+
+        while (true) {
+            while (i <= j) {
+                if (nums.get(i) >= pivot) i++;
+                else break;
+            }
+
+            while (i <= j) {
+                if (nums.get(j) <= pivot) j--;
+                else break;
+            }
+
+            if (i >= j) break;
+            Collections.swap(nums, i++, j--);
+        }
+
+        Collections.swap(nums, left, j);
+        return j;
+    }
+
+    @EpiTestComparator
+    public static boolean comp(List<Integer> expected, List<Integer> result) {
+        if (result == null) {
+            return false;
+        }
+        Collections.sort(expected);
+        Collections.sort(result);
+        return expected.equals(result);
+    }
+
+    public static void main(String[] args) {
+        System.exit(
+                GenericTest
+                        .runFromAnnotations(args, "KLargestInHeap.java",
+                                new Object() {
+                                }.getClass().getEnclosingClass())
+                        .ordinal());
+    }
 }
