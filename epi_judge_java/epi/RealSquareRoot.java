8bf68e4cad2434200e791bcd67f99a541402cffb
Denis Bogdanov <bogdanov.math@gmail.com>
2024-01-29 09:57:26 +0400

    Java.

--- epi_judge_java/epi/RealSquareRoot.java
+++ epi_judge_java/epi/RealSquareRoot.java
@@ -1,19 +1,33 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
+
 public class RealSquareRoot {
-  @EpiTest(testDataFile = "real_square_root.tsv")
 
-  public static double squareRoot(double x) {
-    // TODO - you fill in here.
-    return 0.0;
-  }
+    @EpiTest(testDataFile = "real_square_root.tsv")
+    public static double squareRoot(double x) {
+        double left = -1;
+        double right = x + 1;
 
-  public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "RealSquareRoot.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
-  }
+        while (right - left > 1e-8) {
+            double mid = (left + right) / 2;
+            if (mid * mid > x) {
+                right = mid;
+            } else {
+                left = mid;
+            }
+        }
+
+        return left;
+    }
+
+    public static void main(String[] args) {
+        System.exit(
+                GenericTest
+                        .runFromAnnotations(args, "RealSquareRoot.java",
+                                new Object() {
+                                }.getClass().getEnclosingClass())
+                        .ordinal());
+    }
 }
