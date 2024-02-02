b5d0c4447b82968c6306c9ce1752513c6ad2d89b
Denis Bogdanov <bogdanov.math@gmail.com>
2024-02-02 21:33:55 +0400

    Java.

--- epi_judge_java/epi/Anagrams.java
+++ epi_judge_java/epi/Anagrams.java
@@ -1,41 +1,60 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.EpiTestComparator;
 import epi.test_framework.GenericTest;
 import epi.test_framework.LexicographicalListComparator;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
-import java.util.function.BiPredicate;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public class Anagrams {
-  @EpiTest(testDataFile = "anagrams.tsv")
 
-  public static List<List<String>> findAnagrams(List<String> dictionary) {
-    // TODO - you fill in here.
-    return null;
-  }
-  @EpiTestComparator
-  public static boolean comp(List<List<String>> expected,
-                             List<List<String>> result) {
-    if (result == null) {
-      return false;
-    }
-    for (List<String> l : expected) {
-      Collections.sort(l);
-    }
-    expected.sort(new LexicographicalListComparator<>());
-    for (List<String> l : result) {
-      Collections.sort(l);
-    }
-    result.sort(new LexicographicalListComparator<>());
-    return expected.equals(result);
-  }
+    @EpiTest(testDataFile = "anagrams.tsv")
+    public static List<List<String>> findAnagrams(List<String> dictionary) {
+        Map<String, List<String>> resultMap = new HashMap<>();
 
-  public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "Anagrams.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
-  }
+        for (String s : dictionary) {
+            resultMap.computeIfAbsent(sorted(s), k -> new ArrayList<>())
+                    .add(s);
+        }
+
+        return resultMap.values().stream().filter(list -> list.size() > 1).collect(Collectors.toList());
+    }
+
+    private static String sorted(String s) {
+        char[] chars = s.toCharArray();
+        Arrays.sort(chars);
+        return new String(chars);
+    }
+
+    @EpiTestComparator
+    public static boolean comp(List<List<String>> expected, List<List<String>> result) {
+        if (result == null) {
+            return false;
+        }
+        for (List<String> l : expected) {
+            Collections.sort(l);
+        }
+        expected.sort(new LexicographicalListComparator<>());
+        for (List<String> l : result) {
+            Collections.sort(l);
+        }
+        result.sort(new LexicographicalListComparator<>());
+        return expected.equals(result);
+    }
+
+    public static void main(String[] args) {
+        System.exit(
+                GenericTest
+                        .runFromAnnotations(args, "Anagrams.java",
+                                new Object() {
+                                }.getClass().getEnclosingClass())
+                        .ordinal());
+    }
 }
