e70c07412c325e1bc3683c964833c10352eafe7c
Denis Bogdanov <bogdanov.math@gmail.com>
2024-02-03 23:17:22 +0400

    Java.

--- epi_judge_java/epi/CollatzChecker.java
+++ epi_judge_java/epi/CollatzChecker.java
@@ -1,19 +1,42 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
+
+import java.util.HashSet;
+import java.util.Set;
+
 public class CollatzChecker {
-  @EpiTest(testDataFile = "collatz_checker.tsv")
 
-  public static boolean testCollatzConjecture(int n) {
-    // TODO - you fill in here.
-    return false;
-  }
+    @EpiTest(testDataFile = "collatz_checker.tsv")
+    public static boolean testCollatzConjecture(int n) {
+        Set<Long> checked = new HashSet<>();
+        checked.add(1L);
 
-  public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "CollatzChecker.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
-  }
+        for (int i = 2; i <= n; i++) {
+            Set<Long> curr = new HashSet<>();
+            long num = i;
+            while (!checked.contains(num)) {
+                curr.add(num);
+                if (num % 2 == 0) {
+                    num /= 2;
+                } else {
+                    num = num * 3 + 1;
+                }
+            }
+
+            checked.addAll(curr);
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        System.exit(
+                GenericTest
+                        .runFromAnnotations(args, "CollatzChecker.java",
+                                new Object() {
+                                }.getClass().getEnclosingClass())
+                        .ordinal());
+    }
 }
