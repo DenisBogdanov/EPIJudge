8fe04be28eb47976d4b590cf7785341f6d5b543b
Denis Bogdanov <bogdanov.math@gmail.com>
2024-02-03 09:42:51 +0400

    Java.

--- epi_judge_java/epi/IsStringPermutableToPalindrome.java
+++ epi_judge_java/epi/IsStringPermutableToPalindrome.java
@@ -1,19 +1,27 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
+
 public class IsStringPermutableToPalindrome {
-  @EpiTest(testDataFile = "is_string_permutable_to_palindrome.tsv")
 
-  public static boolean canFormPalindrome(String s) {
-    // TODO - you fill in here.
-    return true;
-  }
+    @EpiTest(testDataFile = "is_string_permutable_to_palindrome.tsv")
+    public static boolean canFormPalindrome(String s) {
+        int counts = 0;
+        for (char c : s.toCharArray()) {
+            if (c == ' ') continue;
+            counts ^= (1 << (c - 'a'));
+        }
 
-  public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "IsStringPermutableToPalindrome.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
-  }
+        return counts == 0 || ((counts & (counts - 1)) == 0);
+    }
+
+    public static void main(String[] args) {
+        System.exit(
+                GenericTest
+                        .runFromAnnotations(args, "IsStringPermutableToPalindrome.java",
+                                new Object() {
+                                }.getClass().getEnclosingClass())
+                        .ordinal());
+    }
 }
