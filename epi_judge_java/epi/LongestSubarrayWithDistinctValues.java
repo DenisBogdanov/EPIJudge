e94f244a7740d5cb67360ef6afd42ba8ea92d79b
Denis Bogdanov <bogdanov.math@gmail.com>
2024-02-03 23:05:01 +0400

    Java.

--- epi_judge_java/epi/LongestSubarrayWithDistinctValues.java
+++ epi_judge_java/epi/LongestSubarrayWithDistinctValues.java
@@ -1,21 +1,42 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+
 public class LongestSubarrayWithDistinctValues {
-  @EpiTest(testDataFile = "longest_subarray_with_distinct_values.tsv")
 
-  public static int longestSubarrayWithDistinctEntries(List<Integer> A) {
-    // TODO - you fill in here.
-    return 0;
-  }
+    @EpiTest(testDataFile = "longest_subarray_with_distinct_values.tsv")
+    public static int longestSubarrayWithDistinctEntries(List<Integer> nums) {
+        Set<Integer> seen = new HashSet<>();
+        int result = 0;
 
-  public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "LongestSubarrayWithDistinctValues.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
-  }
+        int start = 0;
+        for (int end = 0; end < nums.size(); end++) {
+            if (seen.add(nums.get(end))) {
+                result = Math.max(result, end - start + 1);
+            } else {
+                while (seen.contains(nums.get(end))) {
+                    seen.remove(nums.get(start));
+                    start++;
+                }
+
+                seen.add(nums.get(end));
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.exit(
+                GenericTest
+                        .runFromAnnotations(args, "LongestSubarrayWithDistinctValues.java",
+                                new Object() {
+                                }.getClass().getEnclosingClass())
+                        .ordinal());
+    }
 }
