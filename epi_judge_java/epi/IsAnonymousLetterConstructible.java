b4cedb3d86e99f1d6cebb468a9b6384f7e3ca632
Denis Bogdanov <bogdanov.math@gmail.com>
2024-02-03 10:19:05 +0400

    Java.

--- epi_judge_java/epi/IsAnonymousLetterConstructible.java
+++ epi_judge_java/epi/IsAnonymousLetterConstructible.java
@@ -1,20 +1,30 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
+
 public class IsAnonymousLetterConstructible {
-  @EpiTest(testDataFile = "is_anonymous_letter_constructible.tsv")
 
-  public static boolean isLetterConstructibleFromMagazine(String letterText,
-                                                          String magazineText) {
-    // TODO - you fill in here.
-    return true;
-  }
+    @EpiTest(testDataFile = "is_anonymous_letter_constructible.tsv")
+    public static boolean isLetterConstructibleFromMagazine(String letterText, String magazineText) {
+        int[] decomposition = new int[128];
+        for (char c : magazineText.toCharArray()) {
+            decomposition[c]++;
+        }
 
-  public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "IsAnonymousLetterConstructible.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
-  }
+        for (char c : letterText.toCharArray()) {
+            if (--decomposition[c] < 0) return false;
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        System.exit(
+                GenericTest
+                        .runFromAnnotations(args, "IsAnonymousLetterConstructible.java",
+                                new Object() {
+                                }.getClass().getEnclosingClass())
+                        .ordinal());
+    }
 }
