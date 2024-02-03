42c1a18f2ed7a079bf9de50c9294991b700e3d22
Denis Bogdanov <bogdanov.math@gmail.com>
2024-02-03 23:10:43 +0400

    Java.

--- epi_judge_java/epi/LongestContainedInterval.java
+++ epi_judge_java/epi/LongestContainedInterval.java
@@ -1,21 +1,41 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 
 import java.util.List;
+
 public class LongestContainedInterval {
-  @EpiTest(testDataFile = "longest_contained_interval.tsv")
 
-  public static int longestContainedRange(List<Integer> A) {
-    // TODO - you fill in here.
-    return 0;
-  }
+    @EpiTest(testDataFile = "longest_contained_interval.tsv")
+    public static int longestContainedRange(List<Integer> nums) {
+        if (nums == null || nums.isEmpty()) return 0;
 
-  public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "LongestContainedInterval.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
-  }
+        nums.sort(null);
+
+        int result = 1;
+        int currCount = 1;
+
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums.get(i).equals(nums.get(i - 1))) continue;
+            if (nums.get(i) - nums.get(i - 1) == 1) {
+                currCount++;
+            } else {
+                currCount = 1;
+            }
+
+            result = Math.max(result, currCount);
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.exit(
+                GenericTest
+                        .runFromAnnotations(args, "LongestContainedInterval.java",
+                                new Object() {
+                                }.getClass().getEnclosingClass())
+                        .ordinal());
+    }
 }
