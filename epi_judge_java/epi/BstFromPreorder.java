3c8bcf664eb09bda0b8e6642d2fea0fba3b70962
Denis Bogdanov <bogdanov.math@gmail.com>
2024-02-10 23:26:28 +0400

    Java.

--- epi_judge_java/epi/BstFromPreorder.java
+++ epi_judge_java/epi/BstFromPreorder.java
@@ -1,22 +1,36 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 
 import java.util.List;
+
 public class BstFromPreorder {
-  @EpiTest(testDataFile = "bst_from_preorder.tsv")
 
-  public static BstNode<Integer>
-  rebuildBSTFromPreorder(List<Integer> preorderSequence) {
-    // TODO - you fill in here.
-    return null;
-  }
+    @EpiTest(testDataFile = "bst_from_preorder.tsv")
+    public static BstNode<Integer> rebuildBSTFromPreorder(List<Integer> preorderSequence) {
+        return build(preorderSequence, 0, preorderSequence.size());
+    }
 
-  public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "BstFromPreorder.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
-  }
+    private static BstNode<Integer> build(List<Integer> preorderSequence, int left, int right) {
+        if (left >= right) return null;
+
+        int transitionPoint = left + 1;
+        while (transitionPoint < right && preorderSequence.get(transitionPoint) < preorderSequence.get(left)) {
+            transitionPoint++;
+        }
+
+        return new BstNode<>(preorderSequence.get(left),
+                build(preorderSequence, left + 1, transitionPoint),
+                build(preorderSequence, transitionPoint, right));
+    }
+
+    public static void main(String[] args) {
+        System.exit(
+                GenericTest
+                        .runFromAnnotations(args, "BstFromPreorder.java",
+                                new Object() {
+                                }.getClass().getEnclosingClass())
+                        .ordinal());
+    }
 }
