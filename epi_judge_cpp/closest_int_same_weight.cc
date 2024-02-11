7fa7cebee4bb009f858116c627ba07bf193800dd
Denis Bogdanov <bogdanov.math@gmail.com>
2024-02-11 12:43:51 +0400

    C++.

--- epi_judge_cpp/closest_int_same_weight.cc
+++ epi_judge_cpp/closest_int_same_weight.cc
@@ -1,13 +1,19 @@
 #include "test_framework/generic_test.h"
+
 unsigned long long ClosestIntSameBitCount(unsigned long long x) {
-  // TODO - you fill in here.
-  return 0;
+    for (int i = 0; i < 63; i++) {
+        if (((x >> i) & 1) != ((x >> (i + 1)) & 1)) {
+            return x ^ ((1ULL << i) | (1ULL << (i + 1)));
+        }
+    }
+
+    return 0;
 }
 
-int main(int argc, char* argv[]) {
-  std::vector<std::string> args{argv + 1, argv + argc};
-  std::vector<std::string> param_names{"x"};
-  return GenericTestMain(args, "closest_int_same_weight.cc",
-                         "closest_int_same_weight.tsv", &ClosestIntSameBitCount,
-                         DefaultComparator{}, param_names);
+int main(int argc, char *argv[]) {
+    std::vector<std::string> args{argv + 1, argv + argc};
+    std::vector<std::string> param_names{"x"};
+    return GenericTestMain(args, "closest_int_same_weight.cc",
+                           "closest_int_same_weight.tsv", &ClosestIntSameBitCount,
+                           DefaultComparator{}, param_names);
 }
