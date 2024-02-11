c9002806b85d6671e0f5e795d3878c2366155537
Denis Bogdanov <bogdanov.math@gmail.com>
2024-02-11 13:53:05 +0400

    C++.

--- epi_judge_cpp/primitive_divide.cc
+++ epi_judge_cpp/primitive_divide.cc
@@ -1,12 +1,27 @@
 #include "test_framework/generic_test.h"
+
 int Divide(int x, int y) {
-  // TODO - you fill in here.
-  return 0;
+    int result = 0;
+
+    int shift = 32;
+    unsigned long long yPow = static_cast<unsigned long long>(y) << shift;
+
+    while (x >= y) {
+        while (yPow > x) {
+            yPow >>= 1;
+            --shift;
+        }
+
+        result += 1LL << shift;
+        x -= yPow;
+    }
+
+    return result;
 }
 
-int main(int argc, char* argv[]) {
-  std::vector<std::string> args{argv + 1, argv + argc};
-  std::vector<std::string> param_names{"x", "y"};
-  return GenericTestMain(args, "primitive_divide.cc", "primitive_divide.tsv",
-                         &Divide, DefaultComparator{}, param_names);
+int main(int argc, char *argv[]) {
+    std::vector<std::string> args{argv + 1, argv + argc};
+    std::vector<std::string> param_names{"x", "y"};
+    return GenericTestMain(args, "primitive_divide.cc", "primitive_divide.tsv",
+                           &Divide, DefaultComparator{}, param_names);
 }
