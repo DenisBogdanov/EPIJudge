7be76b9886ec1f12383ef1cbac8e865f739fea3d
Denis Bogdanov <bogdanov.math@gmail.com>
2024-02-17 10:37:09 +0400

    C++.

--- epi_judge_cpp/int_as_array_increment.cc
+++ epi_judge_cpp/int_as_array_increment.cc
@@ -1,16 +1,29 @@
 #include <vector>
 
 #include "test_framework/generic_test.h"
+
 using std::vector;
-vector<int> PlusOne(vector<int> A) {
-  // TODO - you fill in here.
-  return {};
+
+vector<int> PlusOne(vector<int> v) {
+    ++v.back();
+    for (int i = v.size() - 1; i > 0; --i) {
+        if (v[i] < 10) break;
+        v[i] = 0;
+        ++v[i - 1];
+    }
+
+    if (v[0] == 10) {
+        v[0] = 0;
+        v.insert(v.begin(), 1);
+    }
+
+    return v;
 }
 
-int main(int argc, char* argv[]) {
-  std::vector<std::string> args{argv + 1, argv + argc};
-  std::vector<std::string> param_names{"A"};
-  return GenericTestMain(args, "int_as_array_increment.cc",
-                         "int_as_array_increment.tsv", &PlusOne,
-                         DefaultComparator{}, param_names);
+int main(int argc, char *argv[]) {
+    std::vector<std::string> args{argv + 1, argv + argc};
+    std::vector<std::string> param_names{"A"};
+    return GenericTestMain(args, "int_as_array_increment.cc",
+                           "int_as_array_increment.tsv", &PlusOne,
+                           DefaultComparator{}, param_names);
 }
