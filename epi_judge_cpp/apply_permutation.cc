4ba9e409990589d5e1e3f1c1c3350a39bc9d4da1
Denis Bogdanov <bogdanov.math@gmail.com>
2024-02-18 13:34:49 +0400

    C++.

--- epi_judge_cpp/apply_permutation.cc
+++ epi_judge_cpp/apply_permutation.cc
@@ -1,20 +1,29 @@
 #include <vector>
 
 #include "test_framework/generic_test.h"
+
+using std::swap;
 using std::vector;
-void ApplyPermutation(vector<int> perm, vector<int>* A_ptr) {
-  // TODO - you fill in here.
-  return;
-}
-vector<int> ApplyPermutationWrapper(const vector<int>& perm, vector<int> A) {
-  ApplyPermutation(perm, &A);
-  return A;
+
+void ApplyPermutation(vector<int> perm, vector<int> *A_ptr) {
+    vector<int> &v = *A_ptr;
+    for (int i = 0; i < v.size(); ++i) {
+        while (perm[i] != i) {
+            swap(v[i], v[perm[i]]);
+            swap(perm[i], perm[perm[i]]);
+        }
+    }
 }
 
-int main(int argc, char* argv[]) {
-  std::vector<std::string> args{argv + 1, argv + argc};
-  std::vector<std::string> param_names{"perm", "A"};
-  return GenericTestMain(args, "apply_permutation.cc", "apply_permutation.tsv",
-                         &ApplyPermutationWrapper, DefaultComparator{},
-                         param_names);
+vector<int> ApplyPermutationWrapper(const vector<int> &perm, vector<int> A) {
+    ApplyPermutation(perm, &A);
+    return A;
+}
+
+int main(int argc, char *argv[]) {
+    std::vector<std::string> args{argv + 1, argv + argc};
+    std::vector<std::string> param_names{"perm", "A"};
+    return GenericTestMain(args, "apply_permutation.cc", "apply_permutation.tsv",
+                           &ApplyPermutationWrapper, DefaultComparator{},
+                           param_names);
 }
