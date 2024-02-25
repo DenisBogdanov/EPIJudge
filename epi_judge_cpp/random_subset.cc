d98ce410928a7a452772722d611d43e4516baa4f
Denis Bogdanov <bogdanov.math@gmail.com>
2024-02-25 12:02:06 +0300

    C++.

--- epi_judge_cpp/random_subset.cc
+++ epi_judge_cpp/random_subset.cc
@@ -3,53 +3,68 @@
 #include <iterator>
 #include <numeric>
 #include <vector>
+#include <random>
 
 #include "test_framework/generic_test.h"
 #include "test_framework/random_sequence_checker.h"
 #include "test_framework/timed_executor.h"
-using std::bind;
-using std::iota;
-using std::vector;
+
+using namespace std;
+
 // Returns a random k-sized subset of {0, 1, ..., n - 1}.
 vector<int> RandomSubset(int n, int k) {
-  // TODO - you fill in here.
-  return {};
-}
-bool RandomSubsetRunner(TimedExecutor& executor, int n, int k) {
-  using namespace test_framework;
-  vector<vector<int>> results;
+    vector<int> result;
+    default_random_engine seed((random_device())());
 
-  executor.Run([&] {
-    std::generate_n(back_inserter(results), 100000,
-                    std::bind(RandomSubset, n, k));
-  });
+    for (int i = 0; i < n; ++i) {
+        if (result.size() < k) {
+            result.push_back(i);
+        } else {
+            auto index = uniform_int_distribution<int>{0, static_cast<int>(i)}(seed);
+            if (index < k) {
+                result[index] = i;
+            }
+        }
+    }
 
-  int total_possible_outcomes = BinomialCoefficient(n, k);
-  vector<int> A(n);
-  iota(begin(A), end(A), 0);
-  vector<vector<int>> combinations;
-  for (int i = 0; i < BinomialCoefficient(n, k); ++i) {
-    combinations.emplace_back(ComputeCombinationIdx(A, n, k, i));
-  }
-  vector<int> sequence;
-  for (vector<int> result : results) {
-    sort(begin(result), end(result));
-    sequence.emplace_back(
-        distance(begin(combinations),
-                 find(begin(combinations), end(combinations), result)));
-  }
-  return CheckSequenceIsUniformlyRandom(sequence, total_possible_outcomes,
-                                        0.01);
+    return result;
 }
 
-void RandomSubsetWrapper(TimedExecutor& executor, int n, int k) {
-  RunFuncWithRetries(bind(RandomSubsetRunner, std::ref(executor), n, k));
+bool RandomSubsetRunner(TimedExecutor &executor, int n, int k) {
+    using namespace test_framework;
+    vector<vector<int>> results;
+
+    executor.Run([&] {
+        std::generate_n(back_inserter(results), 100000,
+                        std::bind(RandomSubset, n, k));
+    });
+
+    int total_possible_outcomes = BinomialCoefficient(n, k);
+    vector<int> A(n);
+    iota(begin(A), end(A), 0);
+    vector<vector<int>> combinations;
+    for (int i = 0; i < BinomialCoefficient(n, k); ++i) {
+        combinations.emplace_back(ComputeCombinationIdx(A, n, k, i));
+    }
+    vector<int> sequence;
+    for (vector<int> result: results) {
+        sort(begin(result), end(result));
+        sequence.emplace_back(
+                distance(begin(combinations),
+                         find(begin(combinations), end(combinations), result)));
+    }
+    return CheckSequenceIsUniformlyRandom(sequence, total_possible_outcomes,
+                                          0.01);
 }
 
-int main(int argc, char* argv[]) {
-  std::vector<std::string> args{argv + 1, argv + argc};
-  std::vector<std::string> param_names{"executor", "n", "k"};
-  return GenericTestMain(args, "random_subset.cc", "random_subset.tsv",
-                         &RandomSubsetWrapper, DefaultComparator{},
-                         param_names);
+void RandomSubsetWrapper(TimedExecutor &executor, int n, int k) {
+    RunFuncWithRetries(bind(RandomSubsetRunner, std::ref(executor), n, k));
+}
+
+int main(int argc, char *argv[]) {
+    std::vector<std::string> args{argv + 1, argv + argc};
+    std::vector<std::string> param_names{"executor", "n", "k"};
+    return GenericTestMain(args, "random_subset.cc", "random_subset.tsv",
+                           &RandomSubsetWrapper, DefaultComparator{},
+                           param_names);
 }
