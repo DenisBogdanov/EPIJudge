edb0dc85e3df3402ecf66d3d0a0ad24a6bcc0b5d
Denis Bogdanov <bogdanov.math@gmail.com>
2024-02-26 10:25:41 +0300

    C++.

--- epi_judge_cpp/string_integer_interconversion.cc
+++ epi_judge_cpp/string_integer_interconversion.cc
@@ -2,30 +2,60 @@
 
 #include "test_framework/generic_test.h"
 #include "test_framework/test_failure.h"
-using std::string;
+
+using namespace std;
 
 string IntToString(int x) {
-  // TODO - you fill in here.
-  return "0";
-}
-int StringToInt(const string& s) {
-  // TODO - you fill in here.
-  return 0;
-}
-void Wrapper(int x, const string& s) {
-  if (stoi(IntToString(x)) != x) {
-    throw TestFailure("Int to string conversion failed");
-  }
+    if (x == 0) return "0";
+    bool isNegative = x < 0;
+    long long num = abs((long long) x);
 
-  if (StringToInt(s) != x) {
-    throw TestFailure("String to int conversion failed");
-  }
+    string result;
+    while (num > 0) {
+        result += '0' + num % 10;
+        num /= 10;
+    }
+
+
+    reverse(result.begin(), result.end());
+    cout << result << endl;
+
+    return isNegative ? "-" + result : result;
 }
 
-int main(int argc, char* argv[]) {
-  std::vector<std::string> args{argv + 1, argv + argc};
-  std::vector<std::string> param_names{"x", "s"};
-  return GenericTestMain(args, "string_integer_interconversion.cc",
-                         "string_integer_interconversion.tsv", &Wrapper,
-                         DefaultComparator{}, param_names);
+int StringToInt(const string &s) {
+    int num = 0;
+    int start = 0;
+    bool isNegative = false;
+    if (s[0] == '-') {
+        start = 1;
+        isNegative = true;
+    } else if (s[0] == '+') {
+        start = 1;
+    }
+
+    for (int i = start; i < s.size(); ++i) {
+        num *= 10;
+        num += s[i] - '0';
+    }
+
+    return isNegative ? -1 * num : num;
+}
+
+void Wrapper(int x, const string &s) {
+    if (stoi(IntToString(x)) != x) {
+        throw TestFailure("Int to string conversion failed");
+    }
+
+    if (StringToInt(s) != x) {
+        throw TestFailure("String to int conversion failed");
+    }
+}
+
+int main(int argc, char *argv[]) {
+    std::vector<std::string> args{argv + 1, argv + argc};
+    std::vector<std::string> param_names{"x", "s"};
+    return GenericTestMain(args, "string_integer_interconversion.cc",
+                           "string_integer_interconversion.tsv", &Wrapper,
+                           DefaultComparator{}, param_names);
 }
