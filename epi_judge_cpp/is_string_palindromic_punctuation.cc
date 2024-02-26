06e3c08760400775821ce22dff62f417b71d0d7c
Denis Bogdanov <bogdanov.math@gmail.com>
2024-02-26 20:03:13 +0300

    C++.

--- epi_judge_cpp/is_string_palindromic_punctuation.cc
+++ epi_judge_cpp/is_string_palindromic_punctuation.cc
@@ -1,16 +1,41 @@
 #include <string>
 
 #include "test_framework/generic_test.h"
+
 using std::string;
-bool IsPalindrome(const string& s) {
-  // TODO - you fill in here.
-  return true;
+
+bool IsPalindrome(const string &s) {
+    int left = 0;
+    int right = s.size() - 1;
+
+    while (left < right) {
+        while (left < right && !isalnum(s[left])) {
+            left++;
+        }
+
+        while (left < right && !isalnum(s[right])) {
+            right--;
+        }
+
+        if (left >= right) {
+            break;
+        }
+
+        if (tolower(s[left]) != tolower(s[right])) {
+            return false;
+        }
+
+        ++left;
+        --right;
+    }
+
+    return true;
 }
 
-int main(int argc, char* argv[]) {
-  std::vector<std::string> args{argv + 1, argv + argc};
-  std::vector<std::string> param_names{"s"};
-  return GenericTestMain(args, "is_string_palindromic_punctuation.cc",
-                         "is_string_palindromic_punctuation.tsv", &IsPalindrome,
-                         DefaultComparator{}, param_names);
+int main(int argc, char *argv[]) {
+    std::vector<std::string> args{argv + 1, argv + argc};
+    std::vector<std::string> param_names{"s"};
+    return GenericTestMain(args, "is_string_palindromic_punctuation.cc",
+                           "is_string_palindromic_punctuation.tsv", &IsPalindrome,
+                           DefaultComparator{}, param_names);
 }
