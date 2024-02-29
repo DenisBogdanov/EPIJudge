ad2d07a8343758f9cd7f3f01d86d9f07bb4719f5
Denis Bogdanov <bogdanov.math@gmail.com>
2024-02-29 18:01:21 +0300

    C++.

--- epi_judge_cpp/int_square_root.cc
+++ epi_judge_cpp/int_square_root.cc
@@ -1,13 +1,28 @@
 #include "test_framework/generic_test.h"
 
 int SquareRoot(int k) {
-  // TODO - you fill in here.
-  return 0;
+    if (k < 0) return -1;
+    if (k <= 1) return k;
+
+    long long left = 0;
+    long long right = k;
+
+    while (left + 1 < right) {
+        long long candidate = left + (right - left) / 2;
+
+        if (candidate * candidate <= k) {
+            left = candidate;
+        } else {
+            right = candidate;
+        }
+    }
+
+    return (int) left;
 }
 
-int main(int argc, char* argv[]) {
-  std::vector<std::string> args{argv + 1, argv + argc};
-  std::vector<std::string> param_names{"k"};
-  return GenericTestMain(args, "int_square_root.cc", "int_square_root.tsv",
-                         &SquareRoot, DefaultComparator{}, param_names);
+int main(int argc, char *argv[]) {
+    std::vector<std::string> args{argv + 1, argv + argc};
+    std::vector<std::string> param_names{"k"};
+    return GenericTestMain(args, "int_square_root.cc", "int_square_root.tsv",
+                           &SquareRoot, DefaultComparator{}, param_names);
 }
