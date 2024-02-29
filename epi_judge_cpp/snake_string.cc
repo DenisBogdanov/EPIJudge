97b447a8972de71c3e6d1dd9b494042c10a76ba4
Denis Bogdanov <bogdanov.math@gmail.com>
2024-02-29 14:36:10 +0300

    C++.

--- epi_judge_cpp/snake_string.cc
+++ epi_judge_cpp/snake_string.cc
@@ -1,16 +1,29 @@
 #include <string>
 
 #include "test_framework/generic_test.h"
+
 using std::string;
 
-string SnakeString(const string& s) {
-  // TODO - you fill in here.
-  return "";
+string SnakeString(const string &s) {
+    string s1;
+    string s2;
+    string s3;
+
+    for (int i = 0; i < s.size(); i++) {
+        if (i % 4 == 1) {
+            s1 += s[i];
+        } else if (i % 4 == 3) {
+            s3 += s[i];
+        } else {
+            s2 += s[i];
+        }
+    }
+    return s1 + s2 + s3;
 }
 
-int main(int argc, char* argv[]) {
-  std::vector<std::string> args{argv + 1, argv + argc};
-  std::vector<std::string> param_names{"s"};
-  return GenericTestMain(args, "snake_string.cc", "snake_string.tsv",
-                         &SnakeString, DefaultComparator{}, param_names);
+int main(int argc, char *argv[]) {
+    std::vector<std::string> args{argv + 1, argv + argc};
+    std::vector<std::string> param_names{"s"};
+    return GenericTestMain(args, "snake_string.cc", "snake_string.tsv",
+                           &SnakeString, DefaultComparator{}, param_names);
 }
