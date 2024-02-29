ab8d4b90612f557ca73cf7ba35500123967b0b98
Denis Bogdanov <bogdanov.math@gmail.com>
2024-02-29 18:13:34 +0300

    C++.

--- epi_judge_cpp/real_square_root.cc
+++ epi_judge_cpp/real_square_root.cc
@@ -1,13 +1,27 @@
 #include "test_framework/generic_test.h"
 
 double SquareRoot(double x) {
-  // TODO - you fill in here.
-  return 0.0;
+    if (x < 0) return -1;
+
+    double left = 0.0;
+    double right = x + 1;
+
+    while (right - left > 1e-8) {
+        double candidate = (left + right) / 2;
+
+        if (candidate * candidate < x) {
+            left = candidate;
+        } else {
+            right = candidate;
+        }
+    }
+
+    return left;
 }
 
-int main(int argc, char* argv[]) {
-  std::vector<std::string> args{argv + 1, argv + argc};
-  std::vector<std::string> param_names{"x"};
-  return GenericTestMain(args, "real_square_root.cc", "real_square_root.tsv",
-                         &SquareRoot, DefaultComparator{}, param_names);
+int main(int argc, char *argv[]) {
+    std::vector<std::string> args{argv + 1, argv + argc};
+    std::vector<std::string> param_names{"x"};
+    return GenericTestMain(args, "real_square_root.cc", "real_square_root.tsv",
+                           &SquareRoot, DefaultComparator{}, param_names);
 }
