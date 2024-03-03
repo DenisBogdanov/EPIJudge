a3c149dc92eb6da356fc76a9302ce38c28f17195
Denis Bogdanov <bogdanov.math@gmail.com>
2024-03-03 14:50:23 +0400

    C++.

--- epi_judge_cpp/h_index.cc
+++ epi_judge_cpp/h_index.cc
@@ -1,15 +1,26 @@
 #include <vector>
 
 #include "test_framework/generic_test.h"
+
 using std::vector;
+using std::sort;
+
 int HIndex(vector<int> citations) {
-  // TODO - you fill in here.
-  return 0;
+    sort(citations.begin(), citations.end());
+    int const n = citations.size();
+
+    for (int i = 0; i < n; ++i) {
+        if (citations[i] >= n - i) {
+            return n - i;
+        }
+    }
+
+    return 0;
 }
 
-int main(int argc, char* argv[]) {
-  std::vector<std::string> args{argv + 1, argv + argc};
-  std::vector<std::string> param_names{"citations"};
-  return GenericTestMain(args, "h_index.cc", "h_index.tsv", &HIndex,
-                         DefaultComparator{}, param_names);
+int main(int argc, char *argv[]) {
+    std::vector<std::string> args{argv + 1, argv + argc};
+    std::vector<std::string> param_names{"citations"};
+    return GenericTestMain(args, "h_index.cc", "h_index.tsv", &HIndex,
+                           DefaultComparator{}, param_names);
 }
