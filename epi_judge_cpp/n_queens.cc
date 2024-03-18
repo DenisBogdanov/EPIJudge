6416a52c356eb4ec7610fd41d76dbbbc0cb269d8
Denis Bogdanov <bogdanov.math@gmail.com>
2024-03-18 20:26:14 +0400

    C++.

--- epi_judge_cpp/n_queens.cc
+++ epi_judge_cpp/n_queens.cc
@@ -3,16 +3,58 @@
 #include <vector>
 
 #include "test_framework/generic_test.h"
+
 using std::vector;
 
+vector<vector<int>> result;
+
+void recur(int n, int col, vector<int>& curr);
+
+bool ok(int n, const vector<int>& curr, int row);
+
 vector<vector<int>> NQueens(int n) {
-  // TODO - you fill in here.
-  return {};
+    result.clear();
+
+    vector<int> curr;
+    recur(n, 0, curr);
+
+    return result;
+}
+
+void recur(int n, int col, vector<int>& curr) {
+    if (curr.size() == n) {
+        result.emplace_back(curr);
+        return;
+    }
+
+    for (int row = 0; row < n; ++row) {
+        if (ok(n, curr, row)) {
+            curr.push_back(row);
+            recur(n, col + 1, curr);
+            curr.pop_back();
+        }
+    }
+}
+
+bool ok(int n, const vector<int>& curr, int row) {
+    if (std::find(curr.begin(), curr.end(), row) != curr.end()) {
+        return false;
+    }
+
+    int nextCol = static_cast<int>(curr.size());
+
+    for (int col = 0; col != curr.size(); ++col) {
+        if ((col + curr[col] == nextCol + row) || (col - curr[col] == nextCol - row)) {
+            return false;
+        }
+    }
+
+    return true;
 }
 
 int main(int argc, char* argv[]) {
-  std::vector<std::string> args{argv + 1, argv + argc};
-  std::vector<std::string> param_names{"n"};
-  return GenericTestMain(args, "n_queens.cc", "n_queens.tsv", &NQueens,
-                         UnorderedComparator{}, param_names);
+    std::vector<std::string> args{argv + 1, argv + argc};
+    std::vector<std::string> param_names{"n"};
+    return GenericTestMain(args, "n_queens.cc", "n_queens.tsv", &NQueens,
+                           UnorderedComparator{}, param_names);
 }
