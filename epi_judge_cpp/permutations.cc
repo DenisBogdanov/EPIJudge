6c274046670013540f60d2b821466ea5feffe2be
Denis Bogdanov <bogdanov.math@gmail.com>
2024-03-18 20:45:11 +0400

    C++.

--- epi_judge_cpp/permutations.cc
+++ epi_judge_cpp/permutations.cc
@@ -1,16 +1,39 @@
 #include <vector>
 
 #include "test_framework/generic_test.h"
-using std::vector;
 
-vector<vector<int>> Permutations(vector<int> A) {
-  // TODO - you fill in here.
-  return {};
+using std::vector;
+using std::swap;
+
+vector<vector<int>> result;
+
+void recur(vector<int>& v, int index);
+
+vector<vector<int>> Permutations(vector<int> v) {
+    result.clear();
+
+    recur(v, 0);
+
+    return result;
+}
+
+void recur(vector<int>& v, int index) {
+    if (index == v.size()) {
+        result.emplace_back(v);
+        return;
+    }
+
+    for (int i = index; i != v.size(); ++i) {
+        swap(v[i], v[index]);
+
+        recur(v, index + 1);
+        swap(v[i], v[index]);
+    }
 }
 
 int main(int argc, char* argv[]) {
-  std::vector<std::string> args{argv + 1, argv + argc};
-  std::vector<std::string> param_names{"A"};
-  return GenericTestMain(args, "permutations.cc", "permutations.tsv",
-                         &Permutations, UnorderedComparator{}, param_names);
+    std::vector<std::string> args{argv + 1, argv + argc};
+    std::vector<std::string> param_names{"A"};
+    return GenericTestMain(args, "permutations.cc", "permutations.tsv",
+                           &Permutations, UnorderedComparator{}, param_names);
 }
