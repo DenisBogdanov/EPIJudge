8f188943e75111d222de85163a465a5ec9651c73
Denis Bogdanov <bogdanov.math@gmail.com>
2024-03-18 19:50:37 +0400

    C++.

--- epi_judge_cpp/hanoi.cc
+++ epi_judge_cpp/hanoi.cc
@@ -6,49 +6,71 @@
 #include "test_framework/generic_test.h"
 #include "test_framework/test_failure.h"
 #include "test_framework/timed_executor.h"
+
 using std::array;
 using std::stack;
 using std::vector;
+
 const int kNumPegs = 3;
+
+vector<vector<int>> result;
+
+void recur(int num_rings, int start, int helper, int end);
+
 vector<vector<int>> ComputeTowerHanoi(int num_rings) {
-  // TODO - you fill in here.
-  return {};
+    result.clear();
+
+    recur(num_rings, 0, 1, 2);
+
+    return result;
 }
-void ComputeTowerHanoiWrapper(TimedExecutor& executor, int num_rings) {
-  array<stack<int>, kNumPegs> pegs;
-  for (int i = num_rings; i >= 1; --i) {
-    pegs[0].push(i);
-  }
 
-  vector<vector<int>> result =
-      executor.Run([&] { return ComputeTowerHanoi(num_rings); });
-
-  for (const vector<int>& operation : result) {
-    int from_peg = operation[0], to_peg = operation[1];
-    if (!pegs[to_peg].empty() && pegs[from_peg].top() >= pegs[to_peg].top()) {
-      throw TestFailure("Illegal move from " +
-                        std::to_string(pegs[from_peg].top()) + " to " +
-                        std::to_string(pegs[to_peg].top()));
+void recur(int num_rings, int start, int helper, int end) {
+    if (num_rings == 1) {
+        result.push_back({start, end});
+        return;
     }
-    pegs[to_peg].push(pegs[from_peg].top());
-    pegs[from_peg].pop();
-  }
-  array<stack<int>, kNumPegs> expected_pegs1, expected_pegs2;
-  for (int i = num_rings; i >= 1; --i) {
-    expected_pegs1[1].push(i);
-  }
-  for (int i = num_rings; i >= 1; --i) {
-    expected_pegs2[2].push(i);
-  }
-  if (pegs != expected_pegs1 && pegs != expected_pegs2) {
-    throw TestFailure("Pegs doesn't place in the right configuration");
-  }
+
+    recur(num_rings - 1, start, end, helper);
+    result.push_back({start, end});
+    recur(num_rings - 1, helper, start, end);
 }
 
-int main(int argc, char* argv[]) {
-  std::vector<std::string> args{argv + 1, argv + argc};
-  std::vector<std::string> param_names{"executor", "num_rings"};
-  return GenericTestMain(args, "hanoi.cc", "hanoi.tsv",
-                         &ComputeTowerHanoiWrapper, DefaultComparator{},
-                         param_names);
+void ComputeTowerHanoiWrapper(TimedExecutor &executor, int num_rings) {
+    array<stack<int>, kNumPegs> pegs;
+    for (int i = num_rings; i >= 1; --i) {
+        pegs[0].push(i);
+    }
+
+    vector<vector<int>> result =
+            executor.Run([&] { return ComputeTowerHanoi(num_rings); });
+
+    for (const vector<int> &operation: result) {
+        int from_peg = operation[0], to_peg = operation[1];
+        if (!pegs[to_peg].empty() && pegs[from_peg].top() >= pegs[to_peg].top()) {
+            throw TestFailure("Illegal move from " +
+                              std::to_string(pegs[from_peg].top()) + " to " +
+                              std::to_string(pegs[to_peg].top()));
+        }
+        pegs[to_peg].push(pegs[from_peg].top());
+        pegs[from_peg].pop();
+    }
+    array<stack<int>, kNumPegs> expected_pegs1, expected_pegs2;
+    for (int i = num_rings; i >= 1; --i) {
+        expected_pegs1[1].push(i);
+    }
+    for (int i = num_rings; i >= 1; --i) {
+        expected_pegs2[2].push(i);
+    }
+    if (pegs != expected_pegs1 && pegs != expected_pegs2) {
+        throw TestFailure("Pegs doesn't place in the right configuration");
+    }
+}
+
+int main(int argc, char *argv[]) {
+    std::vector<std::string> args{argv + 1, argv + argc};
+    std::vector<std::string> param_names{"executor", "num_rings"};
+    return GenericTestMain(args, "hanoi.cc", "hanoi.tsv",
+                           &ComputeTowerHanoiWrapper, DefaultComparator{},
+                           param_names);
 }
