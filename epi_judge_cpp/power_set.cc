fdc492bad8dcc60e5f31031d5a3ea4ff58467ec6
Denis Bogdanov <bogdanov.math@gmail.com>
2024-03-18 20:54:23 +0400

    C++.

--- epi_judge_cpp/power_set.cc
+++ epi_judge_cpp/power_set.cc
@@ -1,16 +1,31 @@
 #include <vector>
 
 #include "test_framework/generic_test.h"
+
 using std::vector;
 
 vector<vector<int>> GeneratePowerSet(const vector<int>& input_set) {
-  // TODO - you fill in here.
-  return {};
+    vector<vector<int>> result;
+
+    size_t n = input_set.size();
+    vector<int> curr;
+    for (int mask = 0; mask != (1 << n); ++mask) {
+        curr.clear();
+        for (int i = 0; i != n; ++i) {
+            if (mask & (1 << i)) {
+                curr.push_back(input_set[i]);
+            }
+        }
+
+        result.emplace_back(curr);
+    }
+    
+    return result;
 }
 
 int main(int argc, char* argv[]) {
-  std::vector<std::string> args{argv + 1, argv + argc};
-  std::vector<std::string> param_names{"input_set"};
-  return GenericTestMain(args, "power_set.cc", "power_set.tsv",
-                         &GeneratePowerSet, UnorderedComparator{}, param_names);
+    std::vector<std::string> args{argv + 1, argv + argc};
+    std::vector<std::string> param_names{"input_set"};
+    return GenericTestMain(args, "power_set.cc", "power_set.tsv",
+                           &GeneratePowerSet, UnorderedComparator{}, param_names);
 }
