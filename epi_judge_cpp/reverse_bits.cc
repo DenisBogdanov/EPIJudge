60bcea0b3010beb32e401433d164bfc7f3b96e79
Denis Bogdanov <bogdanov.math@gmail.com>
2024-09-04 22:40:21 +0400

    C++.

--- epi_judge_cpp/reverse_bits.cc
+++ epi_judge_cpp/reverse_bits.cc
@@ -1,7 +1,15 @@
 #include "test_framework/generic_test.h"
 unsigned long long ReverseBits(unsigned long long x) {
-  // TODO - you fill in here.
-  return 0;
+    int left = 0;
+    int right = 63;
+    while (left < right) {
+        if (((x >> left) & 1) != ((x >> right) & 1)) {
+            x ^= (1LL << left) | (1LL << right);
+        }
+        left++;
+        right--;
+    }
+    return x;
 }
 
 int main(int argc, char* argv[]) {
