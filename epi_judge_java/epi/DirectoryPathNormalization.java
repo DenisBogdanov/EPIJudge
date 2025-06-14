172194d7b8025b4c5b44bb939a09899dc37a7df8
Denis Bogdanov <bogdanov.math@gmail.com>
2025-06-14 14:48:11 +0200

    Java.

--- epi_judge_java/epi/DirectoryPathNormalization.java
+++ epi_judge_java/epi/DirectoryPathNormalization.java
@@ -3,12 +3,36 @@ package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 
-public class DirectoryPathNormalization {
-    @EpiTest(testDataFile = "directory_path_normalization.tsv")
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
+public class DirectoryPathNormalization {
+
+    @EpiTest(testDataFile = "directory_path_normalization.tsv")
     public static String shortestEquivalentPath(String path) {
-        // TODO - you fill in here.
-        return "";
+        boolean isAbsolute = path.startsWith("/");
+        var elementsStack = new ArrayList<String>();
+
+        Arrays.stream(path.split("/"))
+                .filter(element -> !element.isBlank())
+                .filter(element -> !".".equals(element))
+                .forEach(element -> {
+                    if ("..".equals(element)) {
+                        if (elementsStack.isEmpty()) {
+                            elementsStack.add(element);
+                        } else if ("..".equals(elementsStack.getLast())) {
+                            elementsStack.add(element);
+                        } else {
+                            elementsStack.removeLast();
+                        }
+                    } else {
+                        elementsStack.add(element);
+                    }
+                });
+
+        return elementsStack.stream()
+                .collect(Collectors.joining("/", isAbsolute ? "/" : "", ""));
     }
 
     public static void main(String[] args) {
