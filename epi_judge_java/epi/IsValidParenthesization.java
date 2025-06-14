a7010653b26be13b6aafbcc95adf14493fa4cd45
Denis Bogdanov <bogdanov.math@gmail.com>
2025-06-14 14:24:06 +0200

    Java.

--- epi_judge_java/epi/IsValidParenthesization.java
+++ epi_judge_java/epi/IsValidParenthesization.java
@@ -3,12 +3,34 @@ package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 
-public class IsValidParenthesization {
-    @EpiTest(testDataFile = "is_valid_parenthesization.tsv")
+import java.util.ArrayList;
+import java.util.Map;
 
+public class IsValidParenthesization {
+    private static final Map<Character, Character> CLOSED_TO_OPENED_BRACKETS_MAP = Map.of(
+            ')', '(',
+            ']', '[',
+            '}', '{'
+    );
+
+    @EpiTest(testDataFile = "is_valid_parenthesization.tsv")
     public static boolean isWellFormed(String s) {
-        // TODO - you fill in here.
-        return true;
+        var bracketsStack = new ArrayList<Character>();
+        for (char bracket : s.toCharArray()) {
+            switch (bracket) {
+                case ')', ']', '}' -> {
+                    if (bracketsStack.isEmpty()) {
+                        return false;
+                    }
+                    if (bracketsStack.removeLast() != CLOSED_TO_OPENED_BRACKETS_MAP.get(bracket)) {
+                        return false;
+                    }
+                }
+                default -> bracketsStack.add(bracket);
+            }
+        }
+
+        return bracketsStack.isEmpty();
     }
 
     public static void main(String[] args) {
