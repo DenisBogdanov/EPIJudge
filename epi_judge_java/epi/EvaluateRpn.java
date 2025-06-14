d52d654f3921f59e169c5ee73186a04d26ccdc1c
Denis Bogdanov <bogdanov.math@gmail.com>
2025-06-14 13:14:31 +0200

    Java.

--- epi_judge_java/epi/EvaluateRpn.java
+++ epi_judge_java/epi/EvaluateRpn.java
@@ -3,12 +3,45 @@ package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 
-public class EvaluateRpn {
-    @EpiTest(testDataFile = "evaluate_rpn.tsv")
+import java.util.LinkedList;
 
+/**
+ * ToDo: validation is skipped (expecting only valid expressions)
+ */
+public class EvaluateRpn {
+
+    @EpiTest(testDataFile = "evaluate_rpn.tsv")
     public static int eval(String expression) {
-        // TODO - you fill in here.
-        return 0;
+        var elements = expression.split(",");
+        var numsStack = new LinkedList<Integer>();
+        for (String element : elements) {
+            switch (element) {
+                case "+" -> {
+                    int b = numsStack.poll();
+                    int a = numsStack.poll();
+                    numsStack.push(a + b);
+                }
+                case "-" -> {
+                    int b = numsStack.poll();
+                    int a = numsStack.poll();
+                    numsStack.push(a - b);
+                }
+                case "/" -> {
+                    int b = numsStack.poll();
+                    int a = numsStack.poll();
+                    numsStack.push(a / b);
+                }
+                case "*" -> {
+                    int b = numsStack.poll();
+                    int a = numsStack.poll();
+                    numsStack.push(a * b);
+                }
+                default -> {
+                    numsStack.push(Integer.parseInt(element));
+                }
+            }
+        }
+        return numsStack.poll();
     }
 
     public static void main(String[] args) {
