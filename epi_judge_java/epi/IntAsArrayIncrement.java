87ad63dee2fbe778297ec3cdd77d6b405fd7f895
Denis Bogdanov <bogdanov.math@gmail.com>
2025-06-21 14:15:40 +0200

    Java.

--- epi_judge_java/epi/IntAsArrayIncrement.java
+++ epi_judge_java/epi/IntAsArrayIncrement.java
@@ -6,10 +6,27 @@ import epi.test_framework.GenericTest;
 import java.util.List;
 
 public class IntAsArrayIncrement {
+
     @EpiTest(testDataFile = "int_as_array_increment.tsv")
-    public static List<Integer> plusOne(List<Integer> A) {
-        // TODO - you fill in here.
-        return null;
+    public static List<Integer> plusOne(List<Integer> list) {
+        int carry = 1;
+        int index = list.size() - 1;
+        while (index >= 0) {
+            if (list.get(index) == 9) {
+                list.set(index, 0);
+            } else {
+                list.set(index, list.get(index) + 1);
+                carry = 0;
+                break;
+            }
+            index--;
+        }
+
+        if (carry == 1) {
+            list.addFirst(1);
+        }
+
+        return list;
     }
 
     public static void main(String[] args) {
