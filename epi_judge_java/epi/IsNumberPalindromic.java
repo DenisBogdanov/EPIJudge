e6914409827f2f19e2e6bdc1661b7240e8e7cd9d
Denis Bogdanov <bogdanov.math@gmail.com>
2025-06-21 13:27:50 +0200

    Java.

--- epi_judge_java/epi/IsNumberPalindromic.java
+++ epi_judge_java/epi/IsNumberPalindromic.java
@@ -4,10 +4,14 @@ import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 
 public class IsNumberPalindromic {
+
     @EpiTest(testDataFile = "is_number_palindromic.tsv")
     public static boolean isPalindromeNumber(int x) {
-        // TODO - you fill in here.
-        return true;
+        if (x < 0) {
+            return false;
+        }
+
+        return x == ReverseDigits.reverse(x);
     }
 
     public static void main(String[] args) {
