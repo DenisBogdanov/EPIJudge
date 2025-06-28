ded7d2cf0c6e901219e1127c1e579461b345d198
Denis Bogdanov <bogdanov.math@gmail.com>
2025-06-28 14:24:47 +0200

    Java.

--- epi_judge_java/epi/BuyAndSellStockTwice.java
+++ epi_judge_java/epi/BuyAndSellStockTwice.java
@@ -6,10 +6,37 @@ import epi.test_framework.GenericTest;
 import java.util.List;
 
 public class BuyAndSellStockTwice {
+
     @EpiTest(testDataFile = "buy_and_sell_stock_twice.tsv")
     public static double buyAndSellStockTwice(List<Double> prices) {
-        // TODO - you fill in here.
-        return 0.0;
+        var size = prices.size();
+        var buyFirstTime = new double[size];
+        var minPriceSoFar = prices.getFirst();
+        var maxProfit = 0.0;
+
+        for (int i = 0; i < prices.size(); i++) {
+            Double price = prices.get(i);
+            minPriceSoFar = Math.min(minPriceSoFar, price);
+            maxProfit = Math.max(maxProfit, price - minPriceSoFar);
+            buyFirstTime[i] = maxProfit;
+        }
+
+        var result = maxProfit;
+
+        var buySecondTime = new double[size];
+        var maxPriceSoFar = prices.getLast();
+        maxProfit = 0.0;
+        for (int i = size - 1; i >= 0; i--) {
+            maxPriceSoFar = Math.max(maxPriceSoFar, prices.get(i));
+            maxProfit = Math.max(maxProfit, maxPriceSoFar - prices.get(i));
+            buySecondTime[i] = maxProfit;
+        }
+
+        for (int i = 0; i < buyFirstTime.length - 2; i++) {
+            result = Math.max(result, buyFirstTime[i] + buySecondTime[i + 1]);
+        }
+
+        return result;
     }
 
     public static void main(String[] args) {
