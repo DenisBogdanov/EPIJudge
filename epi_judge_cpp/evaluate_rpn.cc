abb675d74dde15195ba5a8a4287ae0c7f817b0b4
Denis Bogdanov <bogdanov.math@gmail.com>
2026-02-14 23:15:19 +0100

    C++.

--- epi_judge_cpp/evaluate_rpn.cc
+++ epi_judge_cpp/evaluate_rpn.cc
@@ -1,15 +1,48 @@
 #include <string>
 
 #include "test_framework/generic_test.h"
-using std::string;
-int Evaluate(const string& expression) {
-  // TODO - you fill in here.
-  return 0;
+
+using namespace std;
+
+int Evaluate(const string &expression) {
+    stringstream ss(expression);
+    string token;
+    deque<int> stack;
+    while (getline(ss, token, ',')) {
+        if (token == "*") {
+            int a = stack.front();
+            stack.pop_front();
+            int b = stack.front();
+            stack.pop_front();
+            stack.push_front(a * b);
+        } else if (token == "+") {
+            int a = stack.front();
+            stack.pop_front();
+            int b = stack.front();
+            stack.pop_front();
+            stack.push_front(a + b);
+        } else if (token == "-") {
+            int a = stack.front();
+            stack.pop_front();
+            int b = stack.front();
+            stack.pop_front();
+            stack.push_front(b - a);
+        } else if (token == "/") {
+            int a = stack.front();
+            stack.pop_front();
+            int b = stack.front();
+            stack.pop_front();
+            stack.push_front(b / a);
+        } else {
+            stack.push_front(stoi(token));
+        }
+    }
+    return stack.front();
 }
 
-int main(int argc, char* argv[]) {
-  std::vector<std::string> args{argv + 1, argv + argc};
-  std::vector<std::string> param_names{"expression"};
-  return GenericTestMain(args, "evaluate_rpn.cc", "evaluate_rpn.tsv", &Evaluate,
-                         DefaultComparator{}, param_names);
+int main(int argc, char *argv[]) {
+    std::vector<std::string> args{argv + 1, argv + argc};
+    std::vector<std::string> param_names{"expression"};
+    return GenericTestMain(args, "evaluate_rpn.cc", "evaluate_rpn.tsv", &Evaluate,
+                           DefaultComparator{}, param_names);
 }
