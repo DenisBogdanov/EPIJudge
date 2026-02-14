f7772a89cf282823957b55dd5229a33f7af8e360
Denis Bogdanov <bogdanov.math@gmail.com>
2026-02-14 07:43:41 +0100

    C++.

--- epi_judge_cpp/even_odd_array.cc
+++ epi_judge_cpp/even_odd_array.cc
@@ -4,37 +4,44 @@
 #include "test_framework/generic_test.h"
 #include "test_framework/test_failure.h"
 #include "test_framework/timed_executor.h"
+
 using std::vector;
 
-void EvenOdd(vector<int>* A_ptr) {
-  // TODO - you fill in here.
-  return;
-}
-void EvenOddWrapper(TimedExecutor& executor, vector<int> A) {
-  std::multiset<int> before(begin(A), end(A));
-
-  executor.Run([&] { EvenOdd(&A); });
-
-  bool in_odd = false;
-  for (int a : A) {
-    if (a % 2 == 0) {
-      if (in_odd) {
-        throw TestFailure("Even elements appear in odd part");
-      }
-    } else {
-      in_odd = true;
+void EvenOdd(vector<int> &nums) {
+    int even_idx = 0;
+    for (int i = 0; i < nums.size(); ++i) {
+        if (nums[i] % 2 == 0) {
+            std::swap(nums[even_idx], nums[i]);
+            even_idx++;
+        }
     }
-  }
-
-  std::multiset<int> after(begin(A), end(A));
-  if (before != after) {
-    throw TestFailure("Elements mismatch");
-  }
 }
 
-int main(int argc, char* argv[]) {
-  std::vector<std::string> args{argv + 1, argv + argc};
-  std::vector<std::string> param_names{"executor", "A"};
-  return GenericTestMain(args, "even_odd_array.cc", "even_odd_array.tsv",
-                         &EvenOddWrapper, DefaultComparator{}, param_names);
+void EvenOddWrapper(TimedExecutor &executor, vector<int> A) {
+    std::multiset<int> before(begin(A), end(A));
+
+    executor.Run([&] { EvenOdd(A); });
+
+    bool in_odd = false;
+    for (int a: A) {
+        if (a % 2 == 0) {
+            if (in_odd) {
+                throw TestFailure("Even elements appear in odd part");
+            }
+        } else {
+            in_odd = true;
+        }
+    }
+
+    std::multiset<int> after(begin(A), end(A));
+    if (before != after) {
+        throw TestFailure("Elements mismatch");
+    }
+}
+
+int main(int argc, char *argv[]) {
+    std::vector<std::string> args{argv + 1, argv + argc};
+    std::vector<std::string> param_names{"executor", "A"};
+    return GenericTestMain(args, "even_odd_array.cc", "even_odd_array.tsv",
+                           &EvenOddWrapper, DefaultComparator{}, param_names);
 }
