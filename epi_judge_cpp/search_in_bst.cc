cdfd061876758e4a7c99965ee01e33e9b69052ea
Denis Bogdanov <bogdanov.math@gmail.com>
2026-02-15 00:07:38 +0100

    C++.

--- epi_judge_cpp/search_in_bst.cc
+++ epi_judge_cpp/search_in_bst.cc
@@ -2,19 +2,27 @@
 
 #include "bst_node.h"
 #include "test_framework/generic_test.h"
+
 using std::unique_ptr;
-BstNode<int>* SearchBST(const unique_ptr<BstNode<int>>& tree, int key) {
-  // TODO - you fill in here.
-  return nullptr;
-}
-int SearchBSTWrapper(const unique_ptr<BstNode<int>>& tree, int key) {
-  auto result = SearchBST(tree, key);
-  return result ? result->data : -1;
+
+BstNode<int> *SearchBST(const unique_ptr<BstNode<int>> &tree, int key) {
+    auto runner = tree.get();
+    while (runner != nullptr) {
+        if (runner->data == key) return runner;
+        else if (runner->data > key) runner = runner->left.get();
+        else runner = runner->right.get();
+    }
+    return nullptr;
 }
 
-int main(int argc, char* argv[]) {
-  std::vector<std::string> args{argv + 1, argv + argc};
-  std::vector<std::string> param_names{"tree", "key"};
-  return GenericTestMain(args, "search_in_bst.cc", "search_in_bst.tsv",
-                         &SearchBSTWrapper, DefaultComparator{}, param_names);
+int SearchBSTWrapper(const unique_ptr<BstNode<int>> &tree, int key) {
+    auto result = SearchBST(tree, key);
+    return result ? result->data : -1;
+}
+
+int main(int argc, char *argv[]) {
+    std::vector<std::string> args{argv + 1, argv + argc};
+    std::vector<std::string> param_names{"tree", "key"};
+    return GenericTestMain(args, "search_in_bst.cc", "search_in_bst.tsv",
+                           &SearchBSTWrapper, DefaultComparator{}, param_names);
 }
