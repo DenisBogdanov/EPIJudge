3f3e42a6caeea409d40a3e0eacbbaa60f8673656
Denis Bogdanov <bogdanov.math@gmail.com>
2026-02-14 21:45:56 +0100

    C++.

--- epi_judge_cpp/sorted_lists_merge.cc
+++ epi_judge_cpp/sorted_lists_merge.cc
@@ -1,15 +1,31 @@
 #include "list_node.h"
 #include "test_framework/generic_test.h"
-shared_ptr<ListNode<int>> MergeTwoSortedLists(shared_ptr<ListNode<int>> L1,
-                                              shared_ptr<ListNode<int>> L2) {
-  // TODO - you fill in here.
-  return nullptr;
+
+using namespace std;
+
+shared_ptr<ListNode<int>> MergeTwoSortedLists(shared_ptr<ListNode<int>> list1,
+                                              shared_ptr<ListNode<int>> list2) {
+
+    auto dummy = make_shared<ListNode<int>>(-1);
+    auto runner = dummy;
+    while (list1 != nullptr || list2 != nullptr) {
+        if (list2 == nullptr || (list1 != nullptr && list1->data < list2->data)) {
+            runner->next = make_shared<ListNode<int>>(list1->data);
+            list1 = list1->next;
+        } else {
+            runner->next = make_shared<ListNode<int>>(list2->data);
+            list2 = list2->next;
+        }
+        runner = runner->next;
+    }
+
+    return dummy->next;
 }
 
-int main(int argc, char* argv[]) {
-  std::vector<std::string> args{argv + 1, argv + argc};
-  std::vector<std::string> param_names{"L1", "L2"};
-  return GenericTestMain(args, "sorted_lists_merge.cc",
-                         "sorted_lists_merge.tsv", &MergeTwoSortedLists,
-                         DefaultComparator{}, param_names);
+int main(int argc, char *argv[]) {
+    std::vector<std::string> args{argv + 1, argv + argc};
+    std::vector<std::string> param_names{"L1", "L2"};
+    return GenericTestMain(args, "sorted_lists_merge.cc",
+                           "sorted_lists_merge.tsv", &MergeTwoSortedLists,
+                           DefaultComparator{}, param_names);
 }
