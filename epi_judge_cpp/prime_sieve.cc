56cfc3a56743aafaf0523250c1f162cf5fe74433
Denis Bogdanov <bogdanov.math@gmail.com>
2026-02-14 12:48:31 +0100

    C++.

--- epi_judge_cpp/prime_sieve.cc
+++ epi_judge_cpp/prime_sieve.cc
@@ -1,16 +1,30 @@
 #include <vector>
 
 #include "test_framework/generic_test.h"
+
 using std::vector;
+
 // Given n, return all primes up to and including n.
 vector<int> GeneratePrimes(int n) {
-  // TODO - you fill in here.
-  return {};
+    vector<bool> is_prime(n + 1, true);
+    for (int num = 2; num <= n; ++num) {
+        if (is_prime[num]) {
+            for (int not_prime = num * 2; not_prime <= n; not_prime += num) {
+                is_prime[not_prime] = false;
+            }
+        }
+    }
+
+    vector<int> ans;
+    for (int num = 2; num <= n; ++num) {
+        if (is_prime[num]) ans.push_back(num);
+    }
+    return ans;
 }
 
-int main(int argc, char* argv[]) {
-  std::vector<std::string> args{argv + 1, argv + argc};
-  std::vector<std::string> param_names{"n"};
-  return GenericTestMain(args, "prime_sieve.cc", "prime_sieve.tsv",
-                         &GeneratePrimes, DefaultComparator{}, param_names);
+int main(int argc, char *argv[]) {
+    std::vector<std::string> args{argv + 1, argv + argc};
+    std::vector<std::string> param_names{"n"};
+    return GenericTestMain(args, "prime_sieve.cc", "prime_sieve.tsv",
+                           &GeneratePrimes, DefaultComparator{}, param_names);
 }
