25000e0a4478073a08227fa1f26ba6fa5d2fa076
Denis Bogdanov <bogdanov.math@gmail.com>
2026-02-15 00:04:58 +0100

    C++.

--- epi_judge_cpp/intersect_sorted_arrays.cc
+++ epi_judge_cpp/intersect_sorted_arrays.cc
@@ -1,18 +1,32 @@
 #include <vector>
 
 #include "test_framework/generic_test.h"
-using std::vector;
 
-vector<int> IntersectTwoSortedArrays(const vector<int>& A,
-                                     const vector<int>& B) {
-  // TODO - you fill in here.
-  return {};
+using namespace std;
+
+vector<int> IntersectTwoSortedArrays(const vector<int> &a, const vector<int> &b) {
+    set<int> unique;
+    if (a.size() > b.size()) {
+        for (int num: b) {
+            if (std::binary_search(a.begin(), a.end(), num)) {
+                unique.insert(num);
+            }
+        }
+    } else {
+        for (int num: a) {
+            if (std::binary_search(b.begin(), b.end(), num)) {
+                unique.insert(num);
+            }
+        }
+    }
+    vector<int> ans(unique.begin(), unique.end());
+    return ans;
 }
 
-int main(int argc, char* argv[]) {
-  std::vector<std::string> args{argv + 1, argv + argc};
-  std::vector<std::string> param_names{"A", "B"};
-  return GenericTestMain(
-      args, "intersect_sorted_arrays.cc", "intersect_sorted_arrays.tsv",
-      &IntersectTwoSortedArrays, DefaultComparator{}, param_names);
+int main(int argc, char *argv[]) {
+    std::vector<std::string> args{argv + 1, argv + argc};
+    std::vector<std::string> param_names{"A", "B"};
+    return GenericTestMain(
+            args, "intersect_sorted_arrays.cc", "intersect_sorted_arrays.tsv",
+            &IntersectTwoSortedArrays, DefaultComparator{}, param_names);
 }
