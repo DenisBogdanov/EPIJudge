92e3fc33ef96f913852d9accb8b8a1d9957ca3fa
Denis Bogdanov <bogdanov.math@gmail.com>
2026-02-27 23:39:01 +0100

    C++.

--- epi_judge_cpp/power_x_y.cc
+++ epi_judge_cpp/power_x_y.cc
@@ -1,12 +1,25 @@
 #include "test_framework/generic_test.h"
+
 double Power(double x, int y) {
-  // TODO - you fill in here.
-  return 0.0;
+    if (y < 0) {
+        x = 1 / x;
+        y = -y;
+    }
+    double ans = 1;
+    double mult = x;
+    while (y) {
+        if (y & 1) {
+            ans *= mult;
+        }
+        mult *= mult;
+        y /= 2;
+    }
+    return ans;
 }
 
-int main(int argc, char* argv[]) {
-  std::vector<std::string> args{argv + 1, argv + argc};
-  std::vector<std::string> param_names{"x", "y"};
-  return GenericTestMain(args, "power_x_y.cc", "power_x_y.tsv", &Power,
-                         DefaultComparator{}, param_names);
+int main(int argc, char *argv[]) {
+    std::vector<std::string> args{argv + 1, argv + argc};
+    std::vector<std::string> param_names{"x", "y"};
+    return GenericTestMain(args, "power_x_y.cc", "power_x_y.tsv", &Power,
+                           DefaultComparator{}, param_names);
 }
