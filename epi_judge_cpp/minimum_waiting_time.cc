a22ec1f75458795b33235ad1ed8d6845870a53f6
Denis Bogdanov <bogdanov.math@gmail.com>
2026-02-27 22:07:44 +0100

    C++.

--- epi_judge_cpp/minimum_waiting_time.cc
+++ epi_judge_cpp/minimum_waiting_time.cc
@@ -1,17 +1,21 @@
 #include <vector>
 
 #include "test_framework/generic_test.h"
-using std::vector;
+using namespace std;
 
 int MinimumTotalWaitingTime(vector<int> service_times) {
-  // TODO - you fill in here.
-  return 0;
+    sort(service_times.begin(), service_times.end());
+    int ans = 0;
+    for (int i = 0; i < service_times.size(); i++) {
+        ans += service_times[i] * (service_times.size() - 1 - i);
+    }
+    return ans;
 }
 
-int main(int argc, char* argv[]) {
-  std::vector<std::string> args{argv + 1, argv + argc};
-  std::vector<std::string> param_names{"service_times"};
-  return GenericTestMain(args, "minimum_waiting_time.cc",
-                         "minimum_waiting_time.tsv", &MinimumTotalWaitingTime,
-                         DefaultComparator{}, param_names);
+int main(int argc, char *argv[]) {
+    std::vector<std::string> args{argv + 1, argv + argc};
+    std::vector<std::string> param_names{"service_times"};
+    return GenericTestMain(args, "minimum_waiting_time.cc",
+                           "minimum_waiting_time.tsv", &MinimumTotalWaitingTime,
+                           DefaultComparator{}, param_names);
 }
