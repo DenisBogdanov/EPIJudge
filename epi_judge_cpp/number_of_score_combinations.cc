3869ba2529c1d5f922f4b3adfbf1ade2caeaef51
Denis Bogdanov <bogdanov.math@gmail.com>
2026-02-27 07:19:56 +0100

    C++.

--- epi_judge_cpp/number_of_score_combinations.cc
+++ epi_judge_cpp/number_of_score_combinations.cc
@@ -3,17 +3,23 @@
 #include "test_framework/generic_test.h"
 using std::vector;
 
-int NumCombinationsForFinalScore(int final_score,
-                                 const vector<int>& individual_play_scores) {
-  // TODO - you fill in here.
-  return 0;
+int NumCombinationsForFinalScore(int final_score, const vector<int> &individual_play_scores) {
+    vector<int> dp(final_score + 1, 0);
+    dp[0] = 1;
+    for (int score: individual_play_scores) {
+        for (int i = score; i <= final_score; i++) {
+            dp[i] += dp[i - score];
+        }
+    }
+
+    return dp[final_score];
 }
 
-int main(int argc, char* argv[]) {
-  std::vector<std::string> args{argv + 1, argv + argc};
-  std::vector<std::string> param_names{"final_score", "individual_play_scores"};
-  return GenericTestMain(args, "number_of_score_combinations.cc",
-                         "number_of_score_combinations.tsv",
-                         &NumCombinationsForFinalScore, DefaultComparator{},
-                         param_names);
+int main(int argc, char *argv[]) {
+    std::vector<std::string> args{argv + 1, argv + argc};
+    std::vector<std::string> param_names{"final_score", "individual_play_scores"};
+    return GenericTestMain(args, "number_of_score_combinations.cc",
+                           "number_of_score_combinations.tsv",
+                           &NumCombinationsForFinalScore, DefaultComparator{},
+                           param_names);
 }
