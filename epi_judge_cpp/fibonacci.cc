83239c09810a55f5ff28895c0d0460d459970000
Denis Bogdanov <bogdanov.math@gmail.com>
2026-02-27 07:13:40 +0100

    C++.

--- epi_judge_cpp/fibonacci.cc
+++ epi_judge_cpp/fibonacci.cc
@@ -1,12 +1,20 @@
 #include "test_framework/generic_test.h"
+
 int Fibonacci(int n) {
-  // TODO - you fill in here.
-  return -1;
+    if (n < 2) return n;
+    int a = 0;
+    int b = 1;
+    for (int i = 1; i < n; i++) {
+        int temp = a;
+        a = b;
+        b += temp;
+    }
+    return b;
 }
 
-int main(int argc, char* argv[]) {
-  std::vector<std::string> args{argv + 1, argv + argc};
-  std::vector<std::string> param_names{"n"};
-  return GenericTestMain(args, "fibonacci.cc", "fibonacci.tsv", &Fibonacci,
-                         DefaultComparator{}, param_names);
+int main(int argc, char *argv[]) {
+    std::vector<std::string> args{argv + 1, argv + argc};
+    std::vector<std::string> param_names{"n"};
+    return GenericTestMain(args, "fibonacci.cc", "fibonacci.tsv", &Fibonacci,
+                           DefaultComparator{}, param_names);
 }
