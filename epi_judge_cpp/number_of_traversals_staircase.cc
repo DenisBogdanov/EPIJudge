d5045cc02059bf2b5f1d2e14d91deaca88ece92b
Denis Bogdanov <bogdanov.math@gmail.com>
2026-02-27 21:57:21 +0100

    C++.

--- epi_judge_cpp/number_of_traversals_staircase.cc
+++ epi_judge_cpp/number_of_traversals_staircase.cc
@@ -1,14 +1,23 @@
 #include "test_framework/generic_test.h"
 
+using namespace std;
+
 int NumberOfWaysToTop(int top, int maximum_step) {
-  // TODO - you fill in here.
-  return 0;
+    vector<int> dp(top + 1, 0);
+    dp[0] = 1;
+    for (int curr = 1; curr <= top; curr++) {
+        for (int step = 1; step <= maximum_step; step++) {
+            if (curr < step) break;
+            dp[curr] += dp[curr - step];
+        }
+    }
+    return dp[top];
 }
 
-int main(int argc, char* argv[]) {
-  std::vector<std::string> args{argv + 1, argv + argc};
-  std::vector<std::string> param_names{"top", "maximum_step"};
-  return GenericTestMain(args, "number_of_traversals_staircase.cc",
-                         "number_of_traversals_staircase.tsv",
-                         &NumberOfWaysToTop, DefaultComparator{}, param_names);
+int main(int argc, char *argv[]) {
+    std::vector<std::string> args{argv + 1, argv + argc};
+    std::vector<std::string> param_names{"top", "maximum_step"};
+    return GenericTestMain(args, "number_of_traversals_staircase.cc",
+                           "number_of_traversals_staircase.tsv",
+                           &NumberOfWaysToTop, DefaultComparator{}, param_names);
 }
