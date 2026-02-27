2ab0f1e89763e9a1de047e709c5db94d6fb514d2
Denis Bogdanov <bogdanov.math@gmail.com>
2026-02-27 23:57:49 +0100

    C++.

--- epi_judge_cpp/search_entry_equal_to_index.cc
+++ epi_judge_cpp/search_entry_equal_to_index.cc
@@ -5,31 +5,39 @@
 #include "test_framework/timed_executor.h"
 using std::vector;
 
-int SearchEntryEqualToItsIndex(const vector<int>& A) {
-  // TODO - you fill in here.
-  return 0;
-}
-void SearchEntryEqualToItsIndexWrapper(TimedExecutor& executor,
-                                       const vector<int>& A) {
-  int result = executor.Run([&] { return SearchEntryEqualToItsIndex(A); });
-
-  if (result != -1) {
-    if (A[result] != result) {
-      throw TestFailure("Entry does not equal to its index");
+int SearchEntryEqualToItsIndex(const vector<int> &nums) {
+    int left = -1;
+    int right = nums.size();
+    while (left + 1 < right) {
+        int candidate = (left + right) / 2;
+        if (nums[candidate] == candidate) return candidate;
+        if (nums[candidate] < candidate) left = candidate;
+        else right = candidate;
     }
-  } else {
-    for (int i = 0; i < A.size(); ++i) {
-      if (A[i] == i) {
-        throw TestFailure("There are entries which equal to its index");
-      }
-    }
-  }
+    return -1;
 }
 
-int main(int argc, char* argv[]) {
-  std::vector<std::string> args{argv + 1, argv + argc};
-  std::vector<std::string> param_names{"executor", "A"};
-  return GenericTestMain(
-      args, "search_entry_equal_to_index.cc", "search_entry_equal_to_index.tsv",
-      &SearchEntryEqualToItsIndexWrapper, DefaultComparator{}, param_names);
+void SearchEntryEqualToItsIndexWrapper(TimedExecutor &executor,
+                                       const vector<int> &A) {
+    int result = executor.Run([&] { return SearchEntryEqualToItsIndex(A); });
+
+    if (result != -1) {
+        if (A[result] != result) {
+            throw TestFailure("Entry does not equal to its index");
+        }
+    } else {
+        for (int i = 0; i < A.size(); ++i) {
+            if (A[i] == i) {
+                throw TestFailure("There are entries which equal to its index");
+            }
+        }
+    }
+}
+
+int main(int argc, char *argv[]) {
+    std::vector<std::string> args{argv + 1, argv + argc};
+    std::vector<std::string> param_names{"executor", "A"};
+    return GenericTestMain(
+        args, "search_entry_equal_to_index.cc", "search_entry_equal_to_index.tsv",
+        &SearchEntryEqualToItsIndexWrapper, DefaultComparator{}, param_names);
 }
