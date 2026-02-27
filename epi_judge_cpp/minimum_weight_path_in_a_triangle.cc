6dab83088d88f90cf4b1d3ded0dcde092c8f05c5
Denis Bogdanov <bogdanov.math@gmail.com>
2026-02-27 16:49:08 +0100

    C++.

--- epi_judge_cpp/minimum_weight_path_in_a_triangle.cc
+++ epi_judge_cpp/minimum_weight_path_in_a_triangle.cc
@@ -1,17 +1,29 @@
 #include <vector>
 
 #include "test_framework/generic_test.h"
-using std::vector;
+using namespace std;
 
-int MinimumPathWeight(const vector<vector<int>>& triangle) {
-  // TODO - you fill in here.
-  return 0;
+const int INF = 1e9;
+
+int MinimumPathWeight(const vector<vector<int> > &triangle) {
+    vector dp(triangle.size() + 1, 0);
+    vector<int> temp(triangle.size() + 1);
+    for (const auto &level: triangle) {
+        fill(temp.begin(), temp.end(), INF);
+        for (int i = 0; i < level.size(); i++) {
+            temp[i] = min(temp[i], dp[i] + level[i]);
+            temp[i + 1] = min(temp[i + 1], dp[i] + level[i]);
+        }
+        swap(dp, temp);
+    }
+
+    return *std::min_element(dp.begin(), dp.end());
 }
 
-int main(int argc, char* argv[]) {
-  std::vector<std::string> args{argv + 1, argv + argc};
-  std::vector<std::string> param_names{"triangle"};
-  return GenericTestMain(args, "minimum_weight_path_in_a_triangle.cc",
-                         "minimum_weight_path_in_a_triangle.tsv",
-                         &MinimumPathWeight, DefaultComparator{}, param_names);
+int main(int argc, char *argv[]) {
+    std::vector<std::string> args{argv + 1, argv + argc};
+    std::vector<std::string> param_names{"triangle"};
+    return GenericTestMain(args, "minimum_weight_path_in_a_triangle.cc",
+                           "minimum_weight_path_in_a_triangle.tsv",
+                           &MinimumPathWeight, DefaultComparator{}, param_names);
 }
