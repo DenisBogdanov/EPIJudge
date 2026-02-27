2643ce131c01e302b6ddb6e69e2f1e83ffbed037
Denis Bogdanov <bogdanov.math@gmail.com>
2026-02-27 23:51:36 +0100

    C++.

--- epi_judge_cpp/uniform_random_number.cc
+++ epi_judge_cpp/uniform_random_number.cc
@@ -10,43 +10,64 @@ using std::default_random_engine;
 using std::random_device;
 using std::uniform_int_distribution;
 using std::vector;
+
 int ZeroOneRandom() {
-  default_random_engine gen((random_device())());
-  uniform_int_distribution<int> dis(0, 1);
-  return dis(gen);
+    default_random_engine gen((random_device())());
+    uniform_int_distribution<int> dis(0, 1);
+    return dis(gen);
 }
 
 int UniformRandom(int lower_bound, int upper_bound) {
-  // TODO - you fill in here.
-  return 0;
+    if (lower_bound > upper_bound) return -1;
+    if (lower_bound == upper_bound) return lower_bound;
+    int diff = upper_bound - lower_bound;
+    while (true) {
+        int ans = 0;
+        int mult = 1;
+        while (mult <= diff) {
+            ans *= 2;
+            ans += ZeroOneRandom();
+            mult *= 2;
+        }
+        if (ans <= diff) {
+            return ans + lower_bound;
+        }
+    }
 }
-bool UniformRandomRunner(TimedExecutor& executor, int lower_bound,
+
+bool UniformRandomRunner(TimedExecutor &executor, int lower_bound,
                          int upper_bound) {
-  using namespace test_framework;
-  vector<int> result;
-  executor.Run([&] {
-    std::generate_n(back_inserter(result), 100000,
-                    std::bind(UniformRandom, lower_bound, upper_bound));
-  });
+    using namespace test_framework;
+    vector<int> result;
+    executor.Run([&] {
+        std::generate_n(back_inserter(result), 100000,
+                        std::bind(UniformRandom, lower_bound, upper_bound));
+    });
 
-  vector<int> sequence;
-  std::transform(begin(result), end(result), back_inserter(sequence),
-                 [lower_bound](int result) { return result - lower_bound; });
-  return CheckSequenceIsUniformlyRandom(result, upper_bound - lower_bound + 1,
-                                        0.01);
+    vector<int> sequence;
+    std::transform(begin(result), end(result), back_inserter(sequence),
+                   [lower_bound](int result) { return result - lower_bound; });
+    return CheckSequenceIsUniformlyRandom(result, upper_bound - lower_bound + 1,
+                                          0.01);
 }
 
-void UniformRandomWrapper(TimedExecutor& executor, int lower_bound,
+void UniformRandomWrapper(TimedExecutor &executor, int lower_bound,
                           int upper_bound) {
-  RunFuncWithRetries(
-      bind(UniformRandomRunner, std::ref(executor), lower_bound, upper_bound));
+    RunFuncWithRetries(
+        bind(UniformRandomRunner, std::ref(executor), lower_bound, upper_bound));
 }
 
-int main(int argc, char* argv[]) {
-  std::vector<std::string> args{argv + 1, argv + argc};
-  std::vector<std::string> param_names{"executor", "lower_bound",
-                                       "upper_bound"};
-  return GenericTestMain(args, "uniform_random_number.cc",
-                         "uniform_random_number.tsv", &UniformRandomWrapper,
-                         DefaultComparator{}, param_names);
+int main(int argc, char *argv[]) {
+    // for (int i = 0; i < 100; i++) {
+    //     std::cout << UniformRandom(0, 10) << '\n';
+    // }
+
+    std::vector<std::string> args{argv + 1, argv + argc};
+    std::vector<std::string> param_names{
+        "executor", "lower_bound",
+        "upper_bound"
+    };
+    return GenericTestMain(args, "uniform_random_number.cc",
+                           "uniform_random_number.tsv", &UniformRandomWrapper,
+                           DefaultComparator{}, param_names);
 }
