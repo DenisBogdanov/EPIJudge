397f2118803f4b641a3b1374f530a91e520cf5ca
Denis Bogdanov <bogdanov.math@gmail.com>
2026-02-27 22:01:16 +0100

    C++.

--- epi_judge_cpp/making_change.cc
+++ epi_judge_cpp/making_change.cc
@@ -1,13 +1,19 @@
 #include "test_framework/generic_test.h"
 
+const int COINS[] = {100, 50, 25, 10, 5, 1};
+
 int ChangeMaking(int cents) {
-  // TODO - you fill in here.
-  return 0;
+    int result = 0;
+    for (int coin : COINS) {
+        result += cents / coin;
+        cents %= coin;
+    }
+    return result;
 }
 
-int main(int argc, char* argv[]) {
-  std::vector<std::string> args{argv + 1, argv + argc};
-  std::vector<std::string> param_names{"cents"};
-  return GenericTestMain(args, "making_change.cc", "making_change.tsv",
-                         &ChangeMaking, DefaultComparator{}, param_names);
+int main(int argc, char *argv[]) {
+    std::vector<std::string> args{argv + 1, argv + argc};
+    std::vector<std::string> param_names{"cents"};
+    return GenericTestMain(args, "making_change.cc", "making_change.tsv",
+                           &ChangeMaking, DefaultComparator{}, param_names);
 }
