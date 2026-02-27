676c8643eb97563e6fef12ea8a72aba2e111a84e
Denis Bogdanov <bogdanov.math@gmail.com>
2026-02-27 10:39:47 +0100

    C++.

--- epi_judge_cpp/knapsack.cc
+++ epi_judge_cpp/knapsack.cc
@@ -2,25 +2,32 @@
 
 #include "test_framework/generic_test.h"
 #include "test_framework/serialization_traits.h"
-using std::vector;
+using namespace std;
 
 struct Item {
-  int weight, value;
+    int weight, value;
 };
 
-int OptimumSubjectToCapacity(const vector<Item>& items, int capacity) {
-  // TODO - you fill in here.
-  return 0;
+int OptimumSubjectToCapacity(const vector<Item> &items, int capacity) {
+    vector<int> dp(capacity + 1, 0);
+    for (auto &item : items) {
+        for (int i = capacity; i >= item.weight; i--) {
+            dp[i] = max(dp[i], dp[i - item.weight] + item.value);
+        }
+    }
+    return *max_element(dp.begin(), dp.end());
 }
-namespace test_framework {
-template <>
-struct SerializationTrait<Item> : UserSerTrait<Item, int, int> {};
-}  // namespace test_framework
 
-int main(int argc, char* argv[]) {
-  std::vector<std::string> args{argv + 1, argv + argc};
-  std::vector<std::string> param_names{"items", "capacity"};
-  return GenericTestMain(args, "knapsack.cc", "knapsack.tsv",
-                         &OptimumSubjectToCapacity, DefaultComparator{},
-                         param_names);
+namespace test_framework {
+    template<>
+    struct SerializationTrait<Item> : UserSerTrait<Item, int, int> {
+    };
+} // namespace test_framework
+
+int main(int argc, char *argv[]) {
+    std::vector<std::string> args{argv + 1, argv + argc};
+    std::vector<std::string> param_names{"items", "capacity"};
+    return GenericTestMain(args, "knapsack.cc", "knapsack.tsv",
+                           &OptimumSubjectToCapacity, DefaultComparator{},
+                           param_names);
 }
--- problem_mapping.js
+++ problem_mapping.js
@@ -2218,7 +2218,7 @@ problem_mapping = {
         },
         "16.06 The knapsack problem": {
             "C++: knapsack.cc": {
-                "passed": 0,
+                "passed": 100,
                 "total": 100
             },
             "Java: Knapsack.java": {
