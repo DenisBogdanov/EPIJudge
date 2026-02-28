fbcee2745a6c8332c1003af36e8768abfdba05f9
Denis Bogdanov <bogdanov.math@gmail.com>
2026-02-28 21:18:18 +0100

    C++.

--- epi_judge_cpp/n_queens.cc
+++ epi_judge_cpp/n_queens.cc
@@ -3,16 +3,41 @@
 #include <vector>
 
 #include "test_framework/generic_test.h"
-using std::vector;
+using namespace std;
 
-vector<vector<int>> NQueens(int n) {
-  // TODO - you fill in here.
-  return {};
+bool ok(int col, const vector<int> &curr_arrangement) {
+    for (int row = 0; row < curr_arrangement.size(); row++) {
+        if (curr_arrangement[row] == col) return false;
+        if (abs(col - curr_arrangement[row]) == curr_arrangement.size() - row) return false;
+    }
+    return true;
 }
 
-int main(int argc, char* argv[]) {
-  std::vector<std::string> args{argv + 1, argv + argc};
-  std::vector<std::string> param_names{"n"};
-  return GenericTestMain(args, "n_queens.cc", "n_queens.tsv", &NQueens,
-                         UnorderedComparator{}, param_names);
+void recur(int r, int n, vector<int> &curr_arrangement, vector<vector<int>> &ans) {
+    if (r == n) {
+        const auto successful_arrangement = curr_arrangement;
+        ans.push_back(successful_arrangement);
+        return;
+    }
+    for (int col = 0; col < n; col++) {
+        if (ok(col, curr_arrangement)) {
+            curr_arrangement.push_back(col);
+            recur(r + 1, n, curr_arrangement, ans);
+            curr_arrangement.pop_back();
+        }
+    }
+}
+
+vector<vector<int> > NQueens(int n) {
+    vector<vector<int>> ans;
+    vector<int> curr_arrangement;
+    recur(0, n, curr_arrangement, ans);
+    return ans;
+}
+
+int main(int argc, char *argv[]) {
+    std::vector<std::string> args{argv + 1, argv + argc};
+    std::vector<std::string> param_names{"n"};
+    return GenericTestMain(args, "n_queens.cc", "n_queens.tsv", &NQueens,
+                           UnorderedComparator{}, param_names);
 }
