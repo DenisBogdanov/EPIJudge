5411b33b1e7b5728d2cdbf68bd8d926f2776d108
Denis Bogdanov <bogdanov.math@gmail.com>
2026-02-28 07:24:56 +0100

    C++.

--- epi_judge_cpp/three_sum.cc
+++ epi_judge_cpp/three_sum.cc
@@ -1,16 +1,32 @@
 #include <vector>
 
 #include "test_framework/generic_test.h"
-using std::vector;
 
-bool HasThreeSum(vector<int> A, int t) {
-  // TODO - you fill in here.
-  return true;
+using namespace std;
+
+bool HasThreeSum(vector<int> nums, int t) {
+    sort(nums.begin(), nums.end());
+    for (int start = 0; start < nums.size(); start++) {
+        int left = start;
+        int right = nums.size() - 1;
+        while (left <= right) {
+            int sum = nums[start] + nums[left] + nums[right];
+            if (sum > t) {
+                right--;
+            } else if (sum < t) {
+                left++;
+            } else {
+                return  true;
+            }
+        }
+    }
+
+    return false;
 }
 
-int main(int argc, char* argv[]) {
-  std::vector<std::string> args{argv + 1, argv + argc};
-  std::vector<std::string> param_names{"A", "t"};
-  return GenericTestMain(args, "three_sum.cc", "three_sum.tsv", &HasThreeSum,
-                         DefaultComparator{}, param_names);
+int main(int argc, char *argv[]) {
+    std::vector<std::string> args{argv + 1, argv + argc};
+    std::vector<std::string> param_names{"A", "t"};
+    return GenericTestMain(args, "three_sum.cc", "three_sum.tsv", &HasThreeSum,
+                           DefaultComparator{}, param_names);
 }
