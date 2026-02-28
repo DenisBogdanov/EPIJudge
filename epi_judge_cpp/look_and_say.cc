f6337e05ace474d3575a93b080de0bd323e4b76e
Denis Bogdanov <bogdanov.math@gmail.com>
2026-02-28 21:46:12 +0100

    C++.

--- epi_judge_cpp/look_and_say.cc
+++ epi_judge_cpp/look_and_say.cc
@@ -1,16 +1,32 @@
 #include <string>
 
 #include "test_framework/generic_test.h"
-using std::string;
+using namespace std;
 
 string LookAndSay(int n) {
-  // TODO - you fill in here.
-  return "";
+    if (n == 0) return "";
+    string ans = "1";
+    string temp;
+    for (int i = 1; i < n; i++) {
+        int count = 1;
+        for (int j = 1; j < ans.size(); j++) {
+            if (ans[j] == ans[j - 1]) {
+                count++;
+            } else {
+                temp += to_string(count) + ans[j - 1];
+                count = 1;
+            }
+        }
+        temp += to_string(count) + ans.back();
+        swap(ans, temp);
+        temp.clear();
+    }
+    return ans;
 }
 
-int main(int argc, char* argv[]) {
-  std::vector<std::string> args{argv + 1, argv + argc};
-  std::vector<std::string> param_names{"n"};
-  return GenericTestMain(args, "look_and_say.cc", "look_and_say.tsv",
-                         &LookAndSay, DefaultComparator{}, param_names);
+int main(int argc, char *argv[]) {
+    std::vector<std::string> args{argv + 1, argv + argc};
+    std::vector<std::string> param_names{"n"};
+    return GenericTestMain(args, "look_and_say.cc", "look_and_say.tsv",
+                           &LookAndSay, DefaultComparator{}, param_names);
 }
