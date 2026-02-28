46146f10a35a73612cb42ce4035f1d4766f7cb46
Denis Bogdanov <bogdanov.math@gmail.com>
2026-02-28 20:25:27 +0100

    C++.

--- epi_judge_cpp/search_row_col_sorted_matrix.cc
+++ epi_judge_cpp/search_row_col_sorted_matrix.cc
@@ -3,15 +3,29 @@
 #include "test_framework/generic_test.h"
 using std::vector;
 
-bool MatrixSearch(const vector<vector<int>>& A, int x) {
-  // TODO - you fill in here.
-  return true;
+bool MatrixSearch(const vector<vector<int> > &grid, int x) {
+    if (grid.empty()) {
+        return false;
+    }
+    int r = grid.size() - 1;
+    int c = 0;
+    while (r >= 0 && c < grid[0].size()) {
+        if (grid[r][c] == x) {
+            return true;
+        }
+        if (grid[r][c] < x) {
+            c++;
+        } else {
+            r--;
+        }
+    }
+    return false;
 }
 
-int main(int argc, char* argv[]) {
-  std::vector<std::string> args{argv + 1, argv + argc};
-  std::vector<std::string> param_names{"A", "x"};
-  return GenericTestMain(args, "search_row_col_sorted_matrix.cc",
-                         "search_row_col_sorted_matrix.tsv", &MatrixSearch,
-                         DefaultComparator{}, param_names);
+int main(int argc, char *argv[]) {
+    std::vector<std::string> args{argv + 1, argv + argc};
+    std::vector<std::string> param_names{"A", "x"};
+    return GenericTestMain(args, "search_row_col_sorted_matrix.cc",
+                           "search_row_col_sorted_matrix.tsv", &MatrixSearch,
+                           DefaultComparator{}, param_names);
 }
