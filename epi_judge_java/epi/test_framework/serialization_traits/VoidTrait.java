4642071e0025ec39140ccef72a4849d4faa09b1e
Denis Bogdanov <bogdanov.math@gmail.com>
2026-04-26 16:06:53 +0200

    Format the code.

--- epi_judge_java/epi/test_framework/serialization_traits/VoidTrait.java
+++ epi_judge_java/epi/test_framework/serialization_traits/VoidTrait.java
@@ -1,4 +1,3 @@
-
 package epi.test_framework.serialization_traits;
 
 import epi.test_framework.minimal_json.JsonValue;
@@ -7,28 +6,28 @@ import java.util.Collections;
 import java.util.List;
 
 public class VoidTrait extends SerializationTrait {
-  @Override
-  public String name() {
-    return "void";
-  }
+    @Override
+    public String name() {
+        return "void";
+    }
 
-  @Override
-  public Object parse(JsonValue jsonObject) {
-    throw new RuntimeException("Can't parse void");
-  }
+    @Override
+    public Object parse(JsonValue jsonObject) {
+        throw new RuntimeException("Can't parse void");
+    }
 
-  @Override
-  public List<String> getMetricNames(String argName) {
-    return Collections.emptyList();
-  }
+    @Override
+    public List<String> getMetricNames(String argName) {
+        return Collections.emptyList();
+    }
 
-  @Override
-  public List<Integer> getMetrics(Object x) {
-    return Collections.emptyList();
-  }
+    @Override
+    public List<Integer> getMetrics(Object x) {
+        return Collections.emptyList();
+    }
 
-  @Override
-  public boolean isVoid() {
-    return true;
-  }
+    @Override
+    public boolean isVoid() {
+        return true;
+    }
 }
