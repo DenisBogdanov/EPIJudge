c2e0db62b1cd7d4397cb320329f064a3e681f103
Denis Bogdanov <bogdanov.math@gmail.com>
2026-04-28 08:34:03 +0200

    Java.

--- epi_judge_java/epi/LowestCommonAncestor.java
+++ epi_judge_java/epi/LowestCommonAncestor.java
@@ -6,8 +6,26 @@ public class LowestCommonAncestor {
     public static BinaryTreeNode<Integer> lca(BinaryTreeNode<Integer> tree,
                                               BinaryTreeNode<Integer> node0,
                                               BinaryTreeNode<Integer> node1) {
-        // TODO - you fill in here.
-        return null;
+        if (tree == null) return tree;
+        if (node0 == node1) return node0;
+        while (true) {
+            if (tree.equals(node0) || tree.equals(node1)) return tree;
+            int leftCount = containedCount(tree.left, node0, node1);
+            if (leftCount == 0) {
+                tree = tree.right;
+            } else if (leftCount == 1) {
+                return tree;
+            } else if (leftCount == 2) {
+                tree = tree.left;
+            }
+        }
+    }
+
+    private static int containedCount(BinaryTreeNode<Integer> root, BinaryTreeNode<Integer> n1, BinaryTreeNode<Integer> n2) {
+        if (root == null) return 0;
+        int ans = 0;
+        if (root.data.equals(n1.data) || root.data.equals(n2.data)) ans++;
+        return ans + containedCount(root.left, n1, n2) + containedCount(root.right, n1, n2);
     }
 
     @EpiTest(testDataFile = "lowest_common_ancestor.tsv")
