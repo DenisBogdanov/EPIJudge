dc5d4c29ca8284e84d2e6eececba9275993e21e0
Denis Bogdanov <bogdanov.math@gmail.com>
2026-04-28 07:42:58 +0200

    Java.

--- epi_judge_java/epi/SunsetView.java
+++ epi_judge_java/epi/SunsetView.java
@@ -3,19 +3,33 @@ package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
 
 public class SunsetView {
-    public static List<Integer>
-    examineBuildingsWithSunset(Iterator<Integer> sequence) {
-        // TODO - you fill in here.
-        return null;
+
+    public static List<Integer> examineBuildingsWithSunset(Iterator<Integer> sequence) {
+        List<Integer> ans = new ArrayList<>();
+        Deque<Integer> heightsStack = new ArrayDeque<>();
+        Deque<Integer> indicesStack = new ArrayDeque<>();
+        int i = 0;
+        while (sequence.hasNext()) {
+            var next = sequence.next();
+            while (!heightsStack.isEmpty() && heightsStack.peek() <= next) {
+                heightsStack.pop();
+                indicesStack.pop();
+            }
+            heightsStack.push(next);
+            indicesStack.push(i);
+            i++;
+        }
+        while (!indicesStack.isEmpty()) {
+            ans.add(indicesStack.pop());
+        }
+        return ans;
     }
 
     @EpiTest(testDataFile = "sunset_view.tsv")
-    public static List<Integer>
-    examineBuildingsWithSunsetWrapper(List<Integer> sequence) {
+    public static List<Integer> examineBuildingsWithSunsetWrapper(List<Integer> sequence) {
         return examineBuildingsWithSunset(sequence.iterator());
     }
 
