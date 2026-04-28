ba299d747074d287889653ed0ed7c0cfdc5e404a
Denis Bogdanov <bogdanov.math@gmail.com>
2026-04-28 19:57:52 +0200

    Java.

--- epi_judge_java/epi/Parity.java
+++ epi_judge_java/epi/Parity.java
@@ -4,10 +4,15 @@ import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 
 public class Parity {
+
     @EpiTest(testDataFile = "parity.tsv")
     public static short parity(long x) {
-        // TODO - you fill in here.
-        return 0;
+        short ans = 0;
+        while (x != 0) {
+            ans ^= 1;
+            x &= x - 1;
+        }
+        return ans;
     }
 
     public static void main(String[] args) {
