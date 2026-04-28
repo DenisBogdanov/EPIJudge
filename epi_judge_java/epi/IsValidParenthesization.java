a11140bbd3648899fc72c33a4bd02520eb1e6b8c
Denis Bogdanov <bogdanov.math@gmail.com>
2026-04-28 06:50:27 +0200

    Java.

--- epi_judge_java/epi/IsValidParenthesization.java
+++ epi_judge_java/epi/IsValidParenthesization.java
@@ -3,12 +3,38 @@ package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 
-public class IsValidParenthesization {
-    @EpiTest(testDataFile = "is_valid_parenthesization.tsv")
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Map;
+import java.util.Stack;
 
+public class IsValidParenthesization {
+    private static final Map<Character, Character> CLOSED_TO_OPENED_MAPPING = Map.of(
+            ')', '(',
+            ']', '[',
+            '}', '{'
+    );
+
+    @EpiTest(testDataFile = "is_valid_parenthesization.tsv")
     public static boolean isWellFormed(String s) {
-        // TODO - you fill in here.
-        return true;
+        Deque<Character> stack = new ArrayDeque<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            switch (c) {
+                case '(':
+                case '[':
+                case '{':
+                    stack.push(c);
+                    break;
+                default: {
+                    if (stack.isEmpty() || CLOSED_TO_OPENED_MAPPING.get(c) != stack.pop()) {
+                        return false;
+                    }
+                    break;
+                }
+            }
+        }
+        return stack.isEmpty();
     }
 
     public static void main(String[] args) {
