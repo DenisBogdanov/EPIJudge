ba505783c78a1b4412ea790a657ffb476d906abe
Denis Bogdanov <bogdanov.math@gmail.com>
2026-04-28 06:42:51 +0200

    Java.

--- epi_judge_java/epi/EvaluateRpn.java
+++ epi_judge_java/epi/EvaluateRpn.java
@@ -3,12 +3,49 @@ package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 
-public class EvaluateRpn {
-    @EpiTest(testDataFile = "evaluate_rpn.tsv")
+import java.util.ArrayDeque;
+import java.util.Deque;
 
+public class EvaluateRpn {
+
+    @EpiTest(testDataFile = "evaluate_rpn.tsv")
     public static int eval(String expression) {
-        // TODO - you fill in here.
-        return 0;
+        var parts = expression.split(",");
+        Deque<Integer> stack = new ArrayDeque<>();
+        for (String part : parts) {
+            switch (part) {
+                case "+": {
+                    int a = stack.pop();
+                    int b = stack.pop();
+                    stack.push(a + b);
+                    break;
+                }
+                case "*": {
+                    int a = stack.pop();
+                    int b = stack.pop();
+                    stack.push(a * b);
+                    break;
+                }
+                case "-": {
+                    int a = stack.pop();
+                    int b = stack.pop();
+                    stack.push(b - a);
+                    break;
+                }
+                case "/": {
+                    int a = stack.pop();
+                    int b = stack.pop();
+                    stack.push(b / a);
+                    break;
+                }
+                default: {
+                    stack.push(Integer.parseInt(part));
+                    break;
+                }
+            }
+        }
+
+        return stack.pop();
     }
 
     public static void main(String[] args) {
