5a862e1b6b0d05dada4554295b525165696971a5
Denis Bogdanov <bogdanov.math@gmail.com>
2026-04-28 20:12:46 +0200

    Java.

--- epi_judge_java/epi/IsStringPalindromic.java
+++ epi_judge_java/epi/IsStringPalindromic.java
@@ -4,10 +4,19 @@ import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 
 public class IsStringPalindromic {
-    @EpiTest(testDataFile = "is_string_palindromic.tsv")
 
+    @EpiTest(testDataFile = "is_string_palindromic.tsv")
     public static boolean isPalindromic(String s) {
-        // TODO - you fill in here.
+        int left = 0;
+        int right = s.length() - 1;
+        while (left < right) {
+            while (left < right && !Character.isAlphabetic(s.charAt(left))) left++;
+            while (left < right && !Character.isAlphabetic(s.charAt(right))) right--;
+            if (left >= right) break;
+            if (Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))) return false;
+            left++;
+            right--;
+        }
         return true;
     }
 
