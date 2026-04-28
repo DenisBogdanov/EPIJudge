5bd73e9d17c43f7ec77860f66d3efb1e00fad1a1
Denis Bogdanov <bogdanov.math@gmail.com>
2026-04-28 19:42:57 +0200

    Java.

--- epi_judge_java/epi/MatrixConnectedRegions.java
+++ epi_judge_java/epi/MatrixConnectedRegions.java
@@ -8,16 +8,32 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MatrixConnectedRegions {
+    private static final int[][] DIRS = {
+            {0, 1},
+            {1, 0},
+            {0, -1},
+            {-1, 0},
+    };
+
     public static void flipColor(int x, int y, List<List<Boolean>> image) {
-        // TODO - you fill in here.
-        return;
+        int n = image.size();
+        int m = image.get(0).size();
+        boolean currColor = image.get(x).get(y);
+        dfs(image, n, m, currColor, x, y);
+    }
+
+    private static void dfs(List<List<Boolean>> image, int n, int m, boolean currColor, int x, int y) {
+        if (x < 0 || x >= n || y < 0 || y >= n) return;
+        if (image.get(x).get(y) != currColor) return;
+        image.get(x).set(y, !currColor);
+        for (var dir : DIRS) {
+            dfs(image, n, m, currColor, x + dir[0], y + dir[1]);
+        }
     }
 
     @EpiTest(testDataFile = "painting.tsv")
-    public static List<List<Integer>> flipColorWrapper(TimedExecutor executor,
-                                                       int x, int y,
-                                                       List<List<Integer>> image)
-            throws Exception {
+    public static List<List<Integer>> flipColorWrapper(TimedExecutor executor, int x, int y,
+                                                       List<List<Integer>> image) throws Exception {
         List<List<Boolean>> B = new ArrayList<>();
         for (int i = 0; i < image.size(); i++) {
             B.add(new ArrayList<>());
