8edc040c3659a0b10f4d8c34ea40e1e05ec18629
Denis Bogdanov <bogdanov.math@gmail.com>
2026-04-28 19:20:02 +0200

    Java.

--- epi_judge_java/epi/NumberOfScoreCombinations.java
+++ epi_judge_java/epi/NumberOfScoreCombinations.java
@@ -3,16 +3,21 @@ package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class NumberOfScoreCombinations {
-    @EpiTest(testDataFile = "number_of_score_combinations.tsv")
 
-    public static int
-    numCombinationsForFinalScore(int finalScore,
-                                 List<Integer> individualPlayScores) {
-        // TODO - you fill in here.
-        return 0;
+    @EpiTest(testDataFile = "number_of_score_combinations.tsv")
+    public static int numCombinationsForFinalScore(int finalScore, List<Integer> individualPlayScores) {
+        int[] dp = new int[finalScore + 1];
+        dp[0] = 1;
+        for (int score : individualPlayScores) {
+            for (int i = 0; i + score <= finalScore; i++) {
+                dp[i + score] += dp[i];
+            }
+        }
+        return dp[finalScore];
     }
 
     public static void main(String[] args) {
