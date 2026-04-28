3bc922d641d53280047c7bf99a8116c39e7eb335
Denis Bogdanov <bogdanov.math@gmail.com>
2026-04-28 20:09:22 +0200

    Java.

--- epi_judge_java/epi/FirstMissingPositiveEntry.java
+++ epi_judge_java/epi/FirstMissingPositiveEntry.java
@@ -6,11 +6,20 @@ import epi.test_framework.GenericTest;
 import java.util.List;
 
 public class FirstMissingPositiveEntry {
-    @EpiTest(testDataFile = "first_missing_positive_entry.tsv")
 
-    public static int findFirstMissingPositive(List<Integer> A) {
-        // TODO - you fill in here.
-        return 0;
+    @EpiTest(testDataFile = "first_missing_positive_entry.tsv")
+    public static int findFirstMissingPositive(List<Integer> nums) {
+        nums.sort(null);
+        int i = 1;
+        for (int j = 0; j < nums.size(); j++) {
+            int num = nums.get(j);
+            if (j > 0 && num == nums.get(j - 1)) continue;
+            if (num > 0) {
+                if (num != i) return i;
+                i++;
+            }
+        }
+        return i;
     }
 
     public static void main(String[] args) {
--- problem_mapping.js
+++ problem_mapping.js
@@ -2590,7 +2590,7 @@ problem_mapping = {
                 "total": 1001
             },
             "Java: FirstMissingPositiveEntry.java": {
-                "passed": 0,
+                "passed": 1001,
                 "total": 1001
             },
             "Python: first_missing_positive_entry.py": {
