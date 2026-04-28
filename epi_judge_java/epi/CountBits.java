f7c8730cad1ab75defdf69c78c942a0f5e794b99
Denis Bogdanov <bogdanov.math@gmail.com>
2026-04-28 19:54:57 +0200

    Java.

--- epi_judge_java/epi/CountBits.java
+++ epi_judge_java/epi/CountBits.java
@@ -4,11 +4,15 @@ import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 
 public class CountBits {
-    @EpiTest(testDataFile = "count_bits.tsv")
 
+    @EpiTest(testDataFile = "count_bits.tsv")
     public static short countBits(int x) {
-        // TODO - you fill in here.
-        return 0;
+        short ans = 0;
+        while (x != 0) {
+            x &= x - 1;
+            ans++;
+        }
+        return ans;
     }
 
     public static void main(String[] args) {
