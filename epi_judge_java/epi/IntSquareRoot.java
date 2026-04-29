04b25318e49b98666cd77c35e91ce65f15da4db6
Denis Bogdanov <bogdanov.math@gmail.com>
2026-04-29 12:25:36 +0200

    Java.

--- epi_judge_java/epi/IntSquareRoot.java
+++ epi_judge_java/epi/IntSquareRoot.java
@@ -4,11 +4,18 @@ import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 
 public class IntSquareRoot {
-    @EpiTest(testDataFile = "int_square_root.tsv")
 
+    @EpiTest(testDataFile = "int_square_root.tsv")
     public static int squareRoot(int k) {
-        // TODO - you fill in here.
-        return 0;
+        if (k < 0) return -1;
+        long left = 0;
+        long right = (long) k + 1;
+        while (left + 1 < right) {
+            long mid = left + (right - left) / 2;
+            if (mid * mid > k) right = mid;
+            else left = mid;
+        }
+        return (int) left;
     }
 
     public static void main(String[] args) {
