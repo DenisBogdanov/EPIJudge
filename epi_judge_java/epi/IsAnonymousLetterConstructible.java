232607f9aecfb31403b218af2aef430038d06639
Denis Bogdanov <bogdanov.math@gmail.com>
2026-04-29 17:45:05 +0200

    Java.

--- epi_judge_java/epi/IsAnonymousLetterConstructible.java
+++ epi_judge_java/epi/IsAnonymousLetterConstructible.java
@@ -4,11 +4,16 @@ import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 
 public class IsAnonymousLetterConstructible {
-    @EpiTest(testDataFile = "is_anonymous_letter_constructible.tsv")
 
-    public static boolean isLetterConstructibleFromMagazine(String letterText,
-                                                            String magazineText) {
-        // TODO - you fill in here.
+    @EpiTest(testDataFile = "is_anonymous_letter_constructible.tsv")
+    public static boolean isLetterConstructibleFromMagazine(String letterText, String magazineText) {
+        int[] counts = new int[256];
+        for (int i = 0; i < magazineText.length(); i++) {
+            counts[magazineText.charAt(i)]++;
+        }
+        for (int i = 0; i < letterText.length(); i++) {
+            if (--counts[letterText.charAt(i)] < 0) return false;
+        }
         return true;
     }
 
