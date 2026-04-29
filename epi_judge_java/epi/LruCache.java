61246d6e9d1f45bfbb5afcd353dcb459206299aa
Denis Bogdanov <bogdanov.math@gmail.com>
2026-04-29 18:44:35 +0200

    Java.

--- epi_judge_java/epi/LruCache.java
+++ epi_judge_java/epi/LruCache.java
@@ -5,25 +5,32 @@ import epi.test_framework.EpiUserType;
 import epi.test_framework.GenericTest;
 import epi.test_framework.TestFailure;
 
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 public class LruCache {
+    private final LinkedHashMap<Integer, Integer> cache;
+
     LruCache(final int capacity) {
+        this.cache = new LinkedHashMap<>(capacity, 1.0f, true) {
+            @Override
+            protected boolean removeEldestEntry(Map.Entry<Integer, Integer> eldest) {
+                return this.size() > capacity;
+            }
+        };
     }
 
     public Integer lookup(Integer key) {
-        // TODO - you fill in here.
-        return 0;
+        return cache.getOrDefault(key, -1);
     }
 
     public void insert(Integer key, Integer value) {
-        // TODO - you fill in here.
-        return;
+        cache.putIfAbsent(key, value);
     }
 
     public Boolean erase(Object key) {
-        // TODO - you fill in here.
-        return true;
+        return cache.remove(key) != null;
     }
 
     @EpiUserType(ctorParams = {String.class, int.class, int.class})
