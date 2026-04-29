2d818a563482a4b222fe47b5e8da2f654bb1f83a
Denis Bogdanov <bogdanov.math@gmail.com>
2026-04-29 10:48:11 +0200

    Java.

--- epi_judge_java/epi/IntAsArrayIncrement.java
+++ epi_judge_java/epi/IntAsArrayIncrement.java
@@ -7,9 +7,16 @@ import java.util.List;
 
 public class IntAsArrayIncrement {
     @EpiTest(testDataFile = "int_as_array_increment.tsv")
-    public static List<Integer> plusOne(List<Integer> A) {
-        // TODO - you fill in here.
-        return null;
+    public static List<Integer> plusOne(List<Integer> num) {
+        int carry = 1;
+        for (int i = num.size() - 1; i >= 0; i--) {
+            int sum = num.get(i) + carry;
+            num.set(i, sum % 10);
+            carry = sum / 10;
+            if (carry == 0) return num;
+        }
+        num.add(0, 1);
+        return num;
     }
 
     public static void main(String[] args) {
