d96d22900eeea12914b22c92cbb4710fbe3f6796
Denis Bogdanov <bogdanov.math@gmail.com>
2026-04-29 20:58:05 +0200

    Java.

--- epi_judge_java/epi/NQueens.java
+++ epi_judge_java/epi/NQueens.java
@@ -5,14 +5,48 @@ import epi.test_framework.EpiTestComparator;
 import epi.test_framework.GenericTest;
 import epi.test_framework.LexicographicalListComparator;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class NQueens {
-    @EpiTest(testDataFile = "n_queens.tsv")
 
+    @EpiTest(testDataFile = "n_queens.tsv")
     public static List<List<Integer>> nQueens(int n) {
-        // TODO - you fill in here.
-        return null;
+        List<List<Integer>> ans = new ArrayList<>();
+        if (n == 1) {
+            ans.add(List.of(0));
+            return ans;
+        }
+        if (n <= 3) return ans;
+        List<Integer> currPosition = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            currPosition.add(-1);
+        }
+        recur(0, n, currPosition, ans);
+        return ans;
+    }
+
+    private static void recur(int row, int n, List<Integer> currPosition, List<List<Integer>> ans) {
+        if (row == n) {
+            ans.add(new ArrayList<>(currPosition));
+            return;
+        }
+        for (int col = 0; col < n; col++) {
+            currPosition.set(row, col);
+            if (isValid(row, n, currPosition)) {
+                recur(row + 1, n, currPosition, ans);
+            }
+        }
+    }
+
+    private static boolean isValid(int row, int n, List<Integer> currPosition) {
+        int latestCol = currPosition.get(row);
+        for (int i = 0; i < row; i++) {
+            if (currPosition.get(i) == latestCol) return false;
+            if (currPosition.get(i) - i == latestCol - row) return false;
+            if (currPosition.get(i) + i == latestCol + row) return false;
+        }
+        return true;
     }
 
     @EpiTestComparator
