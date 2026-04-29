979ab2df123f26e31d6604878468fea2821c4ffe
Denis Bogdanov <bogdanov.math@gmail.com>
2026-04-29 21:25:52 +0200

    Java.

--- epi_judge_java/epi/EnumerateBalancedParentheses.java
+++ epi_judge_java/epi/EnumerateBalancedParentheses.java
@@ -4,15 +4,34 @@ import epi.test_framework.EpiTest;
 import epi.test_framework.EpiTestComparator;
 import epi.test_framework.GenericTest;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class EnumerateBalancedParentheses {
-    @EpiTest(testDataFile = "enumerate_balanced_parentheses.tsv")
 
+    @EpiTest(testDataFile = "enumerate_balanced_parentheses.tsv")
     public static List<String> generateBalancedParentheses(int numPairs) {
-        // TODO - you fill in here.
-        return null;
+        List<String> ans = new ArrayList<>();
+        recur(0, numPairs * 2, 0, new StringBuilder(), ans);
+        return ans;
+    }
+
+    private static void recur(int i, int total, int balance, StringBuilder sb, List<String> ans) {
+        if (i == total) {
+            if (balance == 0) {
+                ans.add(sb.toString());
+            }
+            return;
+        }
+        sb.append('(');
+        recur(i + 1, total, balance + 1, sb, ans);
+        sb.setLength(sb.length() - 1);
+        if (balance > 0) {
+            sb.append(')');
+            recur(i + 1, total, balance - 1, sb, ans);
+            sb.setLength(sb.length() - 1);
+        }
     }
 
     @EpiTestComparator
