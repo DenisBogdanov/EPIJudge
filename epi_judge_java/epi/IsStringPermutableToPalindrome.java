becd97c05d57488d476f56a7c6891d1ddc5d947d
Denis Bogdanov <bogdanov.math@gmail.com>
2026-04-29 17:09:33 +0200

    Java.

--- epi_judge_java/epi/IsStringPermutableToPalindrome.java
+++ epi_judge_java/epi/IsStringPermutableToPalindrome.java
@@ -4,10 +4,18 @@ import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 
 public class IsStringPermutableToPalindrome {
-    @EpiTest(testDataFile = "is_string_permutable_to_palindrome.tsv")
 
+    @EpiTest(testDataFile = "is_string_permutable_to_palindrome.tsv")
     public static boolean canFormPalindrome(String s) {
-        // TODO - you fill in here.
+        char[] chars = new char[256];
+        for (int i = 0; i < s.length(); i++) {
+            chars[s.charAt(i)]++;
+        }
+        int oddCount = 0;
+        for (int count : chars) {
+            if (count % 2 != 0) oddCount++;
+            if (oddCount == 2) return false;
+        }
         return true;
     }
 
