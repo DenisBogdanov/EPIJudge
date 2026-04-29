c47d33a05aba15a6f19ecd9c4ed60c003ed8a84d
Denis Bogdanov <bogdanov.math@gmail.com>
2026-04-29 11:09:22 +0200

    Java.

--- epi_judge_java/epi/SortedListsMerge.java
+++ epi_judge_java/epi/SortedListsMerge.java
@@ -4,12 +4,24 @@ import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 
 public class SortedListsMerge {
+
     @EpiTest(testDataFile = "sorted_lists_merge.tsv")
-    //@include
-    public static ListNode<Integer> mergeTwoSortedLists(ListNode<Integer> L1,
-                                                        ListNode<Integer> L2) {
-        // TODO - you fill in here.
-        return null;
+    public static ListNode<Integer> mergeTwoSortedLists(ListNode<Integer> list1, ListNode<Integer> list2) {
+        ListNode<Integer> dummy = new ListNode<>(-1, null);
+        ListNode<Integer> runner = dummy;
+        while (list1 != null && list2 != null) {
+            if (list1.data < list2.data) {
+                runner.next = list1;
+                list1 = list1.next;
+            } else {
+                runner.next = list2;
+                list2 = list2.next;
+            }
+            runner = runner.next;
+        }
+        if (list1 == null) runner.next = list2;
+        if (list2 == null) runner.next = list1;
+        return dummy.next;
     }
 
     public static void main(String[] args) {
