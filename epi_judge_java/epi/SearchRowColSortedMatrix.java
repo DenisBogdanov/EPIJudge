0f9b0668fa8cbd4440ed4e47ffeb66b8d73ab7ed
Denis Bogdanov <bogdanov.math@gmail.com>
2026-04-29 12:30:50 +0200

    Java.

--- epi_judge_java/epi/SearchRowColSortedMatrix.java
+++ epi_judge_java/epi/SearchRowColSortedMatrix.java
@@ -6,11 +6,17 @@ import epi.test_framework.GenericTest;
 import java.util.List;
 
 public class SearchRowColSortedMatrix {
-    @EpiTest(testDataFile = "search_row_col_sorted_matrix.tsv")
 
-    public static boolean matrixSearch(List<List<Integer>> A, int x) {
-        // TODO - you fill in here.
-        return true;
+    @EpiTest(testDataFile = "search_row_col_sorted_matrix.tsv")
+    public static boolean matrixSearch(List<List<Integer>> grid, int x) {
+        int r = grid.size() - 1;
+        int c = 0;
+        while (r >= 0 && c < grid.get(0).size()) {
+            if (x == grid.get(r).get(c)) return true;
+            else if (x < grid.get(r).get(c)) r--;
+            else c++;
+        }
+        return false;
     }
 
     public static void main(String[] args) {
