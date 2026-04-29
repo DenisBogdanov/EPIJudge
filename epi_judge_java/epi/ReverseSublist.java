bcbd51df4fb193c003aec2bcacfd3a1c50ec0756
Denis Bogdanov <bogdanov.math@gmail.com>
2026-04-29 11:59:23 +0200

    Java.

--- epi_judge_java/epi/ReverseSublist.java
+++ epi_judge_java/epi/ReverseSublist.java
@@ -4,12 +4,23 @@ import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 
 public class ReverseSublist {
-    @EpiTest(testDataFile = "reverse_sublist.tsv")
 
-    public static ListNode<Integer> reverseSublist(ListNode<Integer> L, int start,
-                                                   int finish) {
-        // TODO - you fill in here.
-        return null;
+    @EpiTest(testDataFile = "reverse_sublist.tsv")
+    public static ListNode<Integer> reverseSublist(ListNode<Integer> list, int start, int finish) {
+        if (start == finish) return list;
+        ListNode<Integer> dummy = new ListNode<>(-1, list);
+        ListNode<Integer> beforeStartNode = dummy;
+        for (int i = 1; i < start; i++) {
+            beforeStartNode = beforeStartNode.next;
+        }
+        var runner = beforeStartNode.next;
+        for (int i = start; i < finish; i++) {
+            var temp = beforeStartNode.next;
+            beforeStartNode.next = runner.next;
+            runner.next = runner.next.next;
+            beforeStartNode.next.next = temp;
+        }
+        return dummy.next;
     }
 
     public static void main(String[] args) {
