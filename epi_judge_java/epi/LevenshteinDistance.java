ebd251de8468ba35ec26081322b619b6a4873394
Denis Bogdanov <bogdanov.math@gmail.com>
2026-04-30 11:50:22 +0200

    Java.

--- epi_judge_java/epi/LevenshteinDistance.java
+++ epi_judge_java/epi/LevenshteinDistance.java
@@ -4,11 +4,29 @@ import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 
 public class LevenshteinDistance {
-    @EpiTest(testDataFile = "levenshtein_distance.tsv")
 
-    public static int levenshteinDistance(String A, String B) {
-        // TODO - you fill in here.
-        return 0;
+    @EpiTest(testDataFile = "levenshtein_distance.tsv")
+    public static int levenshteinDistance(String a, String b) {
+        int[][] dp = new int[a.length() + 1][b.length() + 1];
+        for (int i = 0; i <= a.length(); i++) {
+            dp[i][0] = i;
+        }
+        for (int i = 0; i <= b.length(); i++) {
+            dp[0][i] = i;
+        }
+        for (int i = 1; i <= a.length(); i++) {
+            for (int j = 1; j <= b.length(); j++) {
+                if (a.charAt(i - 1) == b.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = 1 + Math.min(
+                            Math.min(dp[i - 1][j], dp[i][j - 1]),
+                            dp[i - 1][j - 1]
+                    );
+                }
+            }
+        }
+        return dp[a.length()][b.length()];
     }
 
     public static void main(String[] args) {
