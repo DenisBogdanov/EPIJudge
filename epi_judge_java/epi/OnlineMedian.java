b97e6e78fd3cdf8a4d5e05d898ad04ce5bde08b1
Denis Bogdanov <bogdanov.math@gmail.com>
2026-04-30 11:30:14 +0200

    Java.

--- epi_judge_java/epi/OnlineMedian.java
+++ epi_judge_java/epi/OnlineMedian.java
@@ -3,13 +3,27 @@ package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
 
 public class OnlineMedian {
     public static List<Double> onlineMedian(Iterator<Integer> sequence) {
-        // TODO - you fill in here.
-        return null;
+        PriorityQueue<Integer> left = new PriorityQueue<>(Comparator.reverseOrder());
+        PriorityQueue<Integer> right = new PriorityQueue<>();
+        List<Double> ans = new ArrayList<>();
+        while (sequence.hasNext()) {
+            int next = sequence.next();
+            left.offer(next);
+            right.offer(left.poll());
+            while (right.size() > left.size()) {
+                left.offer(right.poll());
+            }
+            if (left.size() == right.size()) {
+                ans.add((left.peek() + right.peek()) / 2.0);
+            } else {
+                ans.add(left.peek().doubleValue());
+            }
+        }
+        return ans;
     }
 
     @EpiTest(testDataFile = "online_median.tsv")
