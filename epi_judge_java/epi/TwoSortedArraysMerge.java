c271e87a0a17a9cb31fabb9aac86055c0120fc09
Denis Bogdanov <bogdanov.math@gmail.com>
2026-04-30 16:57:17 +0200

    Java.

--- epi_judge_java/epi/TwoSortedArraysMerge.java
+++ epi_judge_java/epi/TwoSortedArraysMerge.java
@@ -7,10 +7,21 @@ import java.util.List;
 
 public class TwoSortedArraysMerge {
 
-    public static void mergeTwoSortedArrays(List<Integer> A, int m,
-                                            List<Integer> B, int n) {
-        // TODO - you fill in here.
-        return;
+    public static void mergeTwoSortedArrays(List<Integer> a, int m, List<Integer> b, int n) {
+        int writeIdx = m + n - 1;
+        m--;
+        n--;
+        while (m >= 0 || n >= 0) {
+            if (n < 0) break;
+            if (m < 0 || a.get(m) < b.get(n)) {
+                a.set(writeIdx, b.get(n));
+                n--;
+            } else {
+                a.set(writeIdx, a.get(m));
+                m--;
+            }
+            writeIdx--;
+        }
     }
 
     @EpiTest(testDataFile = "two_sorted_arrays_merge.tsv")
