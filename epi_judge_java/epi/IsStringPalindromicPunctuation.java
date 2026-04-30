90d8200d9501aac2deee34530af92569ef8f6513
Denis Bogdanov <bogdanov.math@gmail.com>
2026-05-01 00:15:01 +0200

    Java.

--- epi_judge_java/epi/IsStringPalindromicPunctuation.java
+++ epi_judge_java/epi/IsStringPalindromicPunctuation.java
@@ -4,10 +4,20 @@ import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 
 public class IsStringPalindromicPunctuation {
-    @EpiTest(testDataFile = "is_string_palindromic_punctuation.tsv")
 
+    @EpiTest(testDataFile = "is_string_palindromic_punctuation.tsv")
     public static boolean isPalindrome(String s) {
-        // TODO - you fill in here.
+        int left = 0;
+        int right = s.length() - 1;
+        while (left < right) {
+            while (left < right && !Character.isLetterOrDigit(s.charAt(left))) left++;
+            while (left < right && !Character.isLetterOrDigit(s.charAt(right))) right--;
+            if (Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))) {
+                return false;
+            }
+            left++;
+            right--;
+        }
         return true;
     }
 
