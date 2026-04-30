eb4079731802270ce764d28e6350664275780a00
Denis Bogdanov <bogdanov.math@gmail.com>
2026-04-30 11:40:37 +0200

    Java.

--- epi_judge_java/epi/MinimumPointsCoveringIntervals.java
+++ epi_judge_java/epi/MinimumPointsCoveringIntervals.java
@@ -4,11 +4,27 @@ import epi.test_framework.EpiTest;
 import epi.test_framework.EpiUserType;
 import epi.test_framework.GenericTest;
 
+import java.util.Comparator;
 import java.util.List;
 
 public class MinimumPointsCoveringIntervals {
-    @EpiUserType(ctorParams = {int.class, int.class})
 
+    @EpiTest(testDataFile = "minimum_points_covering_intervals.tsv")
+    public static Integer findMinimumVisits(List<Interval> intervals) {
+        if (intervals.isEmpty()) return 0;
+        intervals.sort(Comparator.comparingInt(i -> i.right));
+        int ans = 1;
+        int latestCover = intervals.get(0).right;
+        for (Interval interval : intervals) {
+            if (interval.left > latestCover) {
+                ans++;
+                latestCover = interval.right;
+            }
+        }
+        return ans;
+    }
+
+    @EpiUserType(ctorParams = {int.class, int.class})
     public static class Interval {
         public int left, right;
 
@@ -16,13 +32,6 @@ public class MinimumPointsCoveringIntervals {
             this.left = l;
             this.right = r;
         }
-    }
-
-    @EpiTest(testDataFile = "minimum_points_covering_intervals.tsv")
-
-    public static Integer findMinimumVisits(List<Interval> intervals) {
-        // TODO - you fill in here.
-        return 0;
     }
 
     public static void main(String[] args) {
