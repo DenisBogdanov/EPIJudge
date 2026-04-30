155afd7a78989eac7ae88e2b3f99d26c99aa028e
Denis Bogdanov <bogdanov.math@gmail.com>
2026-04-30 12:02:08 +0200

    Java.

--- epi_judge_java/epi/BinomialCoefficients.java
+++ epi_judge_java/epi/BinomialCoefficients.java
@@ -3,12 +3,28 @@ package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 
-public class BinomialCoefficients {
-    @EpiTest(testDataFile = "binomial_coefficients.tsv")
+import java.util.ArrayList;
+import java.util.List;
 
+public class BinomialCoefficients {
+
+    @EpiTest(testDataFile = "binomial_coefficients.tsv")
     public static int computeBinomialCoefficient(int n, int k) {
-        // TODO - you fill in here.
-        return 0;
+        // 1: 1
+        // 2: 1 1
+        // 3: 1 2 1
+        // 4: 1 3 3 1
+        List<Integer> currDp = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            List<Integer> temp = new ArrayList<>();
+            temp.add(1);
+            for (int j = 0; j < currDp.size() - 1; j++) {
+                temp.add(currDp.get(j) + currDp.get(j + 1));
+            }
+            temp.add(1);
+            currDp = temp;
+        }
+        return currDp.get(k);
     }
 
     public static void main(String[] args) {
