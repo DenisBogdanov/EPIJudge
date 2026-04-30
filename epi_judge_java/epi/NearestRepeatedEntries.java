bb59492bd39465f221e64079e659fd392b3a8bc4
Denis Bogdanov <bogdanov.math@gmail.com>
2026-04-30 18:33:50 +0200

    Java.

--- epi_judge_java/epi/NearestRepeatedEntries.java
+++ epi_judge_java/epi/NearestRepeatedEntries.java
@@ -3,14 +3,23 @@ package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class NearestRepeatedEntries {
-    @EpiTest(testDataFile = "nearest_repeated_entries.tsv")
 
+    @EpiTest(testDataFile = "nearest_repeated_entries.tsv")
     public static int findNearestRepetition(List<String> paragraph) {
-        // TODO - you fill in here.
-        return 0;
+        Map<String, Integer> wordToPrevIndexMap = new HashMap<>();
+        int ans = Integer.MAX_VALUE;
+        for (int i = 0; i < paragraph.size(); i++) {
+            String word = paragraph.get(i);
+            Integer prevIndex = wordToPrevIndexMap.get(word);
+            if (prevIndex != null) ans = Math.min(ans, i - prevIndex);
+            wordToPrevIndexMap.put(word, i);
+        }
+        return ans == Integer.MAX_VALUE ? -1 : ans;
     }
 
     public static void main(String[] args) {
