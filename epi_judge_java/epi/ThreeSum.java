03f8f7ae719e53a075bf1cb9f2184c9983ad8603
Denis Bogdanov <bogdanov.math@gmail.com>
2026-04-30 11:45:25 +0200

    Java.

--- epi_judge_java/epi/ThreeSum.java
+++ epi_judge_java/epi/ThreeSum.java
@@ -3,14 +3,20 @@ package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 
+import java.util.Collections;
 import java.util.List;
 
 public class ThreeSum {
-    @EpiTest(testDataFile = "three_sum.tsv")
 
-    public static boolean hasThreeSum(List<Integer> A, int t) {
-        // TODO - you fill in here.
-        return true;
+    @EpiTest(testDataFile = "three_sum.tsv")
+    public static boolean hasThreeSum(List<Integer> nums, int t) {
+        nums.sort(null);
+        for (int i = 0; i < nums.size(); i++) {
+            for (int j = i; j < nums.size(); j++) {
+                if (Collections.binarySearch(nums, t - nums.get(i) - nums.get(j)) >= 0) return true;
+            }
+        }
+        return false;
     }
 
     public static void main(String[] args) {
