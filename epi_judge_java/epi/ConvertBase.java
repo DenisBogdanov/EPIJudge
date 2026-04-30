6a85458a53d832d1376748a765100032404aae3f
Denis Bogdanov <bogdanov.math@gmail.com>
2026-04-30 18:40:27 +0200

    Java.

--- epi_judge_java/epi/ConvertBase.java
+++ epi_judge_java/epi/ConvertBase.java
@@ -4,11 +4,11 @@ import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 
 public class ConvertBase {
-    @EpiTest(testDataFile = "convert_base.tsv")
 
+    @EpiTest(testDataFile = "convert_base.tsv")
     public static String convertBase(String numAsString, int b1, int b2) {
-        // TODO - you fill in here.
-        return "";
+        int num = Integer.parseInt(numAsString, b1);
+        return Integer.toString(num, b2).toUpperCase();
     }
 
     public static void main(String[] args) {
