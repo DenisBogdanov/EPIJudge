6b68638e7494a6d94aa1f920ed80540a461aa8f2
Denis Bogdanov <bogdanov.math@gmail.com>
2026-04-30 13:20:24 +0200

    Java.

--- epi_judge_java/epi/IsTreeABst.java
+++ epi_judge_java/epi/IsTreeABst.java
@@ -4,11 +4,16 @@ import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 
 public class IsTreeABst {
-    @EpiTest(testDataFile = "is_tree_a_bst.tsv")
 
+    @EpiTest(testDataFile = "is_tree_a_bst.tsv")
     public static boolean isBinaryTreeBST(BinaryTreeNode<Integer> tree) {
-        // TODO - you fill in here.
-        return true;
+        return isValid(tree, Integer.MIN_VALUE, Integer.MAX_VALUE);
+    }
+
+    private static boolean isValid(BinaryTreeNode<Integer> tree, int left, int right) {
+        if (tree == null) return true;
+        if (tree.data < left || tree.data > right) return false;
+        return isValid(tree.left, left, tree.data) && isValid(tree.right, tree.data, right);
     }
 
     public static void main(String[] args) {
