3b9a38f89eddc175e4e0e06540dcdda9fae33f0d
Denis Bogdanov <bogdanov.math@gmail.com>
2026-05-01 17:55:16 +0200

    Java.

--- epi_judge_java/epi/SortList.java
+++ epi_judge_java/epi/SortList.java
@@ -3,12 +3,27 @@ package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 
-public class SortList {
-    @EpiTest(testDataFile = "sort_list.tsv")
+import java.util.ArrayList;
+import java.util.List;
 
-    public static ListNode<Integer> stableSortList(ListNode<Integer> L) {
-        // TODO - you fill in here.
-        return null;
+public class SortList {
+
+    @EpiTest(testDataFile = "sort_list.tsv")
+    public static ListNode<Integer> stableSortList(ListNode<Integer> head) {
+        List<Integer> nums = new ArrayList<>();
+        var runner = head;
+        while (runner != null) {
+            nums.add(runner.data);
+            runner = runner.next;
+        }
+        nums.sort(null);
+        var dummy = new ListNode<>(-1, null);
+        runner = dummy;
+        for (int num : nums) {
+            runner.next = new ListNode<>(num, null);
+            runner = runner.next;
+        }
+        return dummy.next;
     }
 
     public static void main(String[] args) {
