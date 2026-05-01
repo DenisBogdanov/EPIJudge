99a1784cffc812d91789b52e9b0fb5448cb39b68
Denis Bogdanov <bogdanov.math@gmail.com>
2026-05-01 16:33:07 +0200

    Java.

--- epi_judge_java/epi/DeleteKthLastFromList.java
+++ epi_judge_java/epi/DeleteKthLastFromList.java
@@ -4,12 +4,22 @@ import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 
 public class DeleteKthLastFromList {
-    @EpiTest(testDataFile = "delete_kth_last_from_list.tsv")
 
+    @EpiTest(testDataFile = "delete_kth_last_from_list.tsv")
     // Assumes L has at least k nodes, deletes the k-th last node in L.
-    public static ListNode<Integer> removeKthLast(ListNode<Integer> L, int k) {
-        // TODO - you fill in here.
-        return null;
+    public static ListNode<Integer> removeKthLast(ListNode<Integer> head, int k) {
+        var dummy = new ListNode<Integer>(-1, head);
+        var runner = dummy;
+        for (int i = 0; i < k + 1; i++) {
+            runner = runner.next;
+        }
+        var toRemovePrev = dummy;
+        while (runner != null) {
+            toRemovePrev = toRemovePrev.next;
+            runner = runner.next;
+        }
+        toRemovePrev.next = toRemovePrev.next.next;
+        return dummy.next;
     }
 
     public static void main(String[] args) {
