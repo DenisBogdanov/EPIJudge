2202ca6a2bfc52d1f2e420b5d0088d35532c1bb0
Denis Bogdanov <bogdanov.math@gmail.com>
2026-05-01 16:03:30 +0200

    Java.

--- epi_judge_java/epi/SpiralOrdering.java
+++ epi_judge_java/epi/SpiralOrdering.java
@@ -3,15 +3,39 @@ package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class SpiralOrdering {
-    @EpiTest(testDataFile = "spiral_ordering.tsv")
 
-    public static List<Integer>
-    matrixInSpiralOrder(List<List<Integer>> squareMatrix) {
-        // TODO - you fill in here.
-        return null;
+    @EpiTest(testDataFile = "spiral_ordering.tsv")
+    public static List<Integer> matrixInSpiralOrder(List<List<Integer>> squareMatrix) {
+        if (squareMatrix.isEmpty()) return List.of();
+        int n = squareMatrix.size();
+        List<Integer> ans = new ArrayList<>();
+        int right = n - 1;
+        int left = 0;
+        int top = 0;
+        int bottom = n - 1;
+        while (ans.size() < n * n) {
+            for (int i = left; i <= right; i++) {
+                ans.add(squareMatrix.get(top).get(i));
+            }
+            top++;
+            for (int i = top; i <= bottom; i++) {
+                ans.add(squareMatrix.get(i).get(right));
+            }
+            right--;
+            for (int i = right; i >= left; i--) {
+                ans.add(squareMatrix.get(bottom).get(i));
+            }
+            bottom--;
+            for (int i = bottom; i >= top; i--) {
+                ans.add(squareMatrix.get(i).get(left));
+            }
+            left++;
+        }
+        return ans;
     }
 
     public static void main(String[] args) {
