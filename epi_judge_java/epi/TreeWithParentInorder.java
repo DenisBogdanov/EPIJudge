5c92a2490d2866d5956377f7216d1ed50ca02185
Denis Bogdanov <bogdanov.math@gmail.com>
2026-05-01 16:47:12 +0200

    Java.

--- epi_judge_java/epi/TreeWithParentInorder.java
+++ epi_judge_java/epi/TreeWithParentInorder.java
@@ -3,14 +3,23 @@ package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class TreeWithParentInorder {
-    @EpiTest(testDataFile = "tree_with_parent_inorder.tsv")
 
+    @EpiTest(testDataFile = "tree_with_parent_inorder.tsv")
     public static List<Integer> inorderTraversal(BinaryTree<Integer> tree) {
-        // TODO - you fill in here.
-        return null;
+        if (tree == null) return List.of();
+        List<Integer> ans = new ArrayList<>();
+        while (tree.left != null) {
+            tree = tree.left;
+        }
+        while (tree != null) {
+            ans.add(tree.data);
+            tree = SuccessorInTree.findSuccessor(tree);
+        }
+        return ans;
     }
 
     public static void main(String[] args) {
