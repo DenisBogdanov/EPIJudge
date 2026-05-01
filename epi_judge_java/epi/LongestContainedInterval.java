64b9fdafcc4d46c4b8500822f65a7f27837dda13
Denis Bogdanov <bogdanov.math@gmail.com>
2026-05-01 17:52:18 +0200

    Java.

--- epi_judge_java/epi/LongestContainedInterval.java
+++ epi_judge_java/epi/LongestContainedInterval.java
@@ -6,11 +6,24 @@ import epi.test_framework.GenericTest;
 import java.util.List;
 
 public class LongestContainedInterval {
-    @EpiTest(testDataFile = "longest_contained_interval.tsv")
 
-    public static int longestContainedRange(List<Integer> A) {
-        // TODO - you fill in here.
-        return 0;
+    @EpiTest(testDataFile = "longest_contained_interval.tsv")
+    public static int longestContainedRange(List<Integer> nums) {
+        nums.sort(null);
+        int ans = 1;
+        int start = nums.get(0);
+        int curr = nums.get(0);
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums.get(i) == curr) continue;
+            if (nums.get(i) == curr + 1) {
+                curr++;
+                ans = Math.max(ans, curr - start + 1);
+            } else {
+                start = nums.get(i);
+                curr = nums.get(i);
+            }
+        }
+        return ans;
     }
 
     public static void main(String[] args) {
