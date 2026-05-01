d4eee8edd4c53777cf09281f465f427419e54070
Denis Bogdanov <bogdanov.math@gmail.com>
2026-05-01 15:36:42 +0200

    Java.

--- epi_judge_java/epi/ReverseDigits.java
+++ epi_judge_java/epi/ReverseDigits.java
@@ -4,10 +4,11 @@ import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 
 public class ReverseDigits {
+
     @EpiTest(testDataFile = "reverse_digits.tsv")
     public static long reverse(int x) {
-        // TODO - you fill in here.
-        return 0;
+        int sign = (x < 0 ? -1 : 1);
+        return sign * Long.parseLong(new StringBuilder().append(Math.abs((long) x)).reverse().toString());
     }
 
     public static void main(String[] args) {
