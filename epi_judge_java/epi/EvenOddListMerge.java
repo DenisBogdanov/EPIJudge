63255bbf3274832e29957165bc70ced6f7aac552
Denis Bogdanov <bogdanov.math@gmail.com>
2026-05-01 16:37:59 +0200

    Java.

--- epi_judge_java/epi/EvenOddListMerge.java
+++ epi_judge_java/epi/EvenOddListMerge.java
@@ -4,11 +4,28 @@ import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 
 public class EvenOddListMerge {
-    @EpiTest(testDataFile = "even_odd_list_merge.tsv")
 
-    public static ListNode<Integer> evenOddMerge(ListNode<Integer> L) {
-        // TODO - you fill in here.
-        return null;
+    @EpiTest(testDataFile = "even_odd_list_merge.tsv")
+    public static ListNode<Integer> evenOddMerge(ListNode<Integer> head) {
+        int i = 0;
+        var runner = head;
+        var leftDummy = new ListNode<>(-1, null);
+        var leftRunner = leftDummy;
+        var rightDummy = new ListNode<>(-1, null);
+        var rightRunner = rightDummy;
+        while (runner != null) {
+            if (i % 2 == 0) {
+                leftRunner.next = new ListNode<>(runner.data, null);
+                leftRunner = leftRunner.next;
+            } else {
+                rightRunner.next = new ListNode<>(runner.data, null);
+                rightRunner = rightRunner.next;
+            }
+            i++;
+            runner = runner.next;
+        }
+        leftRunner.next = rightDummy.next;
+        return leftDummy.next;
     }
 
     public static void main(String[] args) {
