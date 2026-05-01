c4b71455891e41f1c6f82d7ec09ce22a6e2466c3
Denis Bogdanov <bogdanov.math@gmail.com>
2026-05-01 14:55:02 +0200

    Java.

--- epi_judge_java/epi/BuyAndSellStockTwice.java
+++ epi_judge_java/epi/BuyAndSellStockTwice.java
@@ -6,10 +6,28 @@ import epi.test_framework.GenericTest;
 import java.util.List;
 
 public class BuyAndSellStockTwice {
+
     @EpiTest(testDataFile = "buy_and_sell_stock_twice.tsv")
     public static double buyAndSellStockTwice(List<Double> prices) {
-        // TODO - you fill in here.
-        return 0.0;
+        int n = prices.size();
+        double[] left = new double[n];
+        double minPriceSoFar = prices.get(0);
+        double maxProfitSoFar = 0.0;
+        for (int i = 1; i < n; i++) {
+            minPriceSoFar = Math.min(minPriceSoFar, prices.get(i));
+            maxProfitSoFar = Math.max(maxProfitSoFar, prices.get(i) - minPriceSoFar);
+            left[i] = maxProfitSoFar;
+        }
+        double ans = maxProfitSoFar;
+
+        double maxPriceSoFar = prices.get(n - 1);
+        maxProfitSoFar = 0.0;
+        for (int i = n - 2; i > 0; i--) {
+            maxPriceSoFar = Math.max(maxPriceSoFar, prices.get(i));
+            maxProfitSoFar = Math.max(maxProfitSoFar, maxPriceSoFar - prices.get(i));
+            ans = Math.max(ans, maxProfitSoFar + left[i - 1]);
+        }
+        return ans;
     }
 
     public static void main(String[] args) {
