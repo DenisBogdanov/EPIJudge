c041634dc16626e92ac73ca1ee4be9c77d7d0ec8
Denis Bogdanov <bogdanov.math@gmail.com>
2026-05-01 16:10:15 +0200

    Java.

--- epi_judge_java/epi/LookAndSay.java
+++ epi_judge_java/epi/LookAndSay.java
@@ -4,11 +4,27 @@ import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 
 public class LookAndSay {
-    @EpiTest(testDataFile = "look_and_say.tsv")
 
+    @EpiTest(testDataFile = "look_and_say.tsv")
     public static String lookAndSay(int n) {
-        // TODO - you fill in here.
-        return "";
+        String s = "1";
+        for (int i = 1; i < n; i++) {
+            StringBuilder sb = new StringBuilder();
+            int count = 1;
+            char prev = s.charAt(0);
+            for (int j = 1; j < s.length(); j++) {
+                if (s.charAt(j) != prev) {
+                    sb.append(count).append(prev);
+                    count = 1;
+                    prev = s.charAt(j);
+                } else {
+                    count++;
+                }
+            }
+            sb.append(count).append(prev);
+            s = sb.toString();
+        }
+        return s;
     }
 
     public static void main(String[] args) {
