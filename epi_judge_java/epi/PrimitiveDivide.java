23f6aaee2d03bbf43b4960088cc4bdd1b4a0f8f8
Denis Bogdanov <bogdanov.math@gmail.com>
2026-05-01 15:42:04 +0200

    Java.

--- epi_judge_java/epi/PrimitiveDivide.java
+++ epi_judge_java/epi/PrimitiveDivide.java
@@ -4,10 +4,23 @@ import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 
 public class PrimitiveDivide {
+
     @EpiTest(testDataFile = "primitive_divide.tsv")
     public static int divide(int x, int y) {
-        // TODO - you fill in here.
-        return 0;
+        long a = x;
+        long b = y;
+        int sign = (a * b >= 0 ? 1 : -1);
+        a = Math.abs(a);
+        b = Math.abs(b);
+        long left = 0;
+        long right = x + 1;
+        while (left + 1 < right) {
+            long mid = left + (right - left) / 2;
+            if (mid * b == a) return (int) (sign * mid);
+            if (mid * b > a) right = mid;
+            else left = mid;
+        }
+        return (int) (sign * left);
     }
 
     public static void main(String[] args) {
