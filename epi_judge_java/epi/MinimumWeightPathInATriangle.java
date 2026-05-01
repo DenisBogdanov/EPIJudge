316a2fdc830a5931ae336746ee4aa524f3a8bf66
Denis Bogdanov <bogdanov.math@gmail.com>
2026-05-01 19:14:58 +0200

    Java.

--- epi_judge_java/epi/MinimumWeightPathInATriangle.java
+++ epi_judge_java/epi/MinimumWeightPathInATriangle.java
@@ -6,11 +6,19 @@ import epi.test_framework.GenericTest;
 import java.util.List;
 
 public class MinimumWeightPathInATriangle {
-    @EpiTest(testDataFile = "minimum_weight_path_in_a_triangle.tsv")
 
+    @EpiTest(testDataFile = "minimum_weight_path_in_a_triangle.tsv")
     public static int minimumPathTotal(List<List<Integer>> triangle) {
-        // TODO - you fill in here.
-        return 0;
+        if (triangle.isEmpty()) return 0;
+        for (int i = 1; i < triangle.size(); i++) {
+            for (int j = 0; j < triangle.get(i).size(); j++) {
+                int min = Integer.MAX_VALUE;
+                if (j > 0) min = triangle.get(i - 1).get(j - 1);
+                if (j < triangle.get(i - 1).size()) min = Math.min(min, triangle.get(i - 1).get(j));
+                triangle.get(i).set(j, triangle.get(i).get(j) + min);
+            }
+        }
+        return triangle.get(triangle.size() - 1).stream().min(Integer::compare).get();
     }
 
     public static void main(String[] args) {
