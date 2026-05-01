0fd1fb308d8ba65e8e7e32dd55ca1c80a1902db8
Denis Bogdanov <bogdanov.math@gmail.com>
2026-05-01 16:57:17 +0200

    Java.

--- epi_judge_java/epi/KLargestInHeap.java
+++ epi_judge_java/epi/KLargestInHeap.java
@@ -4,15 +4,25 @@ import epi.test_framework.EpiTest;
 import epi.test_framework.EpiTestComparator;
 import epi.test_framework.GenericTest;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.PriorityQueue;
 
 public class KLargestInHeap {
-    @EpiTest(testDataFile = "k_largest_in_heap.tsv")
 
-    public static List<Integer> kLargestInBinaryHeap(List<Integer> A, int k) {
-        // TODO - you fill in here.
-        return null;
+    @EpiTest(testDataFile = "k_largest_in_heap.tsv")
+    public static List<Integer> kLargestInBinaryHeap(List<Integer> nums, int k) {
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        for (int num : nums) {
+            minHeap.offer(num);
+            if (minHeap.size() > k) minHeap.poll();
+        }
+        List<Integer> ans = new ArrayList<>();
+        while (!minHeap.isEmpty()) {
+            ans.add(minHeap.poll());
+        }
+        return ans;
     }
 
     @EpiTestComparator
